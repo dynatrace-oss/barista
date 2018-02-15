71ab53949db74885cfa54b2aa1e609565eba7d7d
Dorota Zaranska <d.makotka@gmail.com>
2018-02-15 01:25:19 +0100

    APM-120945, APM-119241 - checkbox + switch init

--- src/docs/docs-components/docs-switch/examples/switch-html-example.component.ts
+++ src/docs/docs-components/docs-switch/examples/switch-html-example.component.ts
@@ -10,8 +10,8 @@ import { Component } from "@angular/core";
   // @formatter:on
 })
 export class SwitchHtmlExampleComponent {
-  static readonly htmlContent = "This is some <sup>HTML</sup> content<sub>example</sub>";
-  static readonly longContent = "This is an example of a long content. Very long content." +
+  public static readonly htmlContent = "This is some <sup>HTML</sup> content<sub>example</sub>";
+  public static readonly longContent = "This is an example of a long content. Very long content." +
       "It's so long that if you are reading this, you must be very bored. " +
     "Such a long text! But to tell the truth, this text is not long at all..." +
       "it could have been much longer if its author was less lazy.";
