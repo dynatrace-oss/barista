77d012427f301ab4167a5d002e1041849b255ffe
Dorota Zaranska <d.makotka@gmail.com>
2018-02-21 17:31:27 +0100

    APM-120945, APM-119241 - checkbox + switch init

---
+++ src/docs/docs-components/docs-switch/examples/switch-form-example.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  // @formatter:off
+  template: `
+  <form [formGroup]="formWithSwitch" (submit)="submit()">
+    <dt-switch formControlName="enabled" label="I agree..."></dt-switch>
+    <a dt-btn class="btn btn--primary" [disabled]="formWithSwitch.invalid">Submit</a>
+  </form>
+`,
+  // @formatter:on
+})
+export class SwitchFormExampleComponent {
+  public formWithSwitch = new FormGroup({
+    enabled: new FormControl(),
+  });
+
+  public constructor(private readonly formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.formWithSwitch = this.formBuilder.group({
+      enabled: ["false", Validators.pattern("true")],
+    });
+  }
+
+  public submit(): void {
+    // TODO
+  }
+
+}
