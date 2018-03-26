5d1bb844e049dce80c120c072a00443f543dc6d7
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-03-26 14:43:09 +0200

    feat(build): added tslint

--- tools/gulp/tasks/lint.ts
+++ tools/gulp/tasks/lint.ts
@@ -4,6 +4,14 @@ import {execNodeTask} from '../util/task_helpers';
 /** Glob that matches all SCSS or CSS files that should be linted. */
 const stylesGlob = 'src/lib/**/!(*.bundle).+(css|scss)';
 
-task('lint', execNodeTask(
+const tsGlob = 'src/lib/**/!(*.spec).ts';
+
+task('lint', ['stylelint', 'tslint']);
+
+task('stylelint', execNodeTask(
   'stylelint', [stylesGlob, '--config', 'stylelint-config.json', '--syntax', 'scss']
+));
+
+task('tslint', execNodeTask(
+  'tslint', [tsGlob, '--config', 'tslint.json']
 ));
