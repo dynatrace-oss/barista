a9ef8b6361d7380f3c08d577465e99029cdf5fe9
Thomas Pink <thomas.pink@me.com>
2018-03-28 14:04:15 +0200

    feat(button): added button to docs

--- src/docs/docs.component.ts
+++ src/docs/docs.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class Docs {
   navItems = [
     {name: 'Start', route: '/'},
-    {name: 'Dummy', route: '/dummy'},
+    {name: 'Button', route: '/button'},
   ];
 }
