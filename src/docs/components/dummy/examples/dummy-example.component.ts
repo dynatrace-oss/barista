f5f8d9e44facd63596197e6fb0b04d8e67f88dd5
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-03-28 13:05:54 +0200

    chore(docs): remove comment

--- src/docs/components/dummy/examples/dummy-example.component.ts
+++ src/docs/components/dummy/examples/dummy-example.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  // inline template for now - create pre build gulp task to inline templateUrls and move template to html
   template: `<dt-dummy>Dummy</dt-dummy>`,
 })
 export class DummyExampleComponent {
