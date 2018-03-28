533e90c1d205669f0d275ac97444678581f582de
Thomas Pink <thomas.pink@dynatrace.com>
2018-03-28 13:13:43 +0200

    feat(core): Added common behaviours for color and disabled

---
+++ src/lib/core/common-behaviours/color.ts
@@ -0,0 +1,54 @@
+import { Constructor } from './constructor';
+import { ElementRef } from '@angular/core';
+
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+export interface CanColor {
+  /** Theme color palette for the component. */
+  color: ThemePalette;
+}
+
+export interface HasElementRef {
+  _elementRef: ElementRef;
+}
+
+/** Possible color palette values. */
+export type ThemePalette = 'main' | 'accent' | 'warning' | 'error' | 'cta' | undefined;
+
+/** Mixin to augment a directive with a `color` property. */
+export function mixinColor<T extends Constructor<HasElementRef>>(
+  base: T, defaultColor?: ThemePalette): Constructor<CanColor> & T;
+export function mixinColor<T extends Constructor<HasElementRef>, P extends Partial<ThemePalette>>(
+  base: T, defaultColor?: P): Constructor<CanColor> & T;
+export function mixinColor<T extends Constructor<HasElementRef>, P extends Partial<ThemePalette>>(
+  base: T, defaultColor?: P): Constructor<CanColor> & T {
+  return class extends base {
+    private _color: P;
+
+    get color(): P { return this._color; }
+    set color(value: P) {
+      const colorPalette = value || defaultColor as P;
+
+      if (colorPalette !== this._color) {
+        if (this._color) {
+          this._elementRef.nativeElement.classList.remove(`dt-color-${this._color}`);
+        }
+        if (colorPalette) {
+          this._elementRef.nativeElement.classList.add(`dt-color-${colorPalette}`);
+        }
+
+        this._color = colorPalette;
+      }
+    }
+
+    // tslint:disable-next-line:no-any
+    constructor(...args: any[]) {
+      super(...args);
+
+      // Set the default color that can be specified from the mixin.
+      this.color = defaultColor as P;
+    }
+  };
+}
---
+++ src/lib/core/common-behaviours/disabled.spec.ts
@@ -0,0 +1,17 @@
+import {mixinDisabled} from './disabled';
+
+describe('MixinDisabled', () => {
+  it('should augment an existing class with a disabled property', () => {
+    class EmptyClass { }
+
+    const classWithDisabled = mixinDisabled(EmptyClass);
+    const instance = new classWithDisabled();
+
+    expect(instance.disabled)
+        .toBe(false, 'Expected the mixed-into class to have a disabled property');
+
+    instance.disabled = true;
+    expect(instance.disabled)
+        .toBe(true, 'Expected the mixed-into class to have an updated disabled property');
+  });
+});
