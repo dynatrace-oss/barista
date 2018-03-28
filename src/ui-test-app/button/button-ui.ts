15a807d8c5168a9705ab46a28c4609e4c2422a9f
Thomas Pink <thomas.pink@me.com>
2018-03-28 15:26:38 +0200

    feat(button): ui tests

---
+++ src/ui-test-app/button/button-ui.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'dt-button-ui',
+  templateUrl: 'button-ui.html',
+})
+export class ButtonUI {
+  isDisabled = false;
+  clickCounter = 0;
+}
