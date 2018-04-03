dbb2619dad3080089c52011a04d709e606573fb2
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-03 17:10:25 +0200

    chore(build): add license header

--- tools/gulp/util/http-server.ts
+++ tools/gulp/util/http-server.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Based on the build of angular/material2 by Google Inc. governed by an
+ * MIT-style license that can be found in the LICENSE file at https://angular.io/license
+ */
 import * as fs from 'fs';
 import * as path from 'path';
 import { buildConfig } from '../build-config';
