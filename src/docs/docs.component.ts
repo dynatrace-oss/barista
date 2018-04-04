173a1b2d092c3ebae66fe19d6df34c6b950ae2f4
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-04 11:22:20 +0200

    refactor(docs): moved home to own component

--- src/docs/docs.component.ts
+++ src/docs/docs.component.ts
@@ -1,29 +1,5 @@
 import { Component } from '@angular/core';
 
-/**
- * Home component for welcome message in DocsApp.
- */
-@Component({
-  selector: 'docs-home',
-  template: `
-  <h1>Dynatrace angular components library</h1>
-  <p>Choose component from the left menu to see its documentation.<p>
-  <h2>Links</h2>
-  <ul class="list">
-    <li>
-      <a href="***REMOVED***
-        Repository
-      </a>
-    </li>
-    <li>
-      <a href="***REMOVED*** styles</a>
-      - latest style guide
-    </li>
-  </ul>
-  `,
-})
-export class Home {}
-
 @Component({
   selector: 'docs-app',
   styleUrls: ['docs.component.scss'],
