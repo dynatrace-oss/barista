2f6906cbe6a290e675bc529accbf19e34974e667
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-04 11:26:26 +0200

    chore(universal): remove Kitchensink unused variables

--- src/universal-app/kitchen-sink/kitchen-sink.ts
+++ src/universal-app/kitchen-sink/kitchen-sink.ts
@@ -1,24 +1,16 @@
 import {Component, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {ServerModule} from '@angular/platform-server';
-// import {
-//   ButtonModule,
-// } from '@dynatrace/angular-components';
 
 @Component({
   selector: 'kitchen-sink',
   templateUrl: './kitchen-sink.html',
 })
-export class KitchenSink {
-  progressCircleMin = 100;
-  progressCircleMax = 200;
-  progressCircleValue = 150;
-}
+export class KitchenSink {}
 
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({appId: 'kitchen-sink'}),
-    // ButtonModule,
   ],
   bootstrap: [KitchenSink],
   declarations: [KitchenSink],
