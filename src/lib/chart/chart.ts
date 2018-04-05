a6bba443a372710d688602cae88683f316739256
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-05 09:58:26 +0200

    feat(chart): observables as input

--- src/lib/chart/chart.ts
+++ src/lib/chart/chart.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 export type ChartType = 'line' | 'column' | 'pie';
 const defaultChartType = 'line';
@@ -12,77 +14,49 @@ const defaultChartType = 'line';
   exportAs: 'dtChart',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DtChart implements AfterViewInit {
+export class DtChart implements AfterViewInit, OnDestroy {
   @ViewChild('container') container: ElementRef;
 
-  private _options: Highcharts.Options = {
-    chart: {},
-    title: {
-      text: 'Monthly Average Rainfall',
-    },
-    subtitle: {
-      text: 'Source: WorldClimate.com',
-    },
-    xAxis: {
-      categories: [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec',
-      ],
-      crosshair: true,
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: 'Rainfall (mm)',
-      },
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-      footerFormat: '</table>',
-      shared: true,
-      useHTML: true,
-    },
-    plotOptions: {
-      column: {
-        pointPadding: 0.2,
-        borderWidth: 0,
-      },
-    },
-    series: [{
-      name: 'Tokyo',
-      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-    }, {
-      name: 'New York',
-      data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3],
-    }],
-  };
+  private _options: Highcharts.Options = {};
   private _chartObject: Highcharts.ChartObject;
+  private _dataSub: Subscription;
 
   @Input()
-  get type(): ChartType {
-    return this._options.chart!.type as ChartType;
+  get options(): Highcharts.Options {
+    return this._options;
   }
-  set type(value: ChartType) {
-    const type = value || defaultChartType;
-    if (!this._options.chart) {
-      this._options.chart = {};
+  set options(value: Highcharts.Options) {
+    this._options = value;
+    this.update();
+  }
+
+  @Input()
+  get data(): Observable<Highcharts.IndividualSeriesOptions[]> | Highcharts.IndividualSeriesOptions[] | undefined {
+    return this._options.series;
+  }
+  set data(series: Observable<Highcharts.IndividualSeriesOptions[]> | Highcharts.IndividualSeriesOptions[] | undefined) {
+
+    if (series instanceof Observable) {
+      if (this._dataSub) {
+        this._dataSub.unsubscribe();
+      }
+      console.log('data set', series);
+      this._dataSub = series.subscribe((s: Highcharts.IndividualSeriesOptions[]) => {
+        this._updateData(s);
+      });
+    } else {
+      this._updateData(series);
     }
-    if (this._options.chart.type !== value && this._chartObject) {
-      this._options.chart.type = value;
-      console.log('test', value);
-      this._chartObject.update(this._options, true);
+  }
+
+  _updateData(series: Highcharts.IndividualSeriesOptions[] | undefined): void {
+    this._options.series = series || [];
+    this.update();
+  }
+
+  update(redraw: boolean = true, oneToOne: boolean = true): void {
+    if (this._chartObject) {
+      this._chartObject.update(this._options, redraw, oneToOne);
     }
   }
 
@@ -90,7 +64,8 @@ export class DtChart implements AfterViewInit {
     this._chartObject = Highcharts.chart(this.container.nativeElement, this._options);
   }
 
-  addSeries(series: Highcharts.IndividualSeriesOptions): void {
-    this._chartObject.addSeries(series);
+  ngOnDestroy(): void {
+    this._chartObject.destroy();
+    this._dataSub.unsubscribe();
   }
 }
