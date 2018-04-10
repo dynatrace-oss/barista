bed7a20363a0f4b2f88da51906a8fe372898b5d9
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-10 14:42:46 +0200

    fix(build): moved docs build to package.json no need to have it wrapped in a gulp task

--- tools/gulp/tasks/docs.ts
+++ tools/gulp/tasks/docs.ts
@@ -4,7 +4,4 @@ import { sequenceTask } from '../util/sequence-task';
 
 task('docs', sequenceTask('library:build', ['docs:serve', 'library:watch']));
 
-// removed --prod flag until ***REMOVED*** is resolved
-task('docs:build', execNodeTask('@angular/cli', 'ng', ['build', '-a', 'docs']));
-
 task('docs:serve', execNodeTask('@angular/cli', 'ng', ['serve', '-a', 'docs']));
