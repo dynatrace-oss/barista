6de39acf3485088c0153f0d3cec8e554bfea3859
Thomas Pink <thomas.pink@dynatrace.com>
2018-04-11 12:06:42 +0200

    fix(table): added missing cdktablemodule import

--- src/lib/table/table-module.ts
+++ src/lib/table/table-module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { DtTable } from './table';
 import { DtHeaderRow, DtHeaderRowDef, DtRow, DtRowDef } from './row';
 import { DtCell, DtCellDef, DtColumnDef, DtHeaderCell, DtHeaderCellDef } from './cell';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CommonModule } from '@angular/common';
 
 const EXPORT_DECLARATIONS = [
   DtTable,
@@ -17,11 +19,12 @@ const EXPORT_DECLARATIONS = [
 ];
 
 @NgModule({
+  imports: [CdkTableModule, CommonModule],
   exports: [
-    ...EXPORT_DECLARATIONS
+    ...EXPORT_DECLARATIONS,
   ],
   declarations: [
-    ...EXPORT_DECLARATIONS
+    ...EXPORT_DECLARATIONS,
   ],
 })
 export class DtTableModule {}
