aca1bfca7fc5fa2a6eaf9ff3531ef461efa77d56
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-04-11 06:29:38 +0300

    InlineEditor: use NgModel

--- src/lib/inline-editor/inline-editor.component.ts
+++ src/lib/inline-editor/inline-editor.component.ts
@@ -1,11 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   EventEmitter,
   HostBinding,
   Input,
   Output,
+  ViewChild,
+  forwardRef,
 } from "@angular/core";
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 const MODES = {
   IDLE: 0,
@@ -20,20 +24,29 @@ const MODES = {
   selector: "[dt-inline-editor]",
   styleUrls: ["./inline-editor.component.scss"],
   template: `
-    <span *ngIf="!isEditingMode()">{{ text }}</span>
-    <input [(ngModel)]="text" *ngIf="isEditingMode()" />
-    <button type="button" *ngIf="!isEditingMode()" (click)="enterEditingMode()">edit</button>
-    <button type="button" *ngIf="isEditingMode()" (click)="saveAndQuitEditing()">save</button>
-    <button type="button" *ngIf="isEditingMode()" (click)="cancelAndQuitEditing()">cancel</button>
+    <span *ngIf="!isEditing()">{{ value }}</span>
+    <input #input
+      *ngIf="isEditing()"
+      [value]="value"
+      (change)="onChange()"
+      (keyup)="onChange()" />
+    <button type="button" *ngIf="!isEditing()" (click)="enterEditing()">edit</button>
+    <button type="button" *ngIf="isEditing()" (click)="saveAndQuitEditing()">save</button>
+    <button type="button" *ngIf="isEditing()" (click)="cancelAndQuitEditing()">cancel</button>
   `,
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => DtInlineEditor), multi: true }
+  ]
 })
-export class DtInlineEditor  {
+export class DtInlineEditor implements ControlValueAccessor {
 
-  private mode = MODES.IDLE;
+  private innerValue: string = '';
+  private changed = new Array<(value: string) => void>();
+  private touched = new Array<() => void>();
   private initialState: string;
+  private mode = MODES.IDLE;
 
-  @Input('dt-inline-editor') text: string = '';
-  @Output('dt-inline-editorChange') textChange = new EventEmitter();
+  @ViewChild('input') inputReference: ElementRef;
 
   @HostBinding("attr.disabled")
   private get disabledBinding(): true | undefined {
@@ -41,26 +54,63 @@ export class DtInlineEditor  {
     return undefined
   }
 
-  public enterEditingMode() {
+  private onChange() {
+    this.value = this.inputReference.nativeElement.value;
+  }
+
+  // Public API
+
+  public enterEditing() {
+    this.initialState = this.value;
     this.mode = MODES.EDITING;
-    this.initialState = this.text;
+    this.touch();
   }
 
   public saveAndQuitEditing() {
     this.mode = MODES.IDLE;
-    this.textChange.emit(this.text);
   }
 
   public cancelAndQuitEditing() {
-    this.text = this.initialState;
+    this.value = this.initialState;
     this.mode = MODES.IDLE;
   }
 
-  public isEditingMode() {
+  public isEditing() {
     return this.mode === MODES.EDITING;
   }
 
   public isDisabled() {
     return this.mode === MODES.DISABLED;
+  }
+
+  // Data binding
+
+  private _value: string = '';
+
+  private set value(value: string) {
+    if (this._value !== value) {
+      this._value = value
+      this.changed.forEach(f => f(value))
+    }
+  }
+
+  private get value() {
+    return this._value
+  }
+
+  private touch() {
+    this.touched.forEach(f => f());
+  }
+
+  public writeValue(value: string) {
+    this._value = value;
+  }
+
+  public registerOnChange(fn: (value: string) => void) {
+    this.changed.push(fn)
+  }
+
+  public registerOnTouched(fn: () => void) {
+    this.touched.push(fn)
   }
 }
