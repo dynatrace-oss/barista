5869329c98c59e9a93273ff61aa7f0b798947218
anonymous <anonymous@dynatrace.com>
2018-04-11 06:29:38 +0300

    InlineEditor: use NgModel

--- src/docs/components/inline-editor/docs-inline-editor.module.ts
+++ src/docs/components/inline-editor/docs-inline-editor.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { UiModule } from '../../ui/ui.module';
 import { InlineEditorModule } from '../../../lib/inline-editor/inline-editor-module';
 import { DocsInlineEditorComponent } from './docs-inline-editor.component'
 
 @NgModule({
   imports: [
-    UiModule,
-    InlineEditorModule
+    FormsModule,
+    InlineEditorModule,
+    UiModule
   ],
   declarations: [
     DocsInlineEditorComponent
