8e97e753c4aa15d28f18d5dbf8021124f9db916c
Thomas Pink <thomas.pink@me.com>
2018-04-12 16:03:06 +0200

    feat(input): examples

--- src/docs/components/input/docs-input.module.ts
+++ src/docs/components/input/docs-input.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { UiModule } from '../../ui/ui.module';
 import { CommonModule } from '@angular/common';
+import { DtButtonModule } from '@dynatrace/angular-components/button';
 import { DtInputModule } from '@dynatrace/angular-components/input';
 import { DtThemingModule } from '@dynatrace/angular-components/theming';
 import { DocsInputComponent } from './docs-input.component';
 import { DefaultInputExample } from './examples/input-default-example';
+import { NgModelInputExample } from './examples/input-ng-model-example';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { TextareaInputExample } from './examples/input-textarea-example';
+import { DisabledReadonlyInputExample } from './examples/input-disabled-readonly-example';
 
 const EXAMPLES = [
   DefaultInputExample,
+  DisabledReadonlyInputExample,
+  NgModelInputExample,
+  TextareaInputExample,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     UiModule,
+    FormsModule,
+    ReactiveFormsModule,
     DtInputModule,
+    DtButtonModule,
   ],
   declarations: [
     ...EXAMPLES,
