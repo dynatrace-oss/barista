e371cc19d5dfe2bb9d44d8c06a67a4ba605a86a7
Siyad <siyad.muhammed@dynatrace.com>
2018-04-13 19:56:47 +0530

    updating library public api to include dt-inline-editor

--- src/docs/components/inline-editor/docs-inline-editor.module.ts
+++ src/docs/components/inline-editor/docs-inline-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UiModule } from '../../ui/ui.module';
-import { InlineEditorModule } from '../../../lib/inline-editor/inline-editor-module';
+import { InlineEditorModule } from '@dynatrace/angular-components';
 import { DocsInlineEditorComponent } from './docs-inline-editor.component'
 
 @NgModule({
