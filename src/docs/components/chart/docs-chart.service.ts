7fe2b195c200f0223294537f61dc9331c828cfe6
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-17 14:26:05 +0200

    fix(docs): corrected data attribute in demo service

--- src/docs/components/chart/docs-chart.service.ts
+++ src/docs/components/chart/docs-chart.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
 import { timer } from 'rxjs/observable/timer';
 import { startWith } from 'rxjs/operators/startWith';
 import { map } from 'rxjs/operators/map';
@@ -11,7 +10,7 @@ export class ChartService {
   getStreamedChartdata(): Observable<DtChartSeries> {
     return timer(0, 5000)
     .pipe(map(() => {
-      const data = [];
+      const data: Array<[number, number]> = [];
       for (let i = 0; i < 10; i++) {
         const now = new Date();
         data.push([
@@ -25,7 +24,7 @@ export class ChartService {
       return [{
         name: 'Actions/min',
         id: `MetricId-${Math.floor(Math.random() * 100)}`,
-        series: data,
+        data,
       }];
     }));
   }
