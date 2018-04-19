8dc37c20379456393c79b3e360cba91f73e69fbf
Siyad <siyad.muhammed@dynatrace.com>
2018-04-19 15:53:31 +0530

    importing dt library button, input and spinner

--- src/lib/inline-editor/inline-editor-module.ts
+++ src/lib/inline-editor/inline-editor-module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';  
 import { DtInlineEditor } from './inline-editor.component';
+import { DtLoadingDistractorModule } from '@dynatrace/angular-components/loading-distractor';
+import { DtButtonModule } from '@dynatrace/angular-components/button';
+import { DtInputModule } from '@dynatrace/angular-components/input';
 
 @NgModule({
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    DtLoadingDistractorModule,
+    DtButtonModule,
+    DtInputModule
   ],
   exports: [
     DtInlineEditor,
