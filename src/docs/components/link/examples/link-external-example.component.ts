c2b7aa5017bbdf85a1b65b5f22f06792c480b46b
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-04-19 17:11:13 +0200

    Link docs WiP

---
+++ src/docs/components/link/examples/link-external-example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // @formatter:off
+  template: `<a class="external">External link</a>`,
+  // @formatter:on
+})
+export class LinkExternalExampleComponent {
+}
