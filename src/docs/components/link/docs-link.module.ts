007aab2e68445161b263a30194af20569f4e91a4
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-04-19 17:31:05 +0200

    Theming

--- src/docs/components/link/docs-link.module.ts
+++ src/docs/components/link/docs-link.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DtThemingModule } from '@dynatrace/angular-components/theming';
 import { UiModule } from '../../ui/ui.module';
 import { DocsLinkComponent } from './docs-link.component';
+import { LinkDarkExampleComponent } from './examples/link-dark-example.component';
 import { LinkExternalExampleComponent } from './examples/link-external-example.component';
 import { LinkSimpleExampleComponent } from './examples/link-simple-example.component';
 
 const EXAMPLES = [
   LinkSimpleExampleComponent,
   LinkExternalExampleComponent,
+  LinkDarkExampleComponent,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
+    DtThemingModule,
     UiModule,
   ],
   declarations: [
