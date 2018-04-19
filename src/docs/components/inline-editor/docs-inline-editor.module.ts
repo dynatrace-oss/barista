da09bf04f7fdb5b4223ae8a0b8d85b406e331b2d
Siyad <mail4siyadmuhammed@gmail.com>
2018-04-19 15:53:31 +0530

    importing dt library button, input and spinner

--- src/docs/components/inline-editor/docs-inline-editor.module.ts
+++ src/docs/components/inline-editor/docs-inline-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UiModule } from '../../ui/ui.module';
-import { InlineEditorModule } from '@dynatrace/angular-components';
+import { InlineEditorModule } from '@dynatrace/angular-components/inline-editor';
 import { DocsInlineEditorComponent } from './docs-inline-editor.component'
 
 @NgModule({
