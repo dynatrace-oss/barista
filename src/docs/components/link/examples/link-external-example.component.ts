db06528b582c41d5d1070bf01b78b7e40625c26f
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-04-19 17:35:37 +0200

    Styles applied only for dt-link class

--- src/docs/components/link/examples/link-external-example.component.ts
+++ src/docs/components/link/examples/link-external-example.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   // @formatter:off
-  template: `<a class="dt-external">External link</a>`,
+  template: `<a class="dt-link dt-external">External link</a>`,
   // @formatter:on
 })
 export class LinkExternalExampleComponent {
