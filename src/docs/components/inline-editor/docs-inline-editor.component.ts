c56ffa6f5d286f75147dd31cab6a31e85739bcff
anonymous <anonymous@dynatrace.com>
2018-04-23 00:48:28 +0300

    InlineEditor: Update documentation with common styling

--- src/docs/components/inline-editor/docs-inline-editor.component.ts
+++ src/docs/components/inline-editor/docs-inline-editor.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { DtInlineEditor } from '@dynatrace/angular-components';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/delay';
+import { Component } from '@angular/core';
+import { DefaultInlineEditorExample } from './examples/inline-editor-default-example';
+import { ApiInlineEditorExample } from './examples/inline-editor-api-example';
+import { SuccessfulInlineEditorExample } from './examples/inline-editor-successful-example';
+import { FailingInlineEditorExample } from './examples/inline-editor-failing-example';
 
 @Component({
   moduleId: module.id,
@@ -9,64 +10,11 @@ import 'rxjs/add/operator/delay';
   styleUrls: ['./docs-inline-editor.component.scss'],
   templateUrl: './docs-inline-editor.component.html',
 })
-<<<<<<< HEAD
-export class DocsInlineEditorComponent implements OnInit {
-  contentString = 'hello world';
-  @ViewChild('sectionTitle') sectionTitleReference: DtInlineEditor;
-
-  ngOnInit() {};
-
-  openEditor () {
-    this.sectionTitleReference.enterEditing();
-  }
-
-  saveAndCloseEditor () {
-    this.sectionTitleReference.saveAndQuitEditing();
-=======
 export class DocsInlineEditorComponent {
-  sampleModel1: string = 'Sample #1';
-  sampleModel2: string = 'Sample #2';
-  sampleModel3Successful: string = 'Sample #3: Successful';
-  sampleModel3Failing: string = 'Sample #3: Failing';
-  @ViewChild('sampleEditor2') sampleEditor2: DtInlineEditor;
-
-  successfulSave () {
-    return new Observable<void>((observer) => {
-      setTimeout(() => {
-        observer.next();
-        observer.complete();
-      }, 2e3);
-    });
-  }
-
-  failingSave () {
-    return new Observable<void>((observer) => {
-      setTimeout(() => {
-        observer.error();
-      }, 2e3);
-    });
-  }
-<<<<<<< HEAD
-
-  saveMessage = () => new Observable<void>((observer) => {
-    observer.next()
-    // observer.error()
-    observer.complete()
-  }).delay(5000);
-
-  // saveMessage = (message) => {
-  //   return new Promise(function(resolve, reject) {
-  //     console.log('async method called with text ' + message);
-  //     setTimeout(() => {
-  //       if (message) {
-  //         resolve('Stuff worked!');
-  //       } else {
-  //         reject(Error('It broke'));
-  //       }
-  //     }, 5000);
-  //   });
-  // }
+  examples = {
+    default: DefaultInlineEditorExample,
+    api: ApiInlineEditorExample,
+    successful: SuccessfulInlineEditorExample,
+    failing: FailingInlineEditorExample,
+  };
 }
-=======
-}
->>>>>>> InlinEditor: Demo page
--- src/docs/components/inline-editor/docs-inline-editor.module.ts
+++ src/docs/components/inline-editor/docs-inline-editor.module.ts
@@ -3,19 +3,34 @@ import { FormsModule } from '@angular/forms';
 import { UiModule } from '../../ui/ui.module';
 import { InlineEditorModule } from '@dynatrace/angular-components/inline-editor';
 import { DocsInlineEditorComponent } from './docs-inline-editor.component'
+import { DefaultInlineEditorExample } from './examples/inline-editor-default-example';
+import { ApiInlineEditorExample } from './examples/inline-editor-api-example';
+import { SuccessfulInlineEditorExample } from './examples/inline-editor-successful-example';
+import { FailingInlineEditorExample } from './examples/inline-editor-failing-example';
+
+const EXAMPLES = [
+  DefaultInlineEditorExample,
+  ApiInlineEditorExample,
+  SuccessfulInlineEditorExample,
+  FailingInlineEditorExample,
+]
 
 @NgModule({
   imports: [
     FormsModule,
     InlineEditorModule,
-    UiModule
+    UiModule,
   ],
   declarations: [
-    DocsInlineEditorComponent
+    ...EXAMPLES,
+    DocsInlineEditorComponent,
   ],
   exports: [
     DocsInlineEditorComponent,
-  ]
+  ],
+  entryComponents: [
+    ...EXAMPLES,
+  ],
 })
 export class DocsInlineEditorModule {
 }
