884ca1762a876e08d611583eb2d6c381fd008a17
Piotr Lewandowski <piotr.lewandowski@dynatrace.com>
2018-04-24 17:51:24 +0200

    APM-124655: docs(link): Add example with external-link on dark theme

--- src/docs/components/link/examples/link-dark-example.component.ts
+++ src/docs/components/link/examples/link-dark-example.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   template: `
     <div class="dark" dtTheme=":dark">
       <a class="dt-link">Link on a dark background</a>
+      <br />
+      <a class="dt-link dt-external">External link on a dark background</a>
     </div>
   `,
   // @formatter:on
