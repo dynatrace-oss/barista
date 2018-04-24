b9e873ee7af140aba0ce41603e409138aa0c1542
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-24 16:14:50 +0200

    fix(build): workaround try #1

---
+++ src/lib/dynatrace-angular-components-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { DtButtonModule } from './button/button-module';
+
+@NgModule({
+  exports: [
+    DtButtonModule,
+  ],
+})
+export class DtAngularComponentsModule {}
