3e2f909a0491b56bf7ddd51cc6067ecd1b506341
Thomas Pink <thomas.pink@me.com>
2018-04-24 10:04:34 +0200

    feat(viewport-resizer): Removed the emit method from the viewport-resizer. Should be implemented in the extended classes

--- src/lib/core/viewport/viewport-resizer.ts
+++ src/lib/core/viewport/viewport-resizer.ts
@@ -1,47 +1,30 @@
-import { Provider, Optional, SkipSelf } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
-import { ViewportRuler } from '@angular/cdk/scrolling';
-import { merge } from 'rxjs/operators/merge';
-import { map } from 'rxjs/operators/map';
+import {Provider, Optional, SkipSelf} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ViewportRuler} from '@angular/cdk/scrolling';
+import {map} from 'rxjs/operators/map';
 
 /** Default timeout used to throttle window resize events */
 const DEFAULT_WINDOW_EVENT_TIMEOUT = 150;
 
-/** ViewportSize object containing information about the viewport */
-export type ViewportSize = Readonly<{
-  width: number;
-  height: number;
-}>;
-
 /** Abstract class so the consumer can implement there own ViewportResizer */
 export abstract class ViewportResizer {
-  abstract emit(): void;
-  abstract change(throttleTime?: number): Observable<ViewportSize>;
+  abstract change(throttleTime?: number): Observable<void>;
 }
 
-/** Default ViewportResizer implementation */
+/** Default ViewportResizer implementation that will only react to window size changes */
 export class DefaultViewportResizer implements ViewportResizer {
-
-  private _refesher = new Subject<void>();
 
   constructor(private _viewportRuler: ViewportRuler) { }
 
-  /** emits an event that the viewport resized */
-  emit(): void {
-    this._refesher.next();
-  }
-
   /** Returns a stream that emits whenever the size of the viewport changes. */
-  change(throttleTime: number = DEFAULT_WINDOW_EVENT_TIMEOUT): Observable<ViewportSize> {
+  change(throttleTime: number = DEFAULT_WINDOW_EVENT_TIMEOUT): Observable<void> {
     return this._viewportRuler.change(throttleTime)
-      .pipe(merge(this._refesher))
-      .pipe(map(() => this._viewportRuler.getViewportSize()));
+      .pipe(map(() => void 0));
   }
 }
 
 /** ViewportResizer Factory to ensure a singleton, can set the timeout for window resize events */
-function VIEWPORT_RESIZER_FACTORY(
+function DEFAULT_VIEWPORT_RESIZER_FACTORY(
   viewportRuler: ViewportRuler,
   parentResizer?: ViewportResizer
 ): ViewportResizer {
@@ -51,6 +34,6 @@ function VIEWPORT_RESIZER_FACTORY(
 /** Default provider */
 export const DEFAULT_VIEWPORT_RESIZER_PROVIDER: Provider = {
   provide: ViewportResizer,
-  useFactory: VIEWPORT_RESIZER_FACTORY,
+  useFactory: DEFAULT_VIEWPORT_RESIZER_FACTORY,
   deps: [ViewportRuler, [new Optional(), new SkipSelf(), ViewportResizer]],
 };
