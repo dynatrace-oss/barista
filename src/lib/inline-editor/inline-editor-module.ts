fd1aaeccac388763fefd02f738fcd9fd4a04091d
jmasramon <jordi.masramon@dynatrace.com>
2018-04-26 16:58:20 +0200

    solved some linting problems

--- src/lib/inline-editor/inline-editor-module.ts
+++ src/lib/inline-editor/inline-editor-module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DtInlineEditor } from './inline-editor.component';
-import { DtInputModule } from '../input/input-module'
-import { DtButtonModule } from '../button/button-module'
-import { DtLoadingDistractorModule } from '../loading-distractor/loading-distractor-module'
+import { DtInputModule } from '../input/input-module';
+import { DtButtonModule } from '../button/button-module';
+import { DtLoadingDistractorModule } from '../loading-distractor/loading-distractor-module';
 
 @NgModule({
   imports: [
