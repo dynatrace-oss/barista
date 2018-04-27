3be3fba3c0661017b05a0c8562cf3b046ae7843d
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-27 15:38:31 +0200

    docs(card): added card examples

--- src/docs/components/card/docs-card.component.ts
+++ src/docs/components/card/docs-card.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { DefaultCardExampleComponent } from './examples/card-default-example.component';
+import { DefaultCardExampleComponent } from './examples/default-card-example.component';
+import { ContentOnlyCardExampleComponent } from './examples/contentonly-card-example.component';
+import { TitleCardExampleComponent } from './examples/title-card-example.component';
+import { SubtitleCardExampleComponent } from './examples/subtitle-card-example.component';
+import { ActionButtonsCardExampleComponent } from './examples/action-buttons-card-example.component';
 
 @Component({
   moduleId: module.id,
   selector: 'docs-card',
-  styleUrls: ['./docs-card.component.scss'],
   templateUrl: './docs-card.component.html',
 })
 export class DocsCardComponent {
 
   examples = {
     default: DefaultCardExampleComponent,
+    contentonly: ContentOnlyCardExampleComponent,
+    title: TitleCardExampleComponent,
+    subtitle: SubtitleCardExampleComponent,
+    actions: ActionButtonsCardExampleComponent,
   };
 }
---
+++ src/docs/components/card/examples/default-card-example.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  styles: ['.demo-card { background-color: #f2f2f2; padding: 1rem; }'],
+  template: `<div class="demo-card">
+<dt-card>
+  <dt-card-title>Top 3 JavaScript errors</dt-card-title>
+  <dt-card-subtitle>Some subtitle</dt-card-subtitle>
+  <dt-card-actions><button dt-button variant="secondary">Some Action</button></dt-card-actions>
+  The card is not an interactive element, therefore, there are no hover, active and disabled card.
+</dt-card></div>`,
+})
+export class DefaultCardExampleComponent { }
