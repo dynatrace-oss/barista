61435fff47b62da3b2465d97a60d2d3424ea6b54
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-04-30 13:29:54 +0200

    feat(tile): tile initial

---
+++ src/lib/tile/tile-module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  DtTile,
+  DtTileTitle,
+  DtTileSubtitle,
+  DtTileIcon
+} from './tile';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [
+    DtTile,
+    DtTileTitle,
+    DtTileSubtitle,
+    DtTileIcon,
+  ],
+  declarations: [
+    DtTile,
+    DtTileTitle,
+    DtTileSubtitle,
+    DtTileIcon,
+  ],
+})
+export class DtTileModule { }
---
+++ src/lib/tile/tile.ts
@@ -0,0 +1,94 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  Directive,
+  ContentChild,
+  Input,
+  ElementRef
+} from '@angular/core';
+import {
+  CanDisable,
+  mixinDisabled,
+  HasTabIndex,
+  mixinTabIndex,
+  HasElementRef,
+  CanColor,
+  mixinColor
+} from '../core/index';
+
+/** Title of a tile, needed as it's used as a selector in the API. */
+@Directive({
+  selector: `dt-tile-title, [dt-tile-title], [dtTileTitle]`,
+  host: {
+    class: 'dt-tile-title',
+  },
+})
+export class DtTileTitle { }
+
+/** Icon of a tile, needed as it's used as a selector in the API. */
+@Directive({
+  selector: `dt-tile-icon, [dt-tile-icon], [dtTileIcon]`,
+  host: {
+    'class': 'dt-tile-icon',
+    '[class.dt-tile-icon-end]': 'align === "end"',
+  },
+})
+export class DtTileIcon {
+  @Input() align: 'start' | 'end' = 'start';
+}
+
+/** Sub-title of a tile, needed as it's used as a selector in the API. */
+@Directive({
+  selector: `dt-tile-subtitle, [dt-tile-subtitle], [dtTileSubtitle]`,
+  host: {
+    class: 'dt-tile-subtitle',
+  },
+})
+export class DtTileSubtitle { }
+
+// Boilerplate for applying mixins to DtTile.
+export class DtTileBase {
+  constructor(public _elementRef: ElementRef) { }
+}
+export const _DtTileMixinBase = mixinTabIndex(mixinDisabled(mixinColor(DtTileBase)));
+
+@Component({
+  moduleId: module.id,
+  selector: 'dt-tile',
+  exportAs: 'dtTile',
+  templateUrl: 'tile.html',
+  styleUrls: ['tile.scss'],
+  inputs: ['disabled', 'tabIndex', 'color'],
+  host: {
+    'role': 'button',
+    '[attr.tabindex]': 'tabIndex',
+    '[attr.aria-disabled]': 'disabled.toString()',
+    'class': 'dt-tile',
+    '[class.dt-tile-small]': '!_subTitle',
+    '[class.dt-tile-disabled]': 'disabled',
+    '(click)': '_haltDisabledEvents($event)',
+  },
+  // tslint:disable-next-line:use-view-encapsulation
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DtTile extends _DtTileMixinBase
+  implements CanDisable, HasElementRef, CanColor, HasTabIndex {
+
+  @ContentChild(DtTileSubtitle) _subTitle: DtTileSubtitle;
+  @ContentChild(DtTileIcon) _icon: DtTileIcon;
+
+  constructor(elementRef: ElementRef) {
+    super(elementRef);
+  }
+
+  _haltDisabledEvents(event: Event): void {
+    // A disabled tile shouldn't apply any actions
+    if (this.disabled) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    }
+  }
+}
