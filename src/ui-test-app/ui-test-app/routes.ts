dd5f1c079f481dfc69ad25ea65b9b01c59efae50
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-05-02 13:48:30 +0200

    test(context-dialog): added universal and ui-tests

--- src/ui-test-app/ui-test-app/routes.ts
+++ src/ui-test-app/ui-test-app/routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './ui-test-app';
 import { ButtonUI } from '../button/button-ui';
-import {ButtonGroupUi} from "../button-group/button-group-ui";
+import { ButtonGroupUi } from '../button-group/button-group-ui';
 import { ExpandableSectionUi } from '../expandable-section/expandable-section-ui';
-import {ExpandablePanelUi} from '../expandable-panel/expandable-panel-ui';
+import { ExpandablePanelUi } from '../expandable-panel/expandable-panel-ui';
 import { TileUI } from '../tile/tile-ui';
+import { ContextDialogUI } from '../context-dialog/context-dialog-ui';
 
 export const UI_TEST_APP_ROUTES: Routes = [
   { path: '', component: Home },
   { path: 'button', component: ButtonUI },
   { path: 'button-group', component: ButtonGroupUi },
+  { path: 'context-dialog', component: ContextDialogUI },
   { path: 'expandable-panel', component: ExpandablePanelUi },
   { path: 'expandable-section', component: ExpandableSectionUi },
   { path: 'tile', component: TileUI },
