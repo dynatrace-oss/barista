8fe90e65034476aa20ebf26e386c097a2bf6bb8b
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-05-02 13:48:30 +0200

    test(context-dialog): added universal and ui-tests

---
+++ ui-tests/context-dialog-ui.spec.ts
@@ -0,0 +1,41 @@
+import {browser, by, element, ExpectedConditions, Key } from 'protractor';
+
+describe('context-dialog', () => {
+  beforeEach(() => browser.get('/context-dialog'));
+
+  describe('disabling behavior', () => {
+
+    it('should open the context dialog when not disabled', async () => {
+      element(by.id('context-dialog')).click();
+      expect(await browser.isElementPresent(by.css('.dt-context-dialog-panel'))).toBeTruthy();
+    });
+
+    it('should not execute click handlers when disabled', async () => {
+      element(by.id('disable-toggle')).click();
+      await element(by.id('context-dialog')).click();
+      expect(await browser.isElementPresent(by.css('.dt-context-dialog-panel'))).toBeFalsy();
+    });
+  });
+
+  describe('focus behaviour', () => {
+    it('should trap the focus inside the overlay', async () => {
+      element(by.id('context-dialog')).click();
+      await browser.isElementPresent(by.css('.dt-context-dialog-panel'));
+      expect(await browser.driver.switchTo().activeElement().getText()).toEqual('Edit');
+      await browser.actions().sendKeys(Key.TAB).perform();
+      expect(await browser.driver.switchTo().activeElement().getAttribute('aria-label'))
+        .toEqual('close');
+      await browser.actions().sendKeys(Key.TAB).perform();
+      expect(await browser.driver.switchTo().activeElement().getText()).toEqual('Edit');
+    });
+  });
+
+  describe('close behaviour', () => {
+    it('should open and close the context dialog', async () => {
+      await element(by.id('context-dialog')).click();
+      await browser.isElementPresent(by.css('.dt-context-dialog-panel'));
+      await element(by.css('.dt-context-dialog-close-trigger')).click();
+      expect(await browser.isElementPresent(by.css('.dt-context-dialog-panel'))).toBeFalsy();
+    });
+  });
+});
