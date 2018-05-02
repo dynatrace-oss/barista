230be92199ec20af47e5ca24f679abba2025e466
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-05-02 13:13:25 +0200

    feat(context-dialog): added tests, fixed styles

--- src/docs/components/context-dialog/docs-context-dialog.component.ts
+++ src/docs/components/context-dialog/docs-context-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { DefaultContextDialogExampleComponent } from './examples/default-context-dialog-example.component';
+import { DarkContextDialogExampleComponent } from './examples/dark-context-dialog-example.component';
+import { FocusContextDialogExampleComponent } from './examples/focus-context-dialog-example.component';
+import { PrevFocusContextDialogExampleComponent } from './examples/previous-focus-context-dialog-example.component';
 
 @Component({
   moduleId: module.id,
@@ -10,5 +13,8 @@ export class DocsContextDialogComponent {
 
   examples = {
     default: DefaultContextDialogExampleComponent,
+    dark: DarkContextDialogExampleComponent,
+    focus: FocusContextDialogExampleComponent,
+    prevFocus: PrevFocusContextDialogExampleComponent,
   };
 }
---
+++ src/docs/components/context-dialog/examples/previous-focus-context-dialog-example.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { DtButton, DtContextDialog } from '@dynatrace/angular-components';
+
+@Component({
+  moduleId: module.id,
+  template: `
+  <button dt-button variant="secondary" (click)="open()" #focusme>Open</button>
+  <dt-context-dialog #contextdialog>
+  <p>Close me to return the focus to the "Focus me" button</p>
+  <button dt-button variant="secondary">Focused</button>
+  </dt-context-dialog>`,
+})
+export class PrevFocusContextDialogExampleComponent {
+  @ViewChild('focusme') focusMe: DtButton;
+  @ViewChild('contextdialog') contextdialog: DtContextDialog;
+
+  open(): void {
+    this.contextdialog.open();
+  }
+}
