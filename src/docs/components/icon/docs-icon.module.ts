2dea584081dd843a18714d3901639fd365174680
Thomas Pink <thomas.pink@me.com>
2018-05-07 16:31:42 +0200

    fix(icon): docs module import

--- src/docs/components/icon/docs-icon.module.ts
+++ src/docs/components/icon/docs-icon.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { UiModule } from '../../ui/ui.module';
 import { CommonModule } from '@angular/common';
-import { DtIconModule } from '@dynatrace/angular-components/icon';
+import { DtIconModule } from '@dynatrace/angular-components';
 import { DocsIconComponent } from './docs-icon.component';
 import { DefaultIconExample } from './examples/icon-default-example';
 import { HttpClientModule } from '@angular/common/http';
