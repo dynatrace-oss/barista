9a08d79f2817b0e56d76b97a5fc6de2e23cfdb62
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-05-08 09:21:40 +0200

    feat(schematics): added first schematic

---
+++ src/schematics/utils/change.ts
@@ -0,0 +1,62 @@
+import { Tree } from '@angular-devkit/schematics';
+
+export interface Host {
+  write(path: string, content: string): Promise<void>;
+  read(path: string): Promise<string>;
+}
+
+export interface Change {
+  apply(host: Host): Promise<void>;
+
+  // The file this change should be applied to. Some changes might not apply to
+  // a file (maybe the config).
+  readonly path: string | null;
+
+  // The order this change should be applied. Normally the position inside the file.
+  // Changes are applied from the bottom of a file to the top.
+  readonly order: number;
+
+  // The description of this change. This will be outputted in a dry or verbose run.
+  readonly description: string;
+}
+
+/**
+ * Will add text to the source code.
+ */
+export class InsertChange implements Change {
+
+  order: number;
+  description: string;
+
+  constructor(public path: string, public pos: number, public toAdd: string) {
+    if (pos < 0) {
+      throw new Error('Negative positions are invalid');
+    }
+    this.description = `Inserted ${toAdd} into position ${pos} of ${path}`;
+    this.order = pos;
+  }
+
+  /**
+   * This method does not insert spaces if there is none in the original string.
+   */
+  apply(host: Host): Promise<void> {
+    return host.read(this.path).then((content) => {
+      const prefix = content.substring(0, this.pos);
+      const suffix = content.substring(this.pos);
+
+      return host.write(this.path, `${prefix}${this.toAdd}${suffix}`);
+    });
+  }
+}
+
+/**
+ * Commit changes to host
+ */
+export function commitChanges(host: Tree, changes: InsertChange[], path: string): Tree {
+  const recorder = host.beginUpdate(path);
+  changes.forEach((change) => {
+    recorder.insertLeft(change.pos, change.toAdd);
+  });
+  host.commitUpdate(recorder);
+  return host;
+}
