f72bb8e37dacd7165991dc46d620859ac3f17028
Thomas Pink <thomas.pink@me.com>
2018-05-11 09:38:49 +0200

    APM-128212 feat(button): icon button

--- src/docs/components/button/docs-button.component.ts
+++ src/docs/components/button/docs-button.component.ts
@@ -6,6 +6,7 @@ import { VariantButtonExampleComponent } from './examples/button-variant-example
 import { ColorButtonExampleComponent } from './examples/button-color-example.component';
 import { DefaultButtonExampleComponent } from './examples/button-default-example.component';
 import { IconsButtonExampleComponent } from './examples/button-icons-example.component';
+import { IconOnlyButtonExampleComponent } from './examples/button-icon-only-example.component';
 
 @Component({
   moduleId: module.id,
@@ -22,6 +23,7 @@ export class DocsButtonComponent {
     variant: VariantButtonExampleComponent,
     color: ColorButtonExampleComponent,
     icons: IconsButtonExampleComponent,
+    iconOnly: IconOnlyButtonExampleComponent,
     all: AllButtonExampleComponent,
   };
 }
