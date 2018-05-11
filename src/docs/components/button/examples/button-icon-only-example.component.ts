cac3cc75a1d3fa4f84924b32900ad0652a8d0bb9
Thomas Pink <thomas.pink@dynatrace.com>
2018-05-11 09:38:49 +0200

    APM-128212 feat(button): icon button

--- src/docs/components/button/examples/button-icon-only-example.component.ts
+++ src/docs/components/button/examples/button-icon-only-example.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-    <button dt-icon-button><dt-icon name="agent"></dt-icon>With icon</button>
+    <button dt-icon-button><dt-icon name="agent"></dt-icon></button>
     <button dt-icon-button variant="secondary"><dt-icon name="agent"></dt-icon></button>
   `,
 })
