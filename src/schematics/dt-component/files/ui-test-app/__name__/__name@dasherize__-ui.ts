720b40fde9f1a96f1d99617869b0c990f20a88c1
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-05-14 13:16:41 +0200

    feat(devkit): added universal capabilities

---
+++ src/schematics/dt-component/files/ui-test-app/__name__/__name@dasherize__-ui.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'dt-<%=dasherize(name)%>-ui',
+  templateUrl: '<%=dasherize(name)%>-ui.html',
+})
+export class <%= classify(name) %>UI {
+
+}
