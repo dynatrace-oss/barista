e3bcb2762f98cd901a1a7bd09756acf51c0499e9
Daniel Kaneider <daniel.kaneider@dynatrace.com>
2018-05-22 09:03:26 +0200

    added example for value type

--- src/docs/components/tag/examples/interactive-tag-example.component.ts
+++ src/docs/components/tag/examples/interactive-tag-example.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import { DtTag } from '@dynatrace/angular-components';
 
 @Component({
   moduleId: module.id,
   template: `
 <div>
-  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive">
+  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive" [value]="value1" (removed)="doRemove(tag1)" #tag1>
     <dt-tag-key *ngIf="hasKey">[AWS]OSType:</dt-tag-key>
     Windows
   </dt-tag>
-  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive">
+  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive" [value]="value2" (removed)="doRemove(tag2)" #tag2>
     <dt-tag-key *ngIf="hasKey">[AWS]Category:</dt-tag-key>
     Managed
   </dt-tag>
@@ -19,4 +20,12 @@ import { Component } from '@angular/core';
 <button dt-button (click)="hasKey=!hasKey" [variant]="hasKey ? 'primary' : 'secondary'">Toggle key</button>
   `,
 })
-export class InteractiveTagExampleComponent { }
+export class InteractiveTagExampleComponent {
+
+  value1 = 'My value 1';
+  value2 = 'My value 2';
+
+  doRemove(tag: DtTag<string>): void {
+    window.alert('Tag removed: ' + tag.value);
+  }
+}
