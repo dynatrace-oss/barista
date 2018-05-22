6a01e79ad4f3bd773141364a787cc3e917df2dc7
Thomas Pink <thomas.pink@dynatrace.com>
2018-05-22 13:18:11 +0200

    fix(form-field): fixes multiple styling issues with icons and buttons

--- src/lib/form-field/prefix.ts
+++ src/lib/form-field/prefix.ts
@@ -3,5 +3,8 @@ import { Directive } from '@angular/core';
 /** Prefix to be placed the the front of the form field. */
 @Directive({
   selector: '[dtPrefix]',
+  host: {
+    class: 'dt-prefix',
+  },
 })
 export class DtPrefix { }
