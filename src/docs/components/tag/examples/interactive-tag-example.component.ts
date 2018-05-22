9e5525646d8769d7a5480d462fde7bf035f2b16e
Daniel Kaneider <daniel.kaneider@dynatrace.com>
2018-05-22 16:51:40 +0200

    removed interactive mode, added some focus handling

--- src/docs/components/tag/examples/interactive-tag-example.component.ts
+++ src/docs/components/tag/examples/interactive-tag-example.component.ts
@@ -5,18 +5,17 @@ import { DtTag } from '@dynatrace/angular-components';
   moduleId: module.id,
   template: `
 <div>
-  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive" [value]="value1" (removed)="doRemove(tag1)" #tag1>
+  <dt-tag [removable]="canRemove" [disabled]="disabled" [value]="value1" (removed)="doRemove(tag1)" #tag1 title="My custom tooltip">
     <dt-tag-key *ngIf="hasKey">[AWS]OSType:</dt-tag-key>
     Windows
   </dt-tag>
-  <dt-tag [removable]="canRemove" [disabled]="disabled" [interactive]="interactive" [value]="value2" (removed)="doRemove(tag2)" #tag2>
+  <dt-tag [removable]="canRemove" [disabled]="disabled" [value]="value2" (removed)="doRemove(tag2)" #tag2 title="Another tooltip">
     <dt-tag-key *ngIf="hasKey">[AWS]Category:</dt-tag-key>
     Managed
   </dt-tag>
 </div>
 <button dt-button (click)="canRemove=!canRemove" [variant]="canRemove ? 'primary' : 'secondary'">Toggle removable</button>
 <button dt-button (click)="disabled=!disabled" [variant]="disabled ? 'primary' : 'secondary'">Toggle disabled</button>
-<button dt-button (click)="interactive=!interactive" [variant]="interactive ? 'primary' : 'secondary'">Toggle interactive</button>
 <button dt-button (click)="hasKey=!hasKey" [variant]="hasKey ? 'primary' : 'secondary'">Toggle key</button>
   `,
 })
