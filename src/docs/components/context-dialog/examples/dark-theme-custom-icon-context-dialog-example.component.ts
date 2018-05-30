2f11c93ce9ae20e42a6711bf8ced0f35472136e9
Alexander Frass <alexander.frass@dynatrace.com>
2018-05-30 15:39:19 +0200

    APM-130540 Context dialog without defult button implementation

--- src/docs/components/context-dialog/examples/dark-theme-custom-icon-context-dialog-example.component.ts
+++ src/docs/components/context-dialog/examples/dark-theme-custom-icon-context-dialog-example.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <div class="dark" dtTheme=":dark">
-  <dt-context-dialog>
-  <dt-icon dtContextDialogIcon name="agent" color="light"></dt-icon>
-  <div>Example Content</div>
-  </dt-context-dialog>
-  </div>`,
+ `,
 })
 export class DarkThemeCustomIconContextDialogExampleComponent { }
