05db6eb42fee664182d24c599c06337a897d91c9
anonymous <anonymous@dynatrace.com>
2018-05-30 15:39:19 +0200

    APM-130540 Context dialog without defult button implementation

--- src/docs/components/context-dialog/examples/default-context-dialog-example.component.ts
+++ src/docs/components/context-dialog/examples/default-context-dialog-example.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <button dt-button variant="secondary">Copy to dashboard</button>
-  <dt-context-dialog>
+  <button dt-icon-button [dtContextDialogTrigger]="dialog"><dt-icon name="agent"></dt-icon></button>
+  <dt-context-dialog #dialog>
   <p>Your dashboard "real user monitoring"<br> is only visible to you</p>
-  <button dt-button variant="secondary">Share</button>
   </dt-context-dialog>`,
 })
 export class DefaultContextDialogExampleComponent { }
