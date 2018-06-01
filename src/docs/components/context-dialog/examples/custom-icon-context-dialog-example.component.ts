412283f7e92e782f97cbc53624aec57e0609cfa7
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-06-01 09:23:17 +0200

    APM-130540 feature(context-dialog): add custom trigger support

--- src/docs/components/context-dialog/examples/custom-icon-context-dialog-example.component.ts
+++ src/docs/components/context-dialog/examples/custom-icon-context-dialog-example.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
+  <button dt-icon-button [dtContextDialogTrigger]="dialog" variant="nested"><dt-icon name="agent"></dt-icon></button>
+  <dt-context-dialog #dialog>
+  <p>Your dashboard "real user monitoring"<br> is only visible to you</p>
+  </dt-context-dialog>
    `,
 })
 export class CustomIconContextDialogExampleComponent {
-  name = 'agent';
 }
--- src/docs/components/context-dialog/examples/default-context-dialog-example.component.ts
+++ src/docs/components/context-dialog/examples/default-context-dialog-example.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <button dt-icon-button [dtContextDialogTrigger]="dialog"><dt-icon name="agent"></dt-icon></button>
+  <button *ngIf="customTrigger" dt-icon-button [dtContextDialogTrigger]="dialog"><dt-icon name="agent"></dt-icon></button>
   <dt-context-dialog #dialog>
   <p>Your dashboard "real user monitoring"<br> is only visible to you</p>
-  </dt-context-dialog>`,
+  </dt-context-dialog>
+  <button dt-button (click)="customTrigger=!customTrigger">Toggle custom trigger</button>`,
 })
-export class DefaultContextDialogExampleComponent { }
+export class DefaultContextDialogExampleComponent {
+  customTrigger: true;
+}
+
