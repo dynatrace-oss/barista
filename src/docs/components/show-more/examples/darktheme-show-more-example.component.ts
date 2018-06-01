5d9d97e4dea2e31234181b5d2b3305db8b9da19b
Daniel Kaneider <daniel.kaneider@dynatrace.com>
2018-06-01 08:12:52 +0200

    tests, cleanup

--- src/docs/components/show-more/examples/darktheme-show-more-example.component.ts
+++ src/docs/components/show-more/examples/darktheme-show-more-example.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: '<section class="dark" dtTheme=":dark"><dt-show-more [showLess]="showLess" (changed)="showLess=!showLess">Toggle more/less</dt-show-more></section>',
+  template: `<section class="dark" dtTheme=":dark">
+  <dt-show-more [showLess]="showLess" (changed)="showLess=!showLess">Toggle more/less</dt-show-more>
+</section>`,
 })
 export class DarkThemeShowMoreExampleComponent { }
