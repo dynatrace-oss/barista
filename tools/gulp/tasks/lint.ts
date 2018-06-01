f4dafa768651dc56ea1f1d75d04485de3f072eeb
anonymous <anonymous@dynatrace.com>
2018-06-01 12:29:03 +0200

    APM-131331 Enable spec files linting
    * added linting for ui-tests, universal-app and ui-test-app

--- tools/gulp/tasks/lint.ts
+++ tools/gulp/tasks/lint.ts
@@ -7,8 +7,19 @@ const stylesGlob = 'src/lib/**/!(*.bundle).+(css|scss)';
 const tsGlob = 'src/lib/**/!(*.spec).ts';
 const tsSpecsGlob = 'src/lib/**/*.spec.ts';
 const tsDocs = 'src/docs/**/!(*.spec).ts';
+const tsUiTestApp = 'src/ui-test-app/**/!(*.spec).ts';
+const tsUniversalApp = 'src/universal-app/**/!(*.spec).ts';
+const tsUiSpecsGlob = 'ui-tests/**/*.spec.ts';
 
-task('lint', ['stylelint', 'tslint', 'tslint:specs', 'tslint:docs']);
+task('lint', [
+  'stylelint',
+  'tslint',
+  'tslint:specs',
+  'tslint:docs',
+  'tslint:ui-test-app',
+  'tslint:universal-app',
+  'tslint:ui-tests',
+]);
 
 task('stylelint', execNodeTask(
   'stylelint', [stylesGlob, '--config', 'stylelint-config.json', '--syntax', 'scss'],
@@ -24,4 +35,16 @@ task('tslint:specs', execNodeTask(
 
 task('tslint:docs', execNodeTask(
   'tslint', ['--project', 'src/docs/tsconfig.json', tsDocs],
+));
+
+task('tslint:ui-test-app', execNodeTask(
+  'tslint', ['--project', 'src/ui-test-app/tsconfig.json', tsUiTestApp],
+));
+
+task('tslint:universal-app', execNodeTask(
+  'tslint', ['--project', 'src/universal-app/tsconfig.json', tsUniversalApp],
+));
+
+task('tslint:ui-tests', execNodeTask(
+  'tslint', ['--config', 'tslint.spec.json', '--project', 'ui-tests/tsconfig.json', tsUiSpecsGlob],
 ));
