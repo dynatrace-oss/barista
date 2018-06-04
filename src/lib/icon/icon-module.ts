3c8acd7733e66dbf14970769048b6ab036815b58
Thomas Pink <thomas.pink@me.com>
2018-06-04 14:39:55 +0200

    feat(icon): Icon registry provider using angular 6

--- src/lib/icon/icon-module.ts
+++ src/lib/icon/icon-module.ts
@@ -1,12 +1,12 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { DT_ICON_CONFIGURATION, DtIconConfiguration } from './icon-config';
-import { DT_ICON_REGISTRY_PROVIDER } from './icon-registry';
+import { DtIconRegistry } from './icon-registry';
 import { DtIcon } from './icon';
 
 @NgModule({
   exports: [DtIcon],
   declarations: [DtIcon],
-  providers: [DT_ICON_REGISTRY_PROVIDER],
+  providers: [DtIconRegistry],
 })
 export class DtIconModule {
   static forRoot(config: DtIconConfiguration): ModuleWithProviders {
@@ -14,7 +14,7 @@ export class DtIconModule {
       ngModule: DtIconModule,
       providers: [
         { provide: DT_ICON_CONFIGURATION, useValue: config },
-        DT_ICON_REGISTRY_PROVIDER,
+        DtIconRegistry,
       ],
     };
   }
