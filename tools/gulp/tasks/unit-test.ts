d5baa1af208934a3a676f6c7388e24f0568da502
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-06-05 10:31:08 +0200

    APM-132837 fix(build): add clean tasks for ui & unit-tests

--- tools/gulp/tasks/unit-test.ts
+++ tools/gulp/tasks/unit-test.ts
@@ -12,7 +12,7 @@ const defaultOptions = {
   singleRun: false,
 };
 
-task('test:build', process.env.SKIP_BUILD === 'true'? []: ['library:build'], (done: ((err?: any) => void)) => {
+task('test:build', process.env.SKIP_BUILD === 'true' ? [] : ['clean:unit-test', 'library:build'], (done) => {
   const tsConfig = join(buildConfig.libDir, 'tsconfig-test.json');
   ngcCompile(['-p', tsConfig])
   .catch(() => {
