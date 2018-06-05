a809e20ee9d2d1db0a0ffae85a62c2e3028363d2
Thomas Pink <thomas.pink@me.com>
2018-06-05 16:35:37 +0200

    APM-130434 fix(checkbox): feedback

--- src/docs/components/checkbox/docs-checkbox.component.ts
+++ src/docs/components/checkbox/docs-checkbox.component.ts
@@ -7,7 +7,6 @@ import { DarkCheckboxExample } from './examples/dark-checkbox-example';
   moduleId: module.id,
   selector: 'docs-checkbox',
   templateUrl: 'docs-checkbox.component.html',
-  styleUrls: ['docs-checkbox.component.scss'],
 })
 export class DocsCheckboxComponent {
   examples = {
