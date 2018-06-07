eca1ce776cf66a782713207d350355060486b495
Alexander Frass <alexander.frass@dynatrace.com>
2018-06-07 15:52:54 +0200

    checked not working

---
+++ src/lib/switch/switch-module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {DtSwitch} from './switch';
+
+@NgModule({
+  exports: [
+    DtSwitch,
+  ],
+  declarations: [
+    DtSwitch,
+  ],
+})
+export class DtSwitchModule {}
