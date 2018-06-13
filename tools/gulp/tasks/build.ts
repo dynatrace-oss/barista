5f72d0e221f906c7d31170296f6734287958af3e
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-06-13 12:34:08 +0200

    APM-133000 fix(build): added styles, assets and removeModuleId back for correct bundle

--- tools/gulp/tasks/build.ts
+++ tools/gulp/tasks/build.ts
@@ -4,6 +4,8 @@ import { buildConfig } from '../build-config';
 import * as through from 'through2';
 import { replaceVersionPlaceholders } from '../util/replace-version-placeholder';
 import { execNodeTask } from '../util/task-runner';
+import { sequenceTask } from '../util/sequence-task';
+import { parseDir } from 'sass-graph';
 
 task('library:version-replace', replaceVersionPlaceholders);
 
@@ -19,4 +21,29 @@ const removeModuleId = () => through.obj((file, _, callback) => {
   callback(null, file);
 });
 
+task('library:removeModuleId', () =>
+  src(join(buildConfig.libOutputDir, '/**/*.js'))
+  .pipe(removeModuleId())
+  .pipe(dest(buildConfig.libOutputDir)));
+
+task('library:styles', () => {
+  const stylesGraph = parseDir(join(buildConfig.libDir, 'style'));
+
+  return src(Object.keys(stylesGraph.index), {base: buildConfig.libDir})
+    .pipe(dest(buildConfig.libOutputDir));
+});
+
+task('library:assets', () =>
+  src(join(buildConfig.libDir, 'assets/**'))
+    .pipe(dest(join(buildConfig.libOutputDir, 'assets')))
+);
+
 task('library:compile', execNodeTask('@angular/cli', 'ng', ['build', 'lib']));
+
+task('library:build', sequenceTask(
+  'library:compile',
+  'library:styles',
+  'library:assets',
+  'library:version-replace',
+  'library:removeModuleId'
+));
