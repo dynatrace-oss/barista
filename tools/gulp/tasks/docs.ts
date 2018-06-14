e4220b520cb9959cd0dc424e8da606597fec7adf
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-06-14 13:34:46 +0200

    APM-133000 fix(tools): remove unused docs tasks

--- tools/gulp/tasks/docs.ts
+++
@@ -1,7 +0,0 @@
-import { task } from 'gulp';
-import { execNodeTask } from '../util/task-runner';
-import { sequenceTask } from '../util/sequence-task';
-
-task('docs', sequenceTask('library:compile', 'docs:serve'));
-
-task('docs:serve', execNodeTask('@angular/cli', 'ng', ['serve', 'docs']));
