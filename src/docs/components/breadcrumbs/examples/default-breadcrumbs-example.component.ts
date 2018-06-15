591c4f16fbcbc7191a1dcaeb8ee58654e5f63195
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-06-15 17:48:56 +0200

    APM-131837 Breadcrumbs - functionality to set active state explicitly

--- src/docs/components/breadcrumbs/examples/default-breadcrumbs-example.component.ts
+++ src/docs/components/breadcrumbs/examples/default-breadcrumbs-example.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   template: `
@@ -7,15 +6,11 @@ import { Router } from '@angular/router';
       <dt-breadcrumbs-item href="first">First view</dt-breadcrumbs-item>
       <dt-breadcrumbs-item [href]="['first', 'second', {param: 123}]">Second view</dt-breadcrumbs-item>
       <dt-breadcrumbs-item [href]="['first', 'second', {param: 123}, 'third']">Third view</dt-breadcrumbs-item>
-      <dt-breadcrumbs-item [href]="currentUrl">Current view</dt-breadcrumbs-item>
+      <dt-breadcrumbs-item [href]="['first', 'second', {param: 123}, 'third', 'current']" [active]="true">
+        Current view
+      </dt-breadcrumbs-item>
     </dt-breadcrumbs>
   `,
 })
 export class DefaultBreadcrumbsExampleComponent {
-
-  constructor(private readonly router: Router) {}
-
-  get currentUrl(): string {
-    return this.router.url;
-  }
 }
