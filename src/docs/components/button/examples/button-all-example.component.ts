5a9df974f1524863b13cc6e9b6b8a41f3cc0853d
Thomas Pink <thomas.pink@me.com>
2018-06-15 10:00:37 +0200

    fix(button): stylefix docs

--- src/docs/components/button/examples/button-all-example.component.ts
+++ src/docs/components/button/examples/button-all-example.component.ts
@@ -58,6 +58,7 @@ import { Component } from '@angular/core';
       display: inline-block;
       min-width: 160px;
     }
+    .dt-button + .dt-button { margin-left: 12px; }
   `],
 })
 export class AllButtonExampleComponent {
