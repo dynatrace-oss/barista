428abf1906c79423459a5707bfb191e8a9a1a08e
Thomas Pink <thomas.pink@me.com>
2018-06-15 10:00:37 +0200

    fix(button): stylefix docs

--- src/docs/components/button/examples/button-icon-only-example.component.ts
+++ src/docs/components/button/examples/button-icon-only-example.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
     <button dt-icon-button variant="secondary"><dt-icon name="agent"></dt-icon></button>
     <button dt-icon-button variant="nested"><dt-icon name="agent"></dt-icon></button>
   `,
+  styles: ['.dt-button + .dt-button { margin-left: 12px; }'],
 })
 export class IconOnlyButtonExampleComponent { }
