e35208c76e8f8358d7ad5688e882c6c211b0d030
Thomas Pink <thomas.pink@me.com>
2018-06-15 10:22:12 +0200

    fix(docs): button spacing to 8px

--- src/docs/components/button/examples/button-color-example.component.ts
+++ src/docs/components/button/examples/button-color-example.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
     <button dt-button color="warning">Button in warning color</button>
     <button dt-button color="cta">Button in cta color</button>
   `,
-  styles: ['.dt-button + .dt-button { margin-left: 12px; }'],
+  styles: ['.dt-button + .dt-button { margin-left: 8px; }'],
 })
 export class ColorButtonExampleComponent { }
