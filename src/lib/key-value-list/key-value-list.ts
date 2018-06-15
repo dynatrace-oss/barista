81fda47cf567588662c9b77ab623b3817f9931b9
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-15 12:54:04 +0200

    APM-131181-KeyValueList-Component include review feedback. remove double includes adapt class handling. adapt class naming. including PR-128 changes.

--- src/lib/key-value-list/key-value-list.ts
+++ src/lib/key-value-list/key-value-list.ts
@@ -3,7 +3,8 @@ import {
   Input,
   ContentChildren,
   QueryList,
-  HostBinding
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
 } from '@angular/core';
 
 @Component({
@@ -14,6 +15,10 @@ import {
   host : {
     class : 'dt-key-value-list-item',
   },
+  exportAs : 'dt-key-value-list-item',
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class DtKeyValueListItem {
   @Input() key;
@@ -23,23 +28,29 @@ export class DtKeyValueListItem {
 @Component({
   moduleId: module.id,
   selector: 'dt-key-value-list',
+  host : {
+    '[class]': 'columnsClass',
+  },
   templateUrl: 'key-value-list.html',
   styleUrls: ['key-value-list.scss'],
+  exportAs : 'dt-key-value-list',
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class DtKeyValueList {
   @ContentChildren(DtKeyValueListItem) items: QueryList<DtKeyValueListItem>;
 
-  @HostBinding('class')
   get columnsClass(): string {
     const threeColumnsLayoutMinItems = 18;
     const twoColumnsLayoutMinItems = 12;
 
     if (this.items.length > threeColumnsLayoutMinItems) {
-        return `dtKeyValueListColumns3`;
+        return `dt-key-value-list-columns3`;
     } else if (this.items.length > twoColumnsLayoutMinItems) {
-        return `dtKeyValueListColumns2`;
+        return `dt-key-value-list-columns2`;
     } else {
-        return `dtKeyValueListColumns1`;
+        return `dt-key-value-list-columns1`;
     }
   }
 }
