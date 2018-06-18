081d37d9a6baf1ceeb5dbd2e030b8f7582741ab8
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-18 14:51:05 +0200

    APM-131181-KeyValueList-Component adapt indentation split components into 2 files.

--- src/lib/key-value-list/key-value-list.ts
+++ src/lib/key-value-list/key-value-list.ts
@@ -6,24 +6,9 @@ import {
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'dt-key-value-list-item',
-  templateUrl: 'key-value-list-item.html',
-  styleUrls: ['key-value-list-item.scss'],
-  host : {
-    class : 'dt-key-value-list-item',
-  },
-  exportAs : 'dtKeyValueListItem',
-  preserveWhitespaces: false,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.Emulated,
-})
-export class DtKeyValueListItem {
-  @Input() key;
-  @Input() value;
-}
+import {
+  DtKeyValueListItem
+} from './key-value-list-item';
 
 @Component({
   moduleId: module.id,
