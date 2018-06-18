1e9cf87848a2284e575edc2566e1f2b91ba75795
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-18 14:42:59 +0200

    Feature/APM-122712-Create-copy-to-clipboard-component fix text copy behaviour include dark theme

--- src/docs/components/copy-clipboard/examples/dark-copy-clipboard-example.component.ts
+++ src/docs/components/copy-clipboard/examples/dark-copy-clipboard-example.component.ts
@@ -2,10 +2,11 @@ import { Component, ViewChild } from '@angular/core';
 import { DtCopyClipboard, DtButton } from '@dynatrace/angular-components';
 
 @Component({
-  template: `<dt-copy-clipboard dtTheme=":dark">
+  template: `<section class="dark" dtTheme=":dark"><dt-copy-clipboard>
   <textarea dtInput>https://ytnzh7i9.dynatrace.com/</textarea>
   <dt-copy-clipboard-btn>Copy</dt-copy-clipboard-btn>
-</dt-copy-clipboard>`,
+</dt-copy-clipboard>
+</section>`,
 })
 export class DarkCopyClipboardExampleComponent {
     
