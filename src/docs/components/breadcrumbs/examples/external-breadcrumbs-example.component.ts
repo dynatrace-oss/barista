5d6d5388f7d8329679f315850cd1d42eff19f61f
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-06-20 13:18:55 +0200

    APM-131837 Breadcrumbs - items set to active if href not given

--- src/docs/components/breadcrumbs/examples/external-breadcrumbs-example.component.ts
+++ src/docs/components/breadcrumbs/examples/external-breadcrumbs-example.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <dt-breadcrumbs>
       <dt-breadcrumbs-item href="http://google.com" external>google.com</dt-breadcrumbs-item>
-      <dt-breadcrumbs-item href="current" [active]="true">Current view</dt-breadcrumbs-item>
+      <dt-breadcrumbs-item>Current view</dt-breadcrumbs-item>
     </dt-breadcrumbs>
   `,
 })
