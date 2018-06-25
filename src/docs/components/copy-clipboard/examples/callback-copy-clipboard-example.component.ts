b275e21e62f8cdc21b7d0d7d5483f276a00b567a
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-25 17:15:23 +0200

    APM-122712-Create-copy-to-clipboard-component
    split theming into separate file
    remove this keyword
    css adaptions

--- src/docs/components/copy-clipboard/examples/callback-copy-clipboard-example.component.ts
+++ src/docs/components/copy-clipboard/examples/callback-copy-clipboard-example.component.ts
@@ -1,14 +1,25 @@
-import {Component} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { timer } from 'rxjs/internal/observable/timer';
 
 @Component({
   template: `
     <dt-copy-clipboard (copied)="copyCallback()">
       <textarea dtInput>https://copyexample.dynatrace.com/</textarea>
       <dt-copy-clipboard-label>Copy</dt-copy-clipboard-label>
-    </dt-copy-clipboard>`,
+    </dt-copy-clipboard>
+    <br/>
+    <div #callbackresult>
+      Will change after copy
+    </div>`,
 })
 export class CallbackCopyClipboardExampleComponent {
+  @ViewChild('callbackresult') callbackResult: ElementRef;
+
   copyCallback(): void {
-    alert('done');
+    this.callbackResult.nativeElement.innerHTML = 'Copy was done';
+    // tslint:disable-next-line:no-magic-numbers
+    timer(2500).subscribe((): void => {
+      this.callbackResult.nativeElement.innerHTML = 'Will change after copy';
+    });
   }
 }
