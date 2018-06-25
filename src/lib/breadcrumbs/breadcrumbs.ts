e023f63572184e9b30057e9d4a8036a8650e1784
Thomas Pink <thomas.pink@me.com>
2018-06-25 16:10:16 +0200

    APM-135276 fix(breadcrumb): remove router dependency

--- src/lib/breadcrumbs/breadcrumbs.ts
+++ src/lib/breadcrumbs/breadcrumbs.ts
@@ -1,4 +1,15 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  OnDestroy
+} from '@angular/core';
+import { startWith } from 'rxjs/operators';
+import { NEVER } from 'rxjs';
+import { DtBreadcrumbsItem } from './item/breadcrumbs-item';
 
 @Component({
   moduleId: module.id,
@@ -13,5 +24,23 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class DtBreadcrumbs {
+export class DtBreadcrumbs implements AfterContentInit, OnDestroy {
+
+  @ContentChildren(DtBreadcrumbsItem) private _items: QueryList<DtBreadcrumbsItem>;
+
+  private _itemChangesSub = NEVER.subscribe();
+
+  ngAfterContentInit(): void {
+    this._itemChangesSub = this._items.changes
+      .pipe(startWith(null))
+      .subscribe(() => {
+        // We need to notify the items whether they are the last one in the list,
+        // because they use this information to determine their active state.
+        this._items.forEach((item, index) => item._lastItem = this._items.length - 1 === index);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._itemChangesSub.unsubscribe();
+  }
 }
--- src/lib/breadcrumbs/item/breadcrumbs-item.ts
+++ src/lib/breadcrumbs/item/breadcrumbs-item.ts
@@ -1,9 +1,16 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation, Optional, SkipSelf, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router, UrlTree } from '@angular/router';
+import { isDefined } from '../../core/util/type-util';
 
 // tslint:disable-next-line no-any
 export type RouterLinkAccepted = string | any[];
+
+export enum DtBreadcrumbItemDisplayMode {
+  RouterLink = 'router-link',
+  ExternalLink = 'external',
+  Active = 'active',
+}
 
 @Component({
   moduleId: module.id,
@@ -17,60 +24,107 @@ export type RouterLinkAccepted = string | any[];
 })
 export class DtBreadcrumbsItem {
 
-  @Input() active: boolean | undefined;
+  /** Whether the item is active and therefore a static element that is not clickable. */
+  @Input()
+  get active(): boolean {
+    if (isDefined(this._active)) {
+      return this._active!;
+    }
+    if (isDefined(this._isRouterLinkActive)) {
+      return this._isRouterLinkActive!;
+    }
+    return this._isLastItem;
+  }
+  set active(value: boolean) {
+    // Coerce can only be done if value is defined.
+    // Otherwise this will set the property to false when the value is undefined.
+    this._active = isDefined(value) ? coerceBooleanProperty(value) : value;
+  }
 
   @Input()
-  get external(): boolean | undefined {
-    return this._external;
-  }
-
-  set external(value: boolean | undefined) {
-    this._external = coerceBooleanProperty(value);
-  }
+  get external(): boolean { return this._external; }
+  set external(value: boolean) { this._external = coerceBooleanProperty(value); }
 
   @Input()
-  get href(): RouterLinkAccepted | undefined {
-    return this._href;
-  }
-
+  get href(): RouterLinkAccepted | undefined { return this._href; }
   set href(value: RouterLinkAccepted | undefined) {
     this._href = value;
-    this._isActive = this.checkIsActive();
+    if (value !== void 0) {
+      this._isRouterLinkActive = this._checkIsRouterLinkActive(value);
+    }
   }
 
-  constructor(
-    private readonly router: Router,
-    private readonly route: ActivatedRoute
-  ) {
+  /**
+   * Whether a router has been injected by DI.
+   * This is needed to decouple this component from the router.
+   * @internal
+   */
+  get _routerAvailable(): boolean { return isDefined(this._router); }
+
+  /**
+   * Determines how the item should be displayed (as an external link, a router link or as a static element)
+   * This is needed so we can have a fallback mode (active) if the other checks do not match.
+   * @internal
+   */
+  get _displayMode(): DtBreadcrumbItemDisplayMode {
+    if (!this.active) {
+      if (this.external) {
+        return DtBreadcrumbItemDisplayMode.ExternalLink;
+      } else if (this._routerAvailable) {
+        return DtBreadcrumbItemDisplayMode.RouterLink;
+      }
+    }
+    return DtBreadcrumbItemDisplayMode.Active;
+  }
+
+  /** Expose the display modes enum to the template */
+  _displayModes = DtBreadcrumbItemDisplayMode;
+
+  /**
+   * Whether this item is the last item in the breadcrumb list.
+   * Will be set from outside by the breadcrumb component.
+   * @internal
+   */
+  get _lastItem(): boolean { return this._isLastItem; }
+  set _lastItem(value: boolean) {
+    if (this._isLastItem !== value) {
+      this._isLastItem = value;
+      this._changeDetectorRef.markForCheck();
+    }
   }
 
   private _external = false;
   private _href: RouterLinkAccepted | undefined;
-  _isActive = true;
+  private _active: boolean | undefined;
+  private _isRouterLinkActive: boolean | undefined;
+  private _isLastItem = true;
 
-  private checkIsActive(): boolean {
-    if (this.active !== undefined) {
-      return this.active;
-    }
+  constructor(
+    private readonly _changeDetectorRef: ChangeDetectorRef,
+    @Optional() @SkipSelf() private readonly _router?: Router,
+    @Optional() @SkipSelf() private readonly _route?: ActivatedRoute
+  ) { }
 
-    if (this._href === undefined) {
-      return true;
+  private _checkIsRouterLinkActive(href: RouterLinkAccepted): boolean {
+    if (this._router) {
+      let urlTree: UrlTree | undefined;
+      if (Array.isArray(href)) {
+        urlTree = this._router.createUrlTree(href, {
+          queryParamsHandling: 'merge',
+        });
+      } else if (this._route) {
+        urlTree = this._mergeQueryParams(this._router.parseUrl(href));
+      }
+      if (urlTree) {
+        return this._router.isActive(urlTree, true);
+      }
     }
-
-    let urlTree: UrlTree;
-    if (Array.isArray(this._href)) {
-      urlTree = this.router.createUrlTree(this._href, {
-        queryParamsHandling: 'merge',
-      });
-    } else {
-      urlTree = this.mergeQueryParams(this.router.parseUrl(this._href));
-    }
-    return this.router.isActive(urlTree, true);
+    return false;
   }
 
-  private mergeQueryParams(urlTree: UrlTree): UrlTree {
+  private _mergeQueryParams(urlTree: UrlTree): UrlTree {
     urlTree.queryParams = {
-      ...this.route.snapshot.queryParams,
+      ...this._route!.snapshot.queryParams,
       ...urlTree.queryParams,
     };
     return urlTree;
