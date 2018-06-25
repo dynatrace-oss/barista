740f307d289328c644c99a30a4d292a6254cbb45
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-25 17:15:23 +0200

    APM-122712-Create-copy-to-clipboard-component
    split theming into separate file
    remove this keyword
    css adaptions

--- src/docs/components/copy-clipboard/examples/disabled-copy-clipboard-example.component.ts
+++ src/docs/components/copy-clipboard/examples/disabled-copy-clipboard-example.component.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
   template: `
-    <dt-copy-clipboard #copyClipboard [enabled]="this._toggleValue">
+    <dt-copy-clipboard #copyClipboard [disabled]="_toggleValue">
       <textarea dtInput>https://inzx79.dynatrace.com/</textarea>
       <dt-copy-clipboard-label>Copy</dt-copy-clipboard-label>
     </dt-copy-clipboard>
     <br/>
-    <button dt-button (click)="this._toggleValue = !this._toggleValue">Toggle</button>`,
+    <button dt-button (click)="_toggleValue = !_toggleValue">Toggle</button>`,
 })
 export class DisabledCopyClipboardExampleComponent {
-  _toggleValue = false;
+  _toggleValue = true;
 }
