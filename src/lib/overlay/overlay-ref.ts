980f0b9a763c4a44ebd97b1e0e7780f76dcbf335
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-06-27 17:05:33 +0200

    APM-122733 feat(overlay): provide first setup

---
+++ src/lib/overlay/overlay-ref.ts
@@ -0,0 +1,25 @@
+import { OverlayRef } from '@angular/cdk/overlay';
+import { addCssClass, removeCssClass } from '../core';
+
+export const DT_OVERLAY_NO_POINTER_CLASS = 'dt-overlay-no-pointer';
+
+export class DtOverlayRef {
+
+  /** wether the overlay is pinned or not */
+  pinned = false;
+
+  constructor(public overlayRef: OverlayRef) {}
+
+  pin(value: boolean): void {
+    this.pinned = value;
+    if (value) {
+      removeCssClass(this.overlayRef.backdropElement, DT_OVERLAY_NO_POINTER_CLASS);
+      this.overlayRef.backdropClick().subscribe(() => {
+        this.overlayRef.detach();
+        this.overlayRef.dispose();
+      });
+    } else {
+      addCssClass(this.overlayRef.backdropElement, DT_OVERLAY_NO_POINTER_CLASS);
+    }
+  }
+}
