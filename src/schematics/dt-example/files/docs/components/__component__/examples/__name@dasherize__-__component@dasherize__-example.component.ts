9e9c70ff58da62a6c09a6fca3ee2145a01cdb5d7
Thomas Pink <thomas.pink@me.com>
2018-06-28 14:51:05 +0200

    UX-7974 fix(docs): added example original class name decorator to schematics

--- src/schematics/dt-example/files/docs/components/__component__/examples/__name@dasherize__-__component@dasherize__-example.component.ts
+++ src/schematics/dt-example/files/docs/components/__component__/examples/__name@dasherize__-__component@dasherize__-example.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { OriginalClassName } from '../../../core/decorators';
 
 @Component({
   moduleId: module.id,
   template: '<dt-<%= component %>></dt-<%= component %>>',
 })
+@OriginalClassName('<%=exampleComponentName%>')
 export class <%=exampleComponentName%> { }
