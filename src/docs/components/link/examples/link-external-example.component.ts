8a5de7e057af6fe2500b45406ddb15799e43cf0b
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2018-06-28 14:50:05 +0200

    UX-7974 fix(docs): add original class name to component examples

--- src/docs/components/link/examples/link-external-example.component.ts
+++ src/docs/components/link/examples/link-external-example.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { OriginalClassName } from '../../../core/decorators';
 
 @Component({
   // @formatter:off
   template: `<a class="dt-link dt-external">External link</a>`,
   // @formatter:on
 })
+@OriginalClassName('LinkExternalExampleComponent')
 export class LinkExternalExampleComponent {
 }
