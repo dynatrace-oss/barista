5059f9a6e7110d8e2c6df3a3f99740eaf77bcfc8
daniel.moertenschlag <daniel.moertenschlag@dynatrace.com>
2018-06-28 12:18:56 +0200

    Feature/APM-122712-Create-copy-to-clipboard-component renaming completed and test adaptions.

--- src/docs/components/copy-to-clipboard/examples/default-copy-to-clipboard-example.component.ts
+++ src/docs/components/copy-to-clipboard/examples/default-copy-to-clipboard-example.component.ts
@@ -2,10 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   template: `
-    <dt-copy-to-to-clipboard>
+    <dt-copy-to-clipboard>
       <textarea dtInput>https://defaultcopy.dynatrace.com/</textarea>
       <dt-copy-to-clipboard-label>Copy</dt-copy-to-clipboard-label>
-    </dt-copy-to-to-clipboard>`,
+    </dt-copy-to-clipboard>`,
 })
 export class DefaultCopyToClipboardExampleComponent {
 }
