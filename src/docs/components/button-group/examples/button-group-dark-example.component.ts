30a11a38d4974b589f2d2064ee3e76da0f18dc3b
lukas.holzer <lukas.holzer@typeflow.cc>
2018-06-28 14:49:31 +0200

    UX-7974 fix(docs): add decorator for classname

--- src/docs/components/button-group/examples/button-group-dark-example.component.ts
+++ src/docs/components/button-group/examples/button-group-dark-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OriginalClassName } from '../../../core/decorators';
 
 @Component({
   moduleId: module.id,
@@ -12,4 +13,5 @@ import { Component } from '@angular/core';
   </dt-button-group>
 </section>`,
 })
+@OriginalClassName('ButtonGroupDarkExampleComponent')
 export class ButtonGroupDarkExampleComponent { }
