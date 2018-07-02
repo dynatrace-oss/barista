ddeec1bf8681c61cdaba5ca8bfd65b9709fbd37a
Manfred DelFabro <manfred.delfabro@dynatrace.com>
2018-07-02 16:07:10 +0200

    APM-135372: add dropdownopen icon to expandable-cell

--- src/lib/table/expandable-row.ts
+++ src/lib/table/expandable-row.ts
@@ -20,7 +20,7 @@ import {DtTable} from './table';
     trigger('detailExpand', [
       state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> collapsed', animate('400ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
   moduleId: module.id,
