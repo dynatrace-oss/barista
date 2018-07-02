308fae0a9aaa37ea5d32971a406cfff3cdf29d46
Szymon Bultrowicz <szymon.bultrowicz@dynatrace.com>
2018-07-02 13:08:17 +0200

    APM-131837 Breadcrumbs - set router dependency as optional

--- src/lib/breadcrumbs/item/breadcrumbs-item.ts
+++ src/lib/breadcrumbs/item/breadcrumbs-item.ts
@@ -1,8 +1,13 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation, ChangeDetectorRef, Optional, Inject } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
 
 // tslint:disable-next-line no-any
 export type RouterLinkAccepted = string | any[];
+
+function routerNotFoundError(): Error {
+  return new Error('Router not found. Cannot set internal link without router provided');
+}
 
 @Component({
   moduleId: module.id,
@@ -35,8 +40,35 @@ export class DtBreadcrumbsItem {
     }
   }
 
+  calculatedHref(): string | undefined {
+    if (this.href === undefined) {
+      return undefined;
+    }
+
+    if (this.external) {
+      return this.href.toString();
+    }
+
+    if (this._router === null) {
+      throw routerNotFoundError();
+    }
+
+    let urlTree: UrlTree;
+    if (Array.isArray(this.href)) {
+      urlTree = this._router.createUrlTree(this.href, {
+        queryParamsHandling: 'merge',
+      });
+    } else {
+      urlTree = this._router.parseUrl(this.href);
+    }
+    return this._router.serializeUrl(urlTree);
+  }
+
   private _external = false;
   private _isLastItem = false;
 
-  constructor(private readonly _changeDetectorRef: ChangeDetectorRef) { }
+  constructor(
+    private readonly _changeDetectorRef: ChangeDetectorRef,
+    @Optional() @Inject(Router) private readonly _router: Router | null
+    ) {}
 }
