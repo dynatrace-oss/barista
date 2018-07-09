f2a8f3cc8b9353692cdf550355932647d69055cc
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-09 11:01:53 +0200

    APM-122741 feat(tabs): tab-label

--- src/docs/components/tabs/examples/default-tabs-example.component.ts
+++ src/docs/components/tabs/examples/default-tabs-example.component.ts
@@ -5,16 +5,12 @@ import { OriginalClassName } from '../../../core/decorators';
   moduleId: module.id,
   template: `
   <dt-tab-group>
-    <dt-tab [value]="cpu-usage" selected>
-      <dt-tab-label>CPU Usage</dt-tab-label>
-      <ng-template>some content</ng-template>
-    </dt-tab>
-    <dt-tab [value]="physical-cpu" disabled>
-                  <dt-tab-label>Physical CPU</dt-tab-label>
+    <dt-tab value="physical-cpu" disabled>
+      <ng-template dt-tab-label>Physical CPU</ng-template>
       some content disabled
     </dt-tab>
-    <dt-tab [value]="cpu-ready-time" color="error">
-      <dt-tab-label>CPU ready time</dt-tab-label>
+    <dt-tab value="cpu-ready-time" color="error">
+      <ng-template dt-tab-label>CPU ready time</ng-template>
       some content
     </dt-tab>
   </dt-tab-group>
