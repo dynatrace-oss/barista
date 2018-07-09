7389f469ea722b67ac3bde8396ed7be7d1ecdd4c
AlexandruDumitru <alexandru.cd@gmail.com>
2018-07-09 15:01:38 +0200

    APM-122733 chore(overlay): cleanup

--- src/lib/overlay/overlay-trigger.ts
+++ src/lib/overlay/overlay-trigger.ts
@@ -1,14 +1,19 @@
-import { Directive, Input, ElementRef, OnDestroy, TemplateRef, Renderer2 } from '@angular/core';
+import { Directive, Input, ElementRef, TemplateRef, ViewChild, Output, EventEmitter } from '@angular/core';
 import { DtOverlayService, DEFAULT_DT_OVERLAY_CONFIG } from './overlay';
 import { DtOverlayConfig } from './overlay-config';
+import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
+import { ESCAPE } from '@angular/cdk/keycodes';
+import { filter } from 'rxjs/operators';
+import { DtOverlayRef } from './overlay-ref';
 
 @Directive({
   selector: '[dtOverlay]',
   exportAs: 'dtOverlayTrigger',
   host: {
-    '(mouseenter)': '_handleMouseEnter()',
+    '(mouseenter)': '_handleMouseEnter($event)',
     '(mouseleave)': '_handleMouseLeave()',
-    '(click)': '_handleClick()',
+    '(click)': '_config.enableClick && _handleClick()',
+    '(focusout)': '_handleFocusOut()'
   },
 })
 export class DtOverlayTrigger<T> {
@@ -16,37 +21,65 @@ export class DtOverlayTrigger<T> {
   private _content: TemplateRef<T>;
   private _config: DtOverlayConfig = DEFAULT_DT_OVERLAY_CONFIG;
 
+  /** Overlay pane containing the content */
+  @ViewChild(CdkConnectedOverlay) _overlayPane: CdkConnectedOverlay;
+  @ViewChild(CdkOverlayOrigin) _defaultTrigger: CdkOverlayOrigin;
+
   @Input('dtOverlay')
   set overlay(value: TemplateRef<T>) {
     this._content = value;
   }
+
   @Input()
   get dtOverlayConfig(): DtOverlayConfig { return this._config; }
   set dtOverlayConfig(value: DtOverlayConfig) {
-    this._config = { ...DEFAULT_DT_OVERLAY_CONFIG, ...value };
+    this._config = { ...value };
   }
 
-  constructor(protected dtOverlayService: DtOverlayService, public elementRef: ElementRef) {
-  }
+  /** Event emitted when the select has been opened. */
+  @Output() readonly overlayOpened: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(
+    protected _dtOverlayService: DtOverlayService,
+    public elementRef: ElementRef
+  ) {}
 
   _handleMouseEnter(): void {
-    if (this.dtOverlayConfig.enableMouseMove) {
-      this.dtOverlayService.create<T>(this.elementRef, this._content, this.dtOverlayConfig);
-    }
+    this._dtOverlayService.create<T>(this.elementRef, this._content, this._config);
+    this.overlayOpened.emit(true);
   }
 
   _handleMouseLeave(): void {
-    const ref = this.dtOverlayService.overlayRef;
-    if (this.dtOverlayConfig.enableMouseMove && ref && !ref.pinned) {
-      this.dtOverlayService.close();
+    const ref = this._dtOverlayService.overlayRef;
+    if (ref && !ref.pinned) {
+      this._dtOverlayService.close();
     }
   }
 
   _handleClick(): void {
-    if (this.dtOverlayConfig.enableClick) {
-      const overlayRef = this.dtOverlayService.overlayRef === null ?
-        this.dtOverlayService.create<T>(this.elementRef, this._content, this.dtOverlayConfig) : this.dtOverlayService.overlayRef;
-      overlayRef.pin(true);
-    }
+    this._dtOverlayService.close();
+
+    const ref: DtOverlayRef = this._dtOverlayService.create<T>(this.elementRef, this._content, this._config)
+
+    ref.overlayRef.backdropClick().subscribe(() => {
+      this.overlayOpened.emit(false);
+      ref.pin(false);
+    });
+
+    ref.overlayRef.keydownEvents()
+      .pipe(filter(event => event.keyCode === ESCAPE && ref.pinned))
+      .subscribe(() => {
+        this._dtOverlayService.close()
+      });
+
+    this._dtOverlayService.trapFocus();
+    this.elementRef.nativeElement.focus();
+    this._dtOverlayService.setFocus();
+
+    ref.pin(true);
+  }
+
+  _handleFocusOut(): void {
+    this._dtOverlayService.close();
   }
 }
