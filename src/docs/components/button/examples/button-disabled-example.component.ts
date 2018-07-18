61e911bb8299d4e07286c5bcc176a67a5a38f4f6
Thomas Pink <thomas.pink@dynatrace.com>
2018-07-18 09:32:19 +0200

    NOISSUE - refactor(docs): button example formatting

--- src/docs/components/button/examples/button-disabled-example.component.ts
+++ src/docs/components/button/examples/button-disabled-example.component.ts
@@ -4,13 +4,17 @@ import { OriginalClassName } from '../../../core/decorators';
 @Component({
   moduleId: module.id,
   template: `
-    <button dt-button disabled>Disabled Button</button>
-    <button dt-button disabled variant="secondary">Disabled Secondary Button</button>
-    <button dt-button disabled><dt-icon name="agent"></dt-icon>Default with icon</button>
-    <button dt-button disabled variant="secondary"><dt-icon name="agent"></dt-icon>Warning with icon</button>
-    <button dt-icon-button disabled><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button disabled variant="secondary"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button disabled variant="nested"><dt-icon name="agent"></dt-icon></button>
+    <p>
+      <button dt-button disabled>Disabled Button</button>
+      <button dt-button disabled variant="secondary">Disabled Secondary Button</button>
+    </p><p>
+      <button dt-button disabled><dt-icon name="agent"></dt-icon>Default with icon</button>
+      <button dt-button disabled variant="secondary"><dt-icon name="agent"></dt-icon>Warning with icon</button>
+    </p><p>
+      <button dt-icon-button disabled><dt-icon name="agent"></dt-icon></button>
+      <button dt-icon-button disabled variant="secondary"><dt-icon name="agent"></dt-icon></button>
+      <button dt-icon-button disabled variant="nested"><dt-icon name="agent"></dt-icon></button>
+    </p>
   `,
   styles: ['.dt-button + .dt-button { margin-left: 8px; }'],
 })
--- src/docs/components/button/examples/button-icons-example.component.ts
+++ src/docs/components/button/examples/button-icons-example.component.ts
@@ -4,12 +4,16 @@ import { OriginalClassName } from '../../../core/decorators';
 @Component({
   moduleId: module.id,
   template: `
-    <button dt-button><dt-icon name="agent"></dt-icon>Primary with icon</button>
-    <button dt-button variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
-    <button dt-button color="warning"><dt-icon name="agent"></dt-icon>Primary with icon</button>
-    <button dt-button color="warning" variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
-    <button dt-button color="cta"><dt-icon name="agent"></dt-icon>Primary with icon</button>
-    <button dt-button color="cta" variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
+    <p>
+      <button dt-button><dt-icon name="agent"></dt-icon>Primary with icon</button>
+      <button dt-button variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
+    </p><p>
+      <button dt-button color="warning"><dt-icon name="agent"></dt-icon>Primary with icon</button>
+      <button dt-button color="warning" variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
+    </p><p>
+      <button dt-button color="cta"><dt-icon name="agent"></dt-icon>Primary with icon</button>
+      <button dt-button color="cta" variant="secondary"><dt-icon name="agent"></dt-icon>Secondary with icon</button>
+    </p>
   `,
   styles: ['.dt-button + .dt-button { margin-left: 8px; }'],
 })
