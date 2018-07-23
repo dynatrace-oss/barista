d3a436d7b0c3a483c6ef101ced8037c21a857950
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-23 16:23:15 +0200

    APM-122746 feat(toast): toastRef and toastContainer - handle deletion

--- src/lib/toast/toast.ts
+++ src/lib/toast/toast.ts
@@ -1,9 +1,10 @@
 import { Injectable, Injector, InjectionToken } from '@angular/core';
 import { Subject, timer, Subscription } from 'rxjs';
-import { DtToastRef } from './toast-ref';
+import { DtToastContainer } from './toast-container';
 import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
 import { switchMap, tap } from 'rxjs/operators';
+import { DtToastRef } from '@dynatrace/angular-components/toast/toast-ref';
 
 const DT_TOAST_BOTTOM_SPACING = 24;
 
@@ -22,39 +23,26 @@ export const DT_TOAST_MESSAGE = new InjectionToken<string>('DtToastMessage');
 export class DtToast {
 
   created: Subject<DtToastRef> = new Subject<DtToastRef>();
-  dismissed: Subject<void> = new Subject<void>();
+  dismissed: Subject<DtToastRef> = new Subject<DtToastRef>();
 
-  private _overlayRef: OverlayRef | null;
-  private _timerSub = Subscription.EMPTY;
+  private _openedToastRef: DtToastRef | null;
 
   constructor(private _overlay: Overlay, private _injector: Injector) {}
 
   /** Creates a new toast and dismisses the current one if one exists */
-  create(message: string): void {
-    if (message) {
-      this._detach();
-      const injector = new PortalInjector(this._injector, new WeakMap<InjectionToken<string>, string>([
-        [DT_TOAST_MESSAGE, message],
-      ]));
-      const containerPortal = new ComponentPortal(DtToastRef, null, injector);
-      this._overlayRef = this._createOverlay();
-      const dtToastRef = this._overlayRef.attach(containerPortal);
-      const duration = this._calculateToastDuration(message);
-      this._timerSub = timer(duration)
-      .pipe(tap(() => {
-        dtToastRef.instance.animationState = 'leave';
-      }))
-      .pipe(switchMap(() => dtToastRef.instance.animationDone))
-      .subscribe(() => {
-        console.log('leave animation done');
-      });
-      this.created.next();
-    }
-  }
+  create(message: string): DtToastRef {
+    const injector = new PortalInjector(this._injector, new WeakMap<InjectionToken<string>, string>([
+      [DT_TOAST_MESSAGE, message],
+    ]));
+    const containerPortal = new ComponentPortal(DtToastContainer, null, injector);
+    const overlayRef = this._createOverlay();
+    const componentRef = overlayRef.attach(containerPortal);
+    const duration = this._calculateToastDuration(message);
+    const toastRef = new DtToastRef(componentRef.instance, overlayRef, duration);
 
-  /** Dismisse the current toast */
-  dismiss(): void {
-    this._detach();
+    this._animateDtToastContainer(toastRef);
+    this._openedToastRef = toastRef;
+    return this._openedToastRef;
   }
 
   /**
@@ -71,18 +59,28 @@ export class DtToast {
     return this._overlay.create({ ...DT_TOAST_DEFAULT_CONFIG, positionStrategy });
   }
 
-  private _detach(): void {
-    if (this._overlayRef) {
-      this._overlayRef.detach();
-      this._overlayRef.dispose();
-      this._overlayRef = null;
-      this.dismissed.next();
-    }
-    this._timerSub.unsubscribe();
-  }
-
   /** calculates the duration the toast is shown based on the message length */
   private _calculateToastDuration(message: string): number {
     return DT_TOAST_PERCEIVE_TIME + DT_TOAST_CHAR_READ_TIME * message.length;
+  }
+
+  private _animateDtToastContainer(toastRef: DtToastRef): void {
+    toastRef._afterDismissed.subscribe(() => {
+      if (this._openedToastRef === toastRef) {
+        this._openedToastRef = null;
+      }
+    });
+
+    /** check if there is already one toast open */
+    if (this._openedToastRef) {
+      /** wait until the open toast is dismissed - then open the new one */
+      this._openedToastRef._afterDismissed.subscribe(() => {
+        toastRef.containerInstance._enter();
+      });
+      /** dismiss the current open toast */
+      this._openedToastRef._dismiss();
+    } else {
+      toastRef.containerInstance._enter();
+    }
   }
 }
