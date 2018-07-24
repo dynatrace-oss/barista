50879061acf5c73d621f8adecdde8d685b84e037
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-24 14:58:49 +0200

    APM-122746 feat(toast): fixed multiple toasts stacked when spammed

--- src/docs/components/toast/examples/default-toast-example.component.ts
+++ src/docs/components/toast/examples/default-toast-example.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
 import { DtToast } from '@dynatrace/angular-components';
 
-let id = 0;
-
 @Component({
   moduleId: module.id,
   template: '<button dt-button (click)="createToast()">Save</button>',
@@ -11,7 +9,8 @@ let id = 0;
 @OriginalClassName('DefaultToastExampleComponent')
 export class DefaultToastExampleComponent {
   constructor(private _toast: DtToast) {}
+
   createToast(): void {
-    this._toast.create(`Your changes have been saved.${++id}`);
+    this._toast.create('Your changes have been saved!');
   }
 }
