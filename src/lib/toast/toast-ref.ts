c9a5680fbfced9b8b234c55a529ff25118bdbee5
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-24 14:58:49 +0200

    APM-122746 feat(toast): fixed multiple toasts stacked when spammed

--- src/lib/toast/toast-ref.ts
+++ src/lib/toast/toast-ref.ts
@@ -1,30 +1,47 @@
 import { DtToastContainer } from './toast-container';
 import { OverlayRef } from '@angular/cdk/overlay';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 export class DtToastRef {
+
   containerInstance: DtToastContainer;
+
+  duration: number;
 
   private _durationTimeoutId: number;
 
-  _afterDismissed = new Subject<void>();
+  private readonly _afterDismissed = new Subject<void>();
 
   constructor(
     containerInstance: DtToastContainer,
-    private _overlayRef: OverlayRef,
-    private _duration: number
+    duration: number,
+    private _overlayRef: OverlayRef
   ) {
     this.containerInstance = containerInstance;
-    containerInstance._afterLeave.subscribe(() => {
-      console.log('afterleave triggered');
+    this.duration = duration;
+    containerInstance._onExit.subscribe(() => {
       this._overlayRef.dispose();
       this._afterDismissed.next();
+      this._afterDismissed.complete();
     });
-    this._durationTimeoutId = window.setTimeout(() => this._dismiss(), this._duration);
   }
 
-  _dismiss(): void {
-    this._overlayRef.detach();
+  afterDismissed(): Observable<void> {
+    return this._afterDismissed.asObservable();
+  }
+
+  afterOpened(): Observable<void> {
+    return this.containerInstance._onEnter;
+  }
+
+  dismiss(): void {
+    if (!this._afterDismissed.closed) {
+      this.containerInstance.exit();
+    }
     clearTimeout(this._durationTimeoutId);
+  }
+
+  dismissAfterTimeout(): void {
+    this._durationTimeoutId = window.setTimeout(() => this.dismiss(), this.duration);
   }
 }
