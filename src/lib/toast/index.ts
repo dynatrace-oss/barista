fd98ad2eb6087d2abab2b18748b9e025ad3a33e5
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-24 17:33:57 +0200

    APM-122746 feat(toast): added tests

--- src/lib/toast/index.ts
+++ src/lib/toast/index.ts
@@ -2,3 +2,4 @@ export * from './toast-module';
 export * from './toast-container';
 export * from './toast';
 export * from './toast-ref';
+export * from './toast-config';
---
+++ src/lib/toast/toast-config.ts
@@ -0,0 +1,12 @@
+import { OverlayConfig } from '@angular/cdk/overlay';
+
+export const DT_TOAST_BOTTOM_SPACING = 24;
+
+export const DT_TOAST_DEFAULT_CONFIG: OverlayConfig = {
+  hasBackdrop: false,
+  minHeight: 52,
+};
+
+export const DT_TOAST_PERCEIVE_TIME = 500;
+export const DT_TOAST_CHAR_READ_TIME = 50;
+export const DT_TOAST_FADE_TIME = 150;
