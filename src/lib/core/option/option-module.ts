1a5911dba22dce9cd2ec3420c186751f759da9e1
Thomas Pink <thomas.pink@me.com>
2018-07-25 15:04:50 +0200

    APM-122714 feat(option): module, exports

---
+++ src/lib/core/option/option-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from 'node_modules/@angular/common';
+import { DtOption } from './option';
+import { DtOptgroup } from './optgroup';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [DtOption, DtOptgroup],
+  declarations: [DtOption, DtOptgroup],
+})
+export class DtOptionModule {}
