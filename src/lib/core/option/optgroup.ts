7921897897735686cbd6796dfea0f8af41ec7d9b
Thomas Pink <thomas.pink@dynatrace.com>
2018-07-25 14:48:09 +0200

    APM-122736 feat(select): option, optgroup

---
+++ src/lib/core/option/optgroup.ts
@@ -0,0 +1,35 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
+import { CanDisable, mixinDisabled } from '../common-behaviours/disabled';
+
+let _uniqueId = 0;
+
+// Boilerplate for applying mixins to DtOptgroup.
+export class DtOptgroupBase { }
+export const _DtOptgroupMixinBase = mixinDisabled(DtOptgroupBase);
+
+/** Component that is used to group instances of `dt-option`. */
+@Component({
+  moduleId: module.id,
+  selector: 'dt-optgroup',
+  exportAs: 'dtOptgroup',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  inputs: ['disabled'],
+  templateUrl: 'optgroup.html',
+  styleUrls: ['optgroup.scss'],
+  host: {
+    'class': 'dt-optgroup',
+    'role': 'group',
+    '[class.dt-optgroup-disabled]': 'disabled',
+    '[attr.aria-disabled]': 'disabled.toString()',
+    '[attr.aria-labelledby]': '_labelId',
+  },
+})
+export class DtOptgroup extends _DtOptgroupMixinBase implements CanDisable {
+
+  /** Label for the option group. */
+  @Input() label: string;
+
+  /** Unique id for the underlying label. */
+  _labelId = `dt-optgroup-label-${_uniqueId++}`;
+}
