02414d04a7652b1b461f47e4202312d16f127082
Thomas Pink <thomas.pink@me.com>
2018-07-26 16:15:29 +0200

    APM-122714 feat(select): optgroup styling & example

--- src/docs/components/select/docs-select.module.ts
+++ src/docs/components/select/docs-select.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { UiModule, COMPONENT_EXAMPLES } from '../../ui/ui.module';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DtSelectModule, DtCheckboxModule } from '@dynatrace/angular-components';
 import { DefaultSelectExampleComponent } from './examples/default-select-example.component';
 import { DisabledSelectExampleComponent } from './examples/disabled-select-example.component';
 import { FormsSelectExampleComponent } from './examples/forms-select-example.component';
-import { FormsModule } from '@angular/forms';
+import { GroupsSelectExampleComponent } from './examples/groups-select-example.component';
 
 export const EXAMPLES = [
   DefaultSelectExampleComponent,
   DisabledSelectExampleComponent,
   FormsSelectExampleComponent,
+  GroupsSelectExampleComponent,
 ];
 
 @NgModule({
