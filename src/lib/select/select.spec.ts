040ab5a18f68b1544e6c00967f93630e253e0adf
Thomas Pink <thomas.pink@me.com>
2018-07-30 16:13:10 +0200

    APM-122714 feat(select): tests

--- src/lib/select/select.spec.ts
+++ src/lib/select/select.spec.ts
@@ -1,5 +1,5 @@
 
-import { async, ComponentFixture, TestBed, inject, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, fakeAsync, flush } from '@angular/core/testing';
 import { Component, ViewChild, QueryList, ViewChildren } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { DtSelectModule, DtSelect, DtFormFieldModule, DtOption, DtIconModule } from '@dynatrace/angular-components';
@@ -9,23 +9,20 @@ import { Subject } from 'rxjs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { Platform } from '@angular/cdk/platform';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DOWN_ARROW, UP_ARROW, RIGHT_ARROW, LEFT_ARROW } from '@angular/cdk/keycodes';
+import { dispatchKeyboardEvent, dispatchEvent } from '../../testing/dispatch-events';
+import { createKeyboardEvent } from '../../testing/event-objects';
 
-// tslint:disable:no-any i18n
+// tslint:disable:no-any i18n no-magic-numbers
 
 fdescribe('DtSelect', () => {
   let overlayContainer: OverlayContainer;
   let overlayContainerElement: HTMLElement;
-  let scrolledSubject = new Subject();
-  let viewportRuler: ViewportRuler;
-  let platform: Platform;
+  const scrolledSubject = new Subject();
+  // let viewportRuler: ViewportRuler;
+  // let platform: Platform;
 
-  /**
-   * Configures the test module for MatSelect with the given declarations. This is broken out so
-   * that we're only compiling the necessary test components for each test in order to speed up
-   * overall test time.
-   * @param declarations Components to declare for this block
-   */
-  function configureMatSelectTestingModule(declarations: any[]): void {
+  function configureDtSelectTestingModule(declarations: any[]): void {
     TestBed.configureTestingModule({
       imports: [
         DtFormFieldModule,
@@ -34,7 +31,7 @@ fdescribe('DtSelect', () => {
         FormsModule,
         NoopAnimationsModule,
         HttpClientTestingModule,
-        DtIconModule.forRoot({svgIconLocation: `{{name}}.svg`}),
+        DtIconModule.forRoot({ svgIconLocation: `{{name}}.svg` }),
       ],
       declarations,
       providers: [
@@ -49,7 +46,7 @@ fdescribe('DtSelect', () => {
     inject([OverlayContainer, Platform], (oc: OverlayContainer, p: Platform) => {
       overlayContainer = oc;
       overlayContainerElement = oc.getContainerElement();
-      platform = p;
+      // platform = p;
     })();
   }
 
@@ -59,7 +56,7 @@ fdescribe('DtSelect', () => {
 
   describe('core', () => {
     beforeEach(async(() => {
-      configureMatSelectTestingModule([
+      configureDtSelectTestingModule([
         BasicSelect,
         SelectWithGroups,
         SelectWithGroupsAndNgContainer,
@@ -80,6 +77,251 @@ fdescribe('DtSelect', () => {
 
         it('should set the role of the select to listbox', fakeAsync(() => {
           expect(select.getAttribute('role')).toEqual('listbox');
+        }));
+
+        it('should set the aria label of the select to the placeholder', fakeAsync(() => {
+          expect(select.getAttribute('aria-label')).toEqual('Food');
+        }));
+
+        it('should support setting a custom aria-label', fakeAsync(() => {
+          fixture.componentInstance.ariaLabel = 'Custom Label';
+          fixture.detectChanges();
+
+          expect(select.getAttribute('aria-label')).toEqual('Custom Label');
+        }));
+
+        it('should not set an aria-label if aria-labelledby is specified', fakeAsync(() => {
+          fixture.componentInstance.ariaLabelledby = 'myLabelId';
+          fixture.detectChanges();
+
+          expect(select.getAttribute('aria-label')).toBeFalsy('Expected no aria-label to be set.');
+          expect(select.getAttribute('aria-labelledby')).toBe('myLabelId');
+        }));
+
+        it('should not have aria-labelledby in the DOM if it`s not specified', fakeAsync(() => {
+          fixture.detectChanges();
+          expect(select.hasAttribute('aria-labelledby')).toBeFalsy();
+        }));
+
+        it('should set the tabindex of the select to 0 by default', fakeAsync(() => {
+          expect(select.getAttribute('tabindex')).toEqual('0');
+        }));
+
+        it('should be able to override the tabindex', fakeAsync(() => {
+          fixture.componentInstance.tabIndexOverride = 3;
+          fixture.detectChanges();
+
+          expect(select.getAttribute('tabindex')).toBe('3');
+        }));
+
+        it('should set aria-required for required selects', fakeAsync(() => {
+          expect(select.getAttribute('aria-required'))
+            .toEqual('false', `Expected aria-required attr to be false for normal selects.`);
+
+          fixture.componentInstance.isRequired = true;
+          fixture.detectChanges();
+
+          expect(select.getAttribute('aria-required'))
+            .toEqual('true', `Expected aria-required attr to be true for required selects.`);
+        }));
+
+        it('should set the dt-select-required class for required selects', fakeAsync(() => {
+          expect(select.classList).not.toContain(
+            'dt-select-required', `Expected the dt-select-required class not to be set.`);
+
+          fixture.componentInstance.isRequired = true;
+          fixture.detectChanges();
+
+          expect(select.classList).toContain(
+            'dt-select-required', `Expected the dt-select-required class to be set.`);
+        }));
+
+        it('should set aria-invalid for selects that are invalid and touched', fakeAsync(() => {
+          expect(select.getAttribute('aria-invalid'))
+            .toEqual('false', `Expected aria-invalid attr to be false for valid selects.`);
+
+          fixture.componentInstance.isRequired = true;
+          fixture.componentInstance.control.markAsDirty();
+          fixture.detectChanges();
+
+          expect(select.getAttribute('aria-invalid'))
+            .toEqual('true', `Expected aria-invalid attr to be true for invalid selects.`);
+        }));
+
+        it('should set aria-disabled for disabled selects', fakeAsync(() => {
+          expect(select.getAttribute('aria-disabled')).toEqual('false');
+
+          fixture.componentInstance.control.disable();
+          fixture.detectChanges();
+
+          expect(select.getAttribute('aria-disabled')).toEqual('true');
+        }));
+
+        it('should set the tabindex of the select to -1 if disabled', fakeAsync(() => {
+          fixture.componentInstance.control.disable();
+          fixture.detectChanges();
+          expect(select.getAttribute('tabindex')).toEqual('-1');
+
+          fixture.componentInstance.control.enable();
+          fixture.detectChanges();
+          expect(select.getAttribute('tabindex')).toEqual('0');
+        }));
+
+        it('should set `aria-labelledby` to form field label if there is no placeholder', () => {
+          fixture.destroy();
+
+          const labelFixture = TestBed.createComponent(SelectWithFormFieldLabel);
+          labelFixture.detectChanges();
+          select = labelFixture.debugElement.query(By.css('dt-select')).nativeElement;
+
+          expect(select.getAttribute('aria-labelledby')).toBeTruthy();
+          expect(select.getAttribute('aria-labelledby'))
+            .toBe(labelFixture.nativeElement.querySelector('label').getAttribute('id'));
+        });
+
+        it('should not set `aria-labelledby` if there is a placeholder', () => {
+          fixture.destroy();
+
+          const labelFixture = TestBed.createComponent(SelectWithFormFieldLabel);
+          labelFixture.componentInstance.placeholder = 'Thing selector';
+          labelFixture.detectChanges();
+          select = labelFixture.debugElement.query(By.css('dt-select')).nativeElement;
+
+          expect(select.getAttribute('aria-labelledby')).toBeFalsy();
+        });
+
+        it('should select options via the UP/DOWN arrow keys on a closed select', fakeAsync(() => {
+          const formControl = fixture.componentInstance.control;
+          const options = fixture.componentInstance.options.toArray();
+
+          expect(formControl.value).toBeFalsy('Expected no initial value.');
+
+          dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+
+          expect(options[0].selected).toBe(true, 'Expected first option to be selected.');
+          expect(formControl.value).toBe(options[0].value, 'Expected value from first option to have been set on the model.');
+
+          dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+          dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+
+          // Note that the third option is skipped, because it is disabled.
+          expect(options[3].selected).toBe(true, 'Expected fourth option to be selected.');
+          expect(formControl.value).toBe(options[3].value, 'Expected value from fourth option to have been set on the model.');
+
+          dispatchKeyboardEvent(select, 'keydown', UP_ARROW);
+
+          expect(options[1].selected).toBe(true, 'Expected second option to be selected.');
+          expect(formControl.value).toBe(options[1].value, 'Expected value from second option to have been set on the model.');
+        }));
+
+        it('should resume focus from selected item after selecting via click', fakeAsync(() => {
+          const formControl = fixture.componentInstance.control;
+          const options = fixture.componentInstance.options.toArray();
+
+          expect(formControl.value).toBeFalsy('Expected no initial value.');
+
+          fixture.componentInstance.select.open();
+          fixture.detectChanges();
+          flush();
+
+          (overlayContainerElement.querySelectorAll('dt-option')[3] as HTMLElement).click();
+          fixture.detectChanges();
+          flush();
+
+          expect(formControl.value).toBe(options[3].value);
+
+          dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+          fixture.detectChanges();
+
+          expect(formControl.value).toBe(options[4].value);
+        }));
+
+        it('should select options via LEFT/RIGHT arrow keys on a closed select', fakeAsync(() => {
+          const formControl = fixture.componentInstance.control;
+          const options = fixture.componentInstance.options.toArray();
+
+          expect(formControl.value).toBeFalsy('Expected no initial value.');
+
+          dispatchKeyboardEvent(select, 'keydown', RIGHT_ARROW);
+
+          expect(options[0].selected).toBe(true, 'Expected first option to be selected.');
+          expect(formControl.value).toBe(options[0].value, 'Expected value from first option to have been set on the model.');
+
+          dispatchKeyboardEvent(select, 'keydown', RIGHT_ARROW);
+          dispatchKeyboardEvent(select, 'keydown', RIGHT_ARROW);
+
+          // Note that the third option is skipped, because it is disabled.
+          expect(options[3].selected).toBe(true, 'Expected fourth option to be selected.');
+          expect(formControl.value).toBe(options[3].value, 'Expected value from fourth option to have been set on the model.');
+
+          dispatchKeyboardEvent(select, 'keydown', LEFT_ARROW);
+
+          expect(options[1].selected).toBe(true, 'Expected second option to be selected.');
+          expect(formControl.value).toBe(options[1].value, 'Expected value from second option to have been set on the model.');
+        }));
+
+        it('should open a single-selection select using ALT + DOWN_ARROW', fakeAsync(() => {
+          const { control: formControl, select: selectInstance } = fixture.componentInstance;
+
+          expect(selectInstance.panelOpen).toBe(false, 'Expected select to be closed.');
+          expect(formControl.value).toBeFalsy('Expected no initial value.');
+
+          const event = createKeyboardEvent('keydown', DOWN_ARROW);
+          Object.defineProperty(event, 'altKey', { get: () => true });
+
+          dispatchEvent(select, event);
+
+          expect(selectInstance.panelOpen).toBe(true, 'Expected select to be open.');
+          expect(formControl.value).toBeFalsy('Expected value not to have changed.');
+        }));
+
+        it('should open a single-selection select using ALT + UP_ARROW', fakeAsync(() => {
+          const { control: formControl, select: selectInstance } = fixture.componentInstance;
+
+          expect(selectInstance.panelOpen).toBe(false, 'Expected select to be closed.');
+          expect(formControl.value).toBeFalsy('Expected no initial value.');
+
+          const event = createKeyboardEvent('keydown', UP_ARROW);
+          Object.defineProperty(event, 'altKey', { get: () => true });
+
+          dispatchEvent(select, event);
+
+          expect(selectInstance.panelOpen).toBe(true, 'Expected select to be open.');
+          expect(formControl.value).toBeFalsy('Expected value not to have changed.');
+        }));
+
+        it('should should close when pressing ALT + DOWN_ARROW', fakeAsync(() => {
+          const { select: selectInstance } = fixture.componentInstance;
+
+          selectInstance.open();
+          fixture.detectChanges();
+
+          expect(selectInstance.panelOpen).toBe(true, 'Expected select to be open.');
+
+          const event = createKeyboardEvent('keydown', DOWN_ARROW);
+          Object.defineProperty(event, 'altKey', { get: () => true });
+
+          dispatchEvent(select, event);
+
+          expect(selectInstance.panelOpen).toBe(false, 'Expected select to be closed.');
+          expect(event.defaultPrevented).toBe(true, 'Expected default action to be prevented.');
+        }));
+
+        it('should should close when pressing ALT + UP_ARROW', fakeAsync(() => {
+          const { select: selectInstance } = fixture.componentInstance;
+
+          selectInstance.open();
+          fixture.detectChanges();
+
+          expect(selectInstance.panelOpen).toBe(true, 'Expected select to be open.');
+
+          const event = createKeyboardEvent('keydown', UP_ARROW);
+          Object.defineProperty(event, 'altKey', { get: () => true });
+
+          dispatchEvent(select, event);
+
+          expect(selectInstance.panelOpen).toBe(false, 'Expected select to be closed.');
+          expect(event.defaultPrevented).toBe(true, 'Expected default action to be prevented.');
         }));
       });
     });
@@ -221,4 +463,4 @@ class SelectWithFormFieldLabel {
   placeholder: string;
 }
 
-// tslint:enable:no-any i18n
+// tslint:enable:no-any i18n no-magic-numbers
