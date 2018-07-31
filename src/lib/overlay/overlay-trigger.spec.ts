655eb8272f6264b6e03a79578b3c2c3fdd3d7fe4
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-31 17:15:55 +0200

    APM-122733 fix(overlay): added tests

---
+++ src/lib/overlay/overlay-trigger.spec.ts
@@ -0,0 +1,82 @@
+
+import {ComponentFixture, TestBed, fakeAsync, inject, flush} from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { DtOverlayModule, DtOverlay, DT_OVERLAY_DEFAULT_OFFSET, DtOverlayConfig } from '@dynatrace/angular-components';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { dispatchMouseEvent } from '../../testing/dispatch-events';
+import { By } from '@angular/platform-browser';
+
+describe('DtOverlayTrigger', () => {
+  let overlayContainer: OverlayContainer;
+  let overlayContainerElement: HTMLElement;
+  let trigger: HTMLElement;
+
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [DtOverlayModule, NoopAnimationsModule],
+      declarations: [TestComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(inject([OverlayContainer], (oc: OverlayContainer) => {
+    overlayContainer = oc;
+    overlayContainerElement = oc.getContainerElement();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    trigger = fixture.debugElement.query(By.css('.dt-overlay-trigger')).nativeElement;
+  });
+
+  afterEach(() => {
+    overlayContainer.ngOnDestroy();
+  });
+
+  it('should create an overlay on mouseover and close on mouseout', fakeAsync(() => {
+    dispatchMouseEvent(trigger, 'mouseover');
+    fixture.detectChanges();
+
+    let overlay = overlayContainerElement.querySelector('.dt-overlay-container') as HTMLElement;
+    expect(overlay).toBeDefined();
+    expect(overlay.innerText).toEqual('overlay');
+
+    dispatchMouseEvent(trigger, 'mouseout');
+    fixture.detectChanges();
+    flush();
+
+    overlay = overlayContainerElement.querySelector('.dt-overlay-container') as HTMLElement;
+    expect(overlay).toBeNull();
+  }));
+
+  // it('should set the offset to the mouseposition', fakeAsync(() => {
+  //   const offset = 10;
+  //   dispatchMouseEvent(trigger, 'mouseover');
+  //   flush();
+  //   dispatchMouseEvent(
+  //     trigger,
+  //     'mousemove',
+  //     trigger.getBoundingClientRect().left + offset,
+  //     trigger.getBoundingClientRect().top + offset);
+  //   fixture.detectChanges();
+  //   flush();
+
+  //   const overlayPane = overlayContainerElement.querySelector('.cdk-overlay-pane') as HTMLElement;
+  //   expect(overlayPane).toBeDefined();
+  //   expect(overlayPane.style.transform).toEqual(
+  //     `translateX(${offset}px) translateY(${offset}px)`
+  //   );
+  // }));
+
+});
+
+/** dummy component */
+@Component({
+  selector: 'dt-test-component',
+  template: '<div [dtOverlay]="overlay">trigger</div><ng-template #overlay>overlay</ng-template>',
+})
+class TestComponent {
+}
