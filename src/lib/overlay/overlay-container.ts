d4f0a1f95e0b5888654a6771a3b76866447125a0
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-07-31 09:01:10 +0200

    APM-122733 fix(overlay): fixes imports, remove tabindex and disabled

--- src/lib/overlay/overlay-container.ts
+++ src/lib/overlay/overlay-container.ts
@@ -3,31 +3,10 @@ import {
   ViewEncapsulation,
   ComponentRef,
   EmbeddedViewRef,
-  ElementRef,
   Component,
   ViewChild,
-  Attribute,
 } from '@angular/core';
-import {
-  HasTabIndex,
-  mixinTabIndex,
-  mixinDisabled,
-} from '@dynatrace/angular-components/core';
 import { BasePortalOutlet, ComponentPortal, CdkPortalOutlet, TemplatePortal } from '@angular/cdk/portal';
-
-// Boilerplate for applying mixins to DtOverlay.
-class BasePortal extends BasePortalOutlet {
-  attachComponentPortal<T>(portal: ComponentPortal<T>): ComponentRef<T> {
-    const compRef = {};
-    return compRef as ComponentRef<T>;
-  }
-  attachTemplatePortal<C>(portal: TemplatePortal<C>): EmbeddedViewRef<C> {
-    const compRef = {};
-    return compRef as EmbeddedViewRef<C>;
-  }
-}
-
-export const _DtOverlayMixinBase = mixinTabIndex(mixinDisabled(BasePortal));
 
 @Component({
   moduleId: module.id,
@@ -38,17 +17,15 @@ export const _DtOverlayMixinBase = mixinTabIndex(mixinDisabled(BasePortal));
     'class': 'dt-overlay-container',
     'attr.aria-hidden': 'true',
   },
-  inputs: ['tabIndex'],
   encapsulation: ViewEncapsulation.Emulated,
   preserveWhitespaces: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DtOverlayContainer extends _DtOverlayMixinBase implements HasTabIndex {
+export class DtOverlayContainer extends BasePortalOutlet {
   @ViewChild(CdkPortalOutlet) _portalOutlet: CdkPortalOutlet;
 
-  constructor(@Attribute('tabindex') tabIndex: string, public elementRef: ElementRef) {
+  constructor() {
     super();
-    this.tabIndex = parseInt(tabIndex, 10) || 0;
   }
 
   /**
