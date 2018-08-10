9ff6c56758bda3fb9bd6775832093505f6a20ee4
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-08-10 15:26:14 +0200

    APM-138921 test(tabs): added tests for navigation feature

--- src/lib/tabs/navigation/tab-fragment-builder.ts
+++ src/lib/tabs/navigation/tab-fragment-builder.ts
@@ -1,4 +1,5 @@
-export class DtRouterFragmentBuilder {
+/** Builds the fragment for the url from a set of ids */
+export class DtTabsRouterFragmentBuilder {
   static build(ids: Set<string>): string {
     return ids.size > 0 ? Array.from(ids).join(',') : '';
   }
