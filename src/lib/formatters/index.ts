16ebd510b385e3a3d51874b97f7262454e272d02
Dorota.Zaranska <d.makotka@gmail.com>
2018-08-22 16:15:53 +0200

    APM-127601 - cleanup

--- src/lib/formatters/index.ts
+++ src/lib/formatters/index.ts
@@ -1,1 +1,6 @@
 export * from './formatters-module';
+export * from './unit';
+export * from './formatted-value';
+export * from './formatter-util';
+export * from './count/count';
+export * from './percent/percent';
