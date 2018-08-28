3b479ae69ddb94ecd174e2c546471a4f1d5c345b
Dorota.Zaranska <d.makotka@gmail.com>
2018-08-28 17:32:44 +0200

    APM-127601, APM-138307 - bytes

--- src/lib/formatters/unit.ts
+++ src/lib/formatters/unit.ts
@@ -1,6 +1,12 @@
 export const enum DtUnit {
   PERCENT = '%',
   COUNT = 'count',
+  BYTES = 'B',
+  KILO_BYTES = 'kB',
+  MEGA_BYTES = 'MB',
+  GIGA_BYTES = 'GB',
+  TERA_BYTES = 'TB',
+  PETA_BYTES = 'PB',
 }
 
 export const enum DtRateUnit {
