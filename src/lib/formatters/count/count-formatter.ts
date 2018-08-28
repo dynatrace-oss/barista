cbb6daa4daadec137757375cf44942d0d12a59aa
Dorota.Zaranska <d.makotka@gmail.com>
2018-08-28 07:05:20 +0200

    APM-127601 - cleanup

--- src/lib/formatters/count/count-formatter.ts
+++ src/lib/formatters/count/count-formatter.ts
@@ -1,21 +1,27 @@
 import { coerceNumberProperty } from '@angular/cdk/coercion';
-import { DtUnit } from '../unit';
-import { DtFormattedValue } from '../formatted-value';
+import { DtRateUnit, DtUnit } from '../unit';
+import { DtFormattedValue, FormattedData, SourceData } from '../formatted-value';
 import { adjustNumber } from '../number-formatter';
 
-export function formatCount(input: number, inputUnit: DtUnit | string = DtUnit.COUNT, inputRateUnit?: string): DtFormattedValue {
+export function formatCount(input: number, inputUnit: DtUnit | string = DtUnit.COUNT,
+                            inputRateUnit?: DtRateUnit | string): DtFormattedValue {
 
-  const formattedValue = new DtFormattedValue(input, inputUnit, inputRateUnit);
+  const inputData: SourceData = {
+    value: input,
+    unit: inputUnit,
+    rateUnit: inputRateUnit,
+    useAbbreviation: true,
+  };
+
   const value = (coerceNumberProperty(input, NaN));
+  const formattedData: FormattedData = {};
   if (!isNaN(value)) {
-    formattedValue.useAbbreviation = true;
-    formattedValue.transformedValue = value;
-    formattedValue.displayValue = adjustNumber(value, true);
-
-    formattedValue.displayUnit = inputUnit !== DtUnit.COUNT
+    formattedData.transformedValue = value;
+    formattedData.displayValue = adjustNumber(value, inputData.useAbbreviation);
+    formattedData.displayUnit = inputUnit !== DtUnit.COUNT
       ? inputUnit
       : undefined;
   }
 
-  return formattedValue;
+  return new DtFormattedValue(inputData, formattedData);
 }
