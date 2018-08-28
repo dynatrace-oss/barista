fd2afee61feaf581f27b5db06c2f08e634706dbe
Dorota.Zaranska <dorota.zaranska@dynatrace.com>
2018-08-28 17:55:05 +0200

    APM-127601, APM-138307 - kilobytes, megabytes

--- src/lib/formatters/formatters-module.ts
+++ src/lib/formatters/formatters-module.ts
@@ -4,16 +4,22 @@ import { DtCount } from './count/count';
 import { DtPercent } from './percent/percent';
 import { DtRate } from './rate/rate';
 import { DtBytes } from './bytes/bytes';
+import { DtKilobytes } from './bytes/kilobytes';
+import { DtMegabytes } from './bytes/megabytes';
 
 @NgModule({
   declarations: [
     DtBytes,
+    DtKilobytes,
+    DtMegabytes,
     DtCount,
     DtPercent,
     DtRate,
   ],
   exports: [
     DtBytes,
+    DtKilobytes,
+    DtMegabytes,
     DtCount,
     DtPercent,
     DtRate,
