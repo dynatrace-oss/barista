d39eb824a87b2df3d4aaa1ad0e18d18d3c6b8211
Dorota.Zaranska <dorota.zaranska@dynatrace.com>
2018-08-29 10:20:02 +0200

    APM-127601, APM-138307 - specs improvement

--- src/lib/formatters/bytes/bytes.spec.ts
+++ src/lib/formatters/bytes/bytes.spec.ts
@@ -2,6 +2,7 @@ import { inject, TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { DtUnit } from '../unit';
 import { DtBytes } from './bytes';
+import { KILO_MULTIPLIER } from '../number-formatter';
 
 describe('DtBytes', () => {
   interface TestCase {
@@ -24,6 +25,14 @@ describe('DtBytes', () => {
   describe('Transforming input without defined outputUnit', () => {
     [
       {
+        input: 0,
+        output: '0 B',
+      },
+      {
+        input: 1,
+        output: '1 B',
+      },
+      {
         input: 1000,
         output: '1 kB',
       },
@@ -34,6 +43,14 @@ describe('DtBytes', () => {
       {
         input: 12500000000,
         output: '12.5 GB',
+      },
+      {
+        input: 1250000000000,
+        output: '1.25 TB',
+      },
+      {
+        input: 7121000000000000,
+        output: '7.12 PB',
       },
     ].forEach((testCase: TestCase) => {
       it(`should display ${testCase.input} converted to auto unit`, inject([DtBytes], (pipe: DtBytes) => {
@@ -47,21 +64,39 @@ describe('DtBytes', () => {
     [
       {
         input: 1000,
-        factor: 1000,
+        factor: KILO_MULTIPLIER,
         inputUnit: DtUnit.BYTES,
         output: '1 kB',
       },
       {
         input: 1000,
-        factor: 1000,
+        factor: KILO_MULTIPLIER,
         inputUnit: DtUnit.KILO_BYTES,
         output: '1 MB',
       },
       {
         input: 1000,
-        factor: 1000,
+        factor: KILO_MULTIPLIER,
         inputUnit: DtUnit.MEGA_BYTES,
         output: '1 GB',
+      },
+      {
+        input: 1000,
+        factor: KILO_MULTIPLIER,
+        inputUnit: DtUnit.GIGA_BYTES,
+        output: '1 TB',
+      },
+      {
+        input: 1000,
+        factor: KILO_MULTIPLIER,
+        inputUnit: DtUnit.TERA_BYTES,
+        output: '1 PB',
+      },
+      {
+        input: 1000,
+        factor: KILO_MULTIPLIER,
+        inputUnit: DtUnit.PETA_BYTES,
+        output: '1,000 PB',
       },
     ].forEach((testCase: TestCase) => {
       it(`should display different result (${testCase.output})`, inject([DtBytes], (pipe: DtBytes) => {
