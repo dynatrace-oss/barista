73cd1b519c301fcb70de47b007dfa3f45db13ee2
Dorota.Zaranska <d.makotka@gmail.com>
2018-08-30 15:38:48 +0200

    APM-143707 - adjust rate formatting + spec

--- src/lib/formatters/rate/rate.spec.ts
+++ src/lib/formatters/rate/rate.spec.ts
@@ -2,6 +2,7 @@ import { inject, TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { DtRateUnit } from '../unit';
 import { DtRate } from './rate';
+import { DtBytes } from '../bytes/bytes';
 
 describe('DtRate', () => {
   interface TestCase {
@@ -16,11 +17,12 @@ describe('DtRate', () => {
       imports: [BrowserModule],
       providers: [
         DtRate,
+        DtBytes,
       ],
     });
   });
 
-  describe('Transforming input with input of type number', () => {
+  describe('Transforming input of type number', () => {
     [
       {
         input: 10000,
@@ -40,6 +42,33 @@ describe('DtRate', () => {
     ].forEach((testCase: TestCase) => {
       it(`should display ${testCase.input} without unit`, inject([DtRate], (pipe: DtRate) => {
         expect(pipe.transform(testCase.input, testCase.rateUnit).toString())
+          .toEqual(testCase.output);
+      }));
+    });
+  });
+
+  describe('Transforming input of type DtFormattedValue', () => {
+    [
+      {
+        input: 10000,
+        rateUnit: DtRateUnit.PER_MINUTE,
+        output: '10 kB/min',
+      },
+      {
+        input: 20000000,
+        rateUnit: DtRateUnit.PER_SECOND,
+        output: '20 MB/s',
+      },
+      {
+        input: 3000000000,
+        rateUnit: 'request',
+        output: '3 GB/request',
+      },
+    ].forEach((testCase: TestCase) => {
+      it(`should display ${testCase.input} orignal unit together with rate`,
+         inject([DtRate, DtBytes], (pipe: DtRate, otherPipe: DtBytes) => {
+        const formattedValue = otherPipe.transform(testCase.input);
+        expect(pipe.transform(formattedValue, testCase.rateUnit).toString())
           .toEqual(testCase.output);
       }));
     });
