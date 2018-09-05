d5ee048b3987949316399dfa0a4a1b7616d8f4f3
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-09-05 14:44:51 +0200

    APM-144052 chore(docs): remove commented code

--- src/docs/components/formatters/examples/rate-example.ts
+++ src/docs/components/formatters/examples/rate-example.ts
@@ -9,8 +9,8 @@ import { DtRateUnit } from '@dynatrace/angular-components';
     <dt-label>Value to be transformed</dt-label>
     <input dtInput #value [(ngModel)]="exampleValue"/>
   </dt-form-field>
-  <!--<p>per request: {{ exampleValue | dtRate:'request' }}</p>
-  <p>per second: {{ exampleValue | dtCount | dtRate:'s' }}</p>-->
+  <p>per request: {{ exampleValue | dtRate:'request' }}</p>
+  <p>per second: {{ exampleValue | dtCount | dtRate:'s' }}</p>
   <p>Chaining rate + bytes: {{ exampleValue | dtRate:'s' | dtBytes }}</p>
   <p>Chaining bytes + rate: {{ exampleValue | dtBytes | dtRate:'s' }}</p>
 
