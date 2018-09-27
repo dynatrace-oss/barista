aa90dbd340def9168ded256334a911cd4a00b4d9
David Laubreiter <david.laubreiter@dynatrace.com>
2018-09-27 09:41:13 +0200

    APM-146167 feat(microchart) Add columns example

---
+++ src/docs/components/chart/examples/micro-chart-columns-example.component.ts
@@ -0,0 +1,29 @@
+// tslint:disable:no-magic-numbers
+
+import { Component } from '@angular/core';
+import { generateData } from './chart-data-utils';
+import { OriginalClassName } from '../../../core/decorators';
+import { IndividualSeriesOptions, Options } from 'highcharts';
+
+@Component({
+  template: '<dt-micro-chart [options]="options" [series]="series"></dt-micro-chart>',
+})
+@OriginalClassName('MicroChartColumnsExampleComponent')
+export class MicroChartColumnsExampleComponent {
+  options: Options = {
+    chart: {
+      type: 'column',
+    },
+    xAxis: {
+      type: 'datetime',
+    },
+    yAxis: [
+      {},
+    ],
+  };
+
+  series: IndividualSeriesOptions[] = [{
+    name: 'Requests',
+    data: generateData(40, 200000, 300000, 1370304000000, 900000),
+  }];
+}
