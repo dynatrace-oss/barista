5d07b4d61c3f8f78ce0c64294237fefb02087b0e
David Laubreiter <david.laubreiter@dynatrace.com>
2018-09-28 12:44:32 +0200

    APM-146167 feat(microchart) Fix bugs with colors

--- src/lib/chart/microchart/micro-chart.ts
+++ src/lib/chart/microchart/micro-chart.ts
@@ -1,5 +1,5 @@
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef,
+  ChangeDetectionStrategy,
   Component,
   EventEmitter,
   forwardRef,
@@ -31,33 +31,38 @@ const SUPPORTED_CHART_TYPES = ['line', 'column'];
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DtMicroChart implements OnDestroy {
-  @ViewChild(forwardRef(() => DtChart)) dtChart: DtChart;
+  @ViewChild(forwardRef(() => DtChart)) private _dtChart: DtChart;
 
   private _series: Observable<DtChartSeries> | DtChartSeries | undefined;
   private _subscription = Subscription.EMPTY;
   private _options: DtChartOptions;
 
+  private _columnSeries: boolean;
+  private _minDataPoint: DataPoint;
+  private _maxDataPoint: DataPoint;
+
   constructor(@Optional() @SkipSelf() private readonly _theme: DtTheme) {
     if (this._theme) {
       this._subscription = this._theme._stateChanges.subscribe(() => {
-        this._colorizeChart(this._options);
-        this.dtChart.options = merge({}, this._options);
+        if (this._options) {
+          this._colorizeChart(this._options);
+          this._colorizeMinMaxDataPoints();
+          this._dtChart.options = merge({}, this._options);
+        }
       });
     }
   }
-
-  @Output() readonly updated: EventEmitter<void> = new EventEmitter<void>();
 
   @Input()
   set options(options: DtChartOptions) {
     DtMicroChart._checkUnsupportedOptions(options);
     this._options = merge({}, DEFAULT_CHART_MICROCHART_OPTIONS, options);
-    DtMicroChart._transformAxis(this._options);
+    this._transformAxis(this._options);
     this._colorizeChart(this._options);
-    this.dtChart.options = this._options;
+    this._dtChart.options = this._options;
   }
   get options(): DtChartOptions {
-    return this.dtChart.options;
+    return this._dtChart.options;
   }
 
   @Input()
@@ -68,12 +73,14 @@ export class DtMicroChart implements OnDestroy {
     return this._series;
   }
 
+  @Output() readonly updated: EventEmitter<void> = new EventEmitter<void>();
+
   get seriesIds(): Array<string | undefined> | undefined {
-    return this.dtChart.seriesIds;
+    return this._dtChart.seriesIds;
   }
 
   get highchartsOptions(): Options {
-    return this.dtChart.highchartsOptions;
+    return this._dtChart.highchartsOptions;
   }
 
   ngOnDestroy(): void {
@@ -92,14 +99,14 @@ export class DtMicroChart implements OnDestroy {
     }
 
     this._series = series;
-    this.dtChart.series = transformed;
+    this._dtChart.series = transformed;
   }
 
   private _colorizeChart(options: DtChartOptions): void {
     MicroChartColorizer.apply(options, this._theme);
   }
 
-  private static _transformAxis(options: DtChartOptions): void {
+  private _transformAxis(options: DtChartOptions): void {
     if (options.xAxis) { DtMicroChart._mergeAxis(options.xAxis); }
     if (options.yAxis) { DtMicroChart._mergeAxis(options.yAxis); }
   }
@@ -123,27 +130,56 @@ export class DtMicroChart implements OnDestroy {
       return [series];
     }
 
+    this._columnSeries =
+      series.type === 'column' || this.highchartsOptions.chart !== undefined && this.highchartsOptions.chart.type === 'column';
+
     const dataPoints = DtMicroChart._normalizeData(series.data);
     const values = dataPoints.map((point: DataPoint) => point.y) as number[];
 
     const minIndex = values.lastIndexOf(Math.min(...values));
     const maxIndex = values.lastIndexOf(Math.max(...values));
 
-    if (this.isColumnSeries(series)) {
-      const palette = MicroChartColorizer.getPalette(this._theme);
-      merge(dataPoints[minIndex], MINMAX_DATAPOINT_OPTIONS, COLUMN_MINMAX_DATAPOINT_OPTIONS, { borderColor: palette.secondary});
-      merge(dataPoints[maxIndex], MINMAX_DATAPOINT_OPTIONS, COLUMN_MINMAX_DATAPOINT_OPTIONS, { borderColor: palette.secondary});
-    } else {
-      merge(dataPoints[minIndex], MINMAX_DATAPOINT_OPTIONS, LINE_MIN_DATAPOINT_OPTIONS);
-      merge(dataPoints[maxIndex], MINMAX_DATAPOINT_OPTIONS, LINE_MAX_DATAPOINT_OPTIONS);
-    }
+    this._minDataPoint = dataPoints[minIndex];
+    this._maxDataPoint = dataPoints[maxIndex];
+
+    this._decorateMinMaxDataPoints();
 
     series.data = dataPoints;
     return [series];
   }
 
-  private isColumnSeries(series: DtChartSeries): boolean {
-    return series.type === 'column' || this.highchartsOptions.chart !== undefined && this.highchartsOptions.chart.type === 'column';
+  private _decorateMinMaxDataPoints(): void {
+    if (this._columnSeries) {
+      merge(this._minDataPoint, MINMAX_DATAPOINT_OPTIONS, COLUMN_MINMAX_DATAPOINT_OPTIONS);
+      merge(this._maxDataPoint, MINMAX_DATAPOINT_OPTIONS, COLUMN_MINMAX_DATAPOINT_OPTIONS);
+    } else {
+      merge(this._minDataPoint, MINMAX_DATAPOINT_OPTIONS, LINE_MIN_DATAPOINT_OPTIONS);
+      merge(this._maxDataPoint, MINMAX_DATAPOINT_OPTIONS, LINE_MAX_DATAPOINT_OPTIONS);
+    }
+
+    this._colorizeMinMaxDataPoints();
+  }
+
+  private _colorizeMinMaxDataPoints(): void {
+    const palette = MicroChartColorizer.getPalette(this._theme);
+    if (this._columnSeries) {
+      merge(this._minDataPoint, { borderColor: palette.secondary });
+      merge(this._maxDataPoint, { borderColor: palette.secondary });
+    } else {
+      const options: DataPoint = {
+        marker: {
+          lineColor: palette.secondary,
+          states: {
+            hover: {
+              lineColor: palette.secondary,
+              fillColor: palette.tertiary,
+            },
+          },
+        },
+      };
+      merge(this._minDataPoint, options);
+      merge(this._maxDataPoint, options);
+    }
   }
 
   /**
