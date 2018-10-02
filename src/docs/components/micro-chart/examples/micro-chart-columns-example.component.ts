247131404bd8b9317b1ed59e62222719efda96b1
David Laubreiter <david.laubreiter@dynatrace.com>
2018-10-02 11:38:32 +0200

    APM-146167 feat(microchart) Fix build issues

--- src/docs/components/micro-chart/examples/micro-chart-columns-example.component.ts
+++ src/docs/components/micro-chart/examples/micro-chart-columns-example.component.ts
@@ -2,16 +2,15 @@
 
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
-import { Options } from 'highcharts';
-import {DtChartSeries} from '@dynatrace/angular-components';
-import {generateData} from "../../chart/examples/chart-data-utils";
+import { DtChartOptions, DtMicroChartSeries } from '@dynatrace/angular-components';
+import { generateData } from '../../chart/examples/chart-data-utils';
 
 @Component({
   template: '<dt-micro-chart [options]="options" [series]="series"></dt-micro-chart>',
 })
 @OriginalClassName('MicroChartColumnsExampleComponent')
 export class MicroChartColumnsExampleComponent {
-  options: Options = {
+  options: DtChartOptions = {
     chart: {
       type: 'column',
     },
@@ -22,7 +21,7 @@ export class MicroChartColumnsExampleComponent {
     },
   };
 
-  series: DtChartSeries = {
+  series: DtMicroChartSeries = {
     name: 'Requests',
     data: generateData(40, 200000, 300000, 1370304000000, 900000),
   };
