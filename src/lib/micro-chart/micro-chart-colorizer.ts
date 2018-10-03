b220ad63d89aef25770e2477d72f208e54218fee
David Laubreiter <david.laubreiter@dynatrace.com>
2018-10-03 10:06:44 +0200

    APM-146167 feat(microchart) Allow theming colors with placeholders

--- src/lib/micro-chart/micro-chart-colorizer.ts
+++ src/lib/micro-chart/micro-chart-colorizer.ts
@@ -1,70 +1,76 @@
 import { Colors, DtTheme } from '@dynatrace/angular-components/theming';
 import { DtChartOptions } from '@dynatrace/angular-components/chart';
-import { merge } from 'lodash';
+import { merge, isPlainObject, isArray } from 'lodash';
+import { DataPoint } from 'highcharts';
 
-export interface DtMicroChartColorPalette {
+interface MicroChartColorPalette {
   primary: string;
   secondary: string;
   tertiary: string;
 }
 
-const purple: DtMicroChartColorPalette = {
+const purple: MicroChartColorPalette = {
   primary: Colors.PURPLE_400,
   secondary: Colors.PURPLE_700,
   tertiary: Colors.PURPLE_200,
 };
 
-const blue: DtMicroChartColorPalette = {
+const blue: MicroChartColorPalette = {
   primary: Colors.ROYALBLUE_400,
   secondary: Colors.ROYALBLUE_700,
   tertiary: Colors.ROYALBLUE_200,
 };
 
-const royalblue: DtMicroChartColorPalette = {
+const royalblue: MicroChartColorPalette = {
   primary: Colors.ROYALBLUE_400,
   secondary: Colors.ROYALBLUE_800,
   tertiary: Colors.ROYALBLUE_200,
 };
 
-export const DT_MICROCHART_PALETTES = {
+const MICROCHART_PALETTES = {
   purple,
   blue,
   royalblue,
 };
 
-export function getPalette(theme: DtTheme | undefined): DtMicroChartColorPalette {
-  return theme && theme.name && DT_MICROCHART_PALETTES[theme.name] ? DT_MICROCHART_PALETTES[theme.name] : purple;
+export const enum DtMicroChartColorPlaceholder {
+  PRIMARY = '!color:primary',
+  SECONDARY = '!color:secondary',
+  TERTIARY = '!color:tertiary',
 }
 
-export function colorizeOptions(options: DtChartOptions, theme: DtTheme | undefined): void {
+function getPalette(theme: DtTheme | undefined): MicroChartColorPalette {
+  return theme && theme.name && MICROCHART_PALETTES[theme.name] ? MICROCHART_PALETTES[theme.name] : purple;
+}
+
+function applyColors(options: object, palette: MicroChartColorPalette): void {
+  let value;
+  Object.keys(options).forEach((key) => {
+    value = options[key];
+
+    if (value === DtMicroChartColorPlaceholder.PRIMARY) {
+      options[key] = palette.primary;
+    } else if (value === DtMicroChartColorPlaceholder.SECONDARY) {
+      options[key] = palette.secondary;
+    } else if (value === DtMicroChartColorPlaceholder.TERTIARY) {
+      options[key] = palette.tertiary;
+    } else if (isPlainObject(value) || isArray(value)) {
+      applyColors(value, palette);
+    }
+  });
+}
+
+export function colorizeOptions(options: Readonly<DtChartOptions>, theme: DtTheme | undefined): DtChartOptions {
+  const palette = getPalette(theme);
+  const result = merge({}, options) as DtChartOptions;
+
+  applyColors(result, palette);
+
+  return result;
+}
+
+export function colorizeDataPoint(point: DataPoint, theme: DtTheme | undefined): void {
   const palette = getPalette(theme);
 
-  options.colors = [palette.primary];
-
-  const colorOptions = {
-    column: {
-      states: {
-        hover: {
-          color: palette.tertiary,
-        },
-        select: {
-          color: palette.secondary,
-        },
-      },
-    },
-    series: {
-      marker: {
-        states: {
-          hover: {
-            fillColor: palette.tertiary,
-          },
-          select: {
-            fillColor: palette.secondary,
-          },
-        },
-      },
-    },
-  };
-
-  merge(options.plotOptions, colorOptions);
+  applyColors(point, palette);
 }
