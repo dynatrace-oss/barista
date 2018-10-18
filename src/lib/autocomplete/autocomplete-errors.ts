6f83bcc7ffd502feed3e7dd6717dd8fef85bf828
Thomas Pink <thomas.pink@me.com>
2018-10-18 14:22:08 +0200

    APM-149910 feat(autocomplete): finished logic

---
+++ src/lib/autocomplete/autocomplete-errors.ts
@@ -0,0 +1,6 @@
+/** Creates an error to be thrown when attempting to use an autocomplete trigger without a panel. */
+export function getDtAutocompleteMissingPanelError(): Error {
+  return Error('Attempting to open an undefined instance of `dt-autocomplete`. ' +
+    'Make sure that the id passed to the `dtAutocomplete` is correct and that ' +
+    'you\'re attempting to open it after the ngAfterContentInit hook.');
+}
