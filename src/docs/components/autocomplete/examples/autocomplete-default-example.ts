9aa0b90cac52fdc123606c8fd83710f7f9fe1f0d
Thomas Pink <thomas.pink@me.com>
2018-10-18 14:22:08 +0200

    APM-149910 feat(autocomplete): finished logic

--- src/docs/components/autocomplete/examples/autocomplete-default-example.ts
+++ src/docs/components/autocomplete/examples/autocomplete-default-example.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
+import { COUNTRIES, countryCompareWithFn } from './countries';
 
 @Component({
   moduleId: module.id,
-  template: '<input type="text" dtInput dtAutocomplete>',
+  template: `
+  <input dtInput placeholder="Country" [dtAutocomplete]="auto" [(ngModel)]="value" >
+  <dt-autocomplete #auto="dtAutocomplete">
+    <dt-option *ngFor="let country of countries" [value]="country.name">{{country.name}}</dt-option>
+  </dt-autocomplete>
+  `,
 })
 @OriginalClassName('DefaultAutocompleteExample')
 export class DefaultAutocompleteExample {
+
+  value: string;
+
+  get countries(): any[] {
+    return COUNTRIES.filter((country) => this.value ? countryCompareWithFn(country, this.value) : true);
+  }
 }
