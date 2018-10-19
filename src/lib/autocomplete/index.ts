7993105e604761b59d0132c198e0fcc3ded666fa
Thomas Pink <thomas.pink@me.com>
2018-10-19 11:26:28 +0200

    APM-149910 feat(autocomplete): unit tests

--- src/lib/autocomplete/index.ts
+++ src/lib/autocomplete/index.ts
@@ -2,3 +2,4 @@ export * from './autocomplete-module';
 export * from './autocomplete';
 export * from './autocomplete-trigger';
 export * from './autocomplete-origin';
+export * from './autocomplete-errors';
