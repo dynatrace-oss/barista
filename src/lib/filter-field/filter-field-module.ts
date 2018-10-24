304dbe2cb22f975cdee34ce4ec2035d0befc015d
Thomas Pink <thomas.pink@me.com>
2018-10-24 17:23:38 +0200

    APM-151315 feat(filter-field): added autocomplete & nodes

--- src/lib/filter-field/filter-field-module.ts
+++ src/lib/filter-field/filter-field-module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { DtIconModule } from '@dynatrace/angular-components/icon';
-import { DtFilterField } from './filter-field';
-import { _DtFilterFieldTag } from './filter-field-tag';
 import { DtButtonModule } from '@dynatrace/angular-components/button';
+import { DtOptionModule } from '@dynatrace/angular-components/core';
+import { DtAutocompleteModule } from '@dynatrace/angular-components/autocomplete';
+import { DtFilterField } from './filter-field';
+import { DtFilterFieldOptions } from './filter-field-options';
+import { _DtFilterFieldTag } from './filter-field-tag/filter-field-tag';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     DtIconModule,
     DtButtonModule,
+    DtOptionModule,
+    DtAutocompleteModule,
   ],
   exports: [
+    DtAutocompleteModule,
+    DtOptionModule,
     DtFilterField,
     _DtFilterFieldTag,
+    DtFilterFieldOptions,
   ],
   declarations: [
     DtFilterField,
     _DtFilterFieldTag,
+    DtFilterFieldOptions,
   ],
 })
 export class DtFilterFieldModule { }
