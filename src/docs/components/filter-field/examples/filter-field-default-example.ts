85f69ed8c8ca04361b399f222fda889455b5ea12
Thomas Pink <thomas.pink@dynatrace.com>
2018-10-24 17:23:38 +0200

    APM-151315 feat(filter-field): added autocomplete & nodes

--- src/docs/components/filter-field/examples/filter-field-default-example.ts
+++ src/docs/components/filter-field/examples/filter-field-default-example.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
 
+const STATES = [
+  {
+    name: 'Oberösterreich',
+    value: 'OOE',
+    cities: ['Linz', 'Wels', 'Steyr', 'Leonding', 'Traun', 'Vöcklabruck'],
+  },
+  {
+    name: 'Niederösterreich',
+    value: 'NOE',
+    cities: ['St. Pölten', 'Melk', 'Krems', 'St. Valentin', 'Amstetten'],
+  },
+  {
+    name: 'Wien',
+    value: 'W',
+  },
+  {
+    name: 'Burgenland',
+    value: 'B',
+  },
+  {
+    name: 'Steiermark',
+    value: 'SMK',
+  },
+  {
+    name: 'Kärnten',
+    value: 'KTN',
+  },
+  {
+    name: 'Tirol',
+    value: 'TRL',
+  },
+  {
+    name: 'Vorarlberg',
+    value: 'VRB',
+  },
+  {
+    name: 'Salzburg',
+    value: 'SBZ',
+  },
+];
+
 @Component({
   moduleId: module.id,
   template: `
-    <dt-filter-field></dt-filter-field>
+    <dt-filter-field (inputChange)="_handleInputChange($event)">
+      <dt-autocomplete [displayWith]="optionsDisplayFn">
+        <dt-option *ngFor="let category of categories" [value]="category">{{category.name}}</dt-option>
+      </dt-autocomplete>
+    </dt-filter-field>
   `,
 })
 @OriginalClassName('DefaultFilterFieldExample')
-export class DefaultFilterFieldExample { }
+export class DefaultFilterFieldExample {
+  categories: any[] | null = STATES;
+
+  optionsDisplayFn = (value: any) => value.name;
+
+  _handleInputChange(value: string): void {
+    console.log(value);
+  }
+
+  constructor() {
+
+  }
+}
