2d704f3b546dbc45ce86ceb58f0dab4614601dd6
Thomas Pink <thomas.pink@me.com>
2018-10-30 17:11:29 +0100

    APM-151315 feat(filter-field): submit active filter

--- src/lib/filter-field/nodes/filter-field-node.ts
+++ src/lib/filter-field/nodes/filter-field-node.ts
@@ -1,21 +1,25 @@
 export class DtFilterFieldNode {
-  constructor(public parent: DtFilterFieldGroup | null = null) {}
+  constructor(public parent: DtFilterFieldGroup | null = null, public viewValue?: string) {}
+  toString(): string { return this.viewValue || ''; }
 }
 
 export class DtFilterFieldGroup extends DtFilterFieldNode {
   constructor(
     public nodes: DtFilterFieldNode,
+    viewValue?: string,
     parent?: DtFilterFieldGroup | null) {
-    super(parent);
+    super(parent, viewValue);
   }
 }
 
 export class DtFilterFieldNodeValue<T> {
   constructor(public value: T, public viewValue: string) {}
+  toString(): string { return this.viewValue; }
 }
 
 export class DtFilterFieldNodeText {
   constructor(public value: string) {}
+  toString(): string { return `"${this.value}"`; }
 }
 
 // tslint:disable-next-line:no-any
@@ -24,8 +28,17 @@ export type DtFilterFieldNodeProperty = DtFilterFieldNodeValue<any> | DtFilterFi
 export class DtFilterFieldFilterNode extends DtFilterFieldNode {
   constructor(
     public properties: DtFilterFieldNodeProperty[] = [],
+    viewValue?: string,
     parent?: DtFilterFieldGroup | null) {
-    super(parent);
+    super(parent, viewValue);
+  }
+
+  toString(): string {
+    // tslint:disable-next-line:no-magic-numbers
+    const lastNodes = this.properties.slice(-2);
+    return this.viewValue ? this. viewValue :
+      (lastNodes.length ?
+        (lastNodes.length > 1 ? `${lastNodes[0].toString()}: ${lastNodes[1].toString()}` : lastNodes[0].toString()) : '');
   }
 }
 
