26268da7de95bc68a42ea84af047ae155cdfb6ce
Thomas Pink <thomas.pink@dynatrace.com>
2018-10-31 15:09:50 +0100

    APM-151315 fix(filter-field): fixes build

--- src/lib/filter-field/index.ts
+++ src/lib/filter-field/index.ts
@@ -1,4 +1,5 @@
 export * from './filter-field';
 export * from './filter-field-module';
 export * from './filter-field-options';
+export * from './filter-field-tag/filter-field-tag';
 export * from './nodes/filter-field-node';
