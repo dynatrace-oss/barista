029749ab611a84749624a89323504a11afe19994
Thomas Pink <thomas.pink@dynatrace.com>
2018-11-06 14:56:11 +0100

    APM-151315 feat(filter-field): free text, improved example

--- src/docs/components/filter-field/examples/filter-field-default-example.ts
+++ src/docs/components/filter-field/examples/filter-field-default-example.ts
@@ -1,59 +1,36 @@
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
 import { DtActiveFilterChangeEvent, DtFilterFieldFilterNode, DtFilterFieldNodeValue } from '@dynatrace/angular-components';
+import { FILTER_FIELD_EXAMPLE_DATA, ComplexType, isAutocomplete, getViewValue, AutocompleteItemType, isGroup, ItemType, isFreeText } from './data';
 
-interface State { name: string; value: string; cities?: string[]; }
-
-const STATES = [
-  {
-    name: 'Oberösterreich',
-    value: 'OOE',
-    cities: ['Linz', 'Wels', 'Steyr', 'Leonding', 'Traun', 'Vöcklabruck'],
-  },
-  {
-    name: 'Niederösterreich',
-    value: 'NOE',
-    cities: ['St. Pölten', 'Melk', 'Krems', 'St. Valentin', 'Amstetten'],
-  },
-  {
-    name: 'Wien',
-    value: 'W',
-  },
-  {
-    name: 'Burgenland',
-    value: 'B',
-  },
-  {
-    name: 'Steiermark',
-    value: 'SMK',
-  },
-  {
-    name: 'Kärnten',
-    value: 'KTN',
-  },
-  {
-    name: 'Tirol',
-    value: 'TRL',
-  },
-  {
-    name: 'Vorarlberg',
-    value: 'VRB',
-  },
-  {
-    name: 'Salzburg',
-    value: 'SBZ',
-  },
-];
+function filterItems(items: AutocompleteItemType[], needle: string): AutocompleteItemType[] {
+  if (needle.length) {
+    const neeldeUC = needle.toLocaleUpperCase();
+    return items
+      .map((item: AutocompleteItemType) => isGroup(item) ? {
+        name: item.name,
+        // tslint:disable-next-line:no-any
+        group: filterItems(item.group, needle) as any[],
+      } : item)
+      .filter((item: AutocompleteItemType) =>
+        (isGroup(item) ? !!item.group.length : getViewValue(item).toUpperCase().indexOf(neeldeUC) !== -1));
+  }
+  return items;
+}
 
 @Component({
   moduleId: module.id,
   template: `
     <dt-filter-field (inputChange)="_inputValue = $event" (activeFilterChange)="_handleActiveFilterChange($event)">
-      <dt-autocomplete *ngIf="filteredStates" [displayWith]="stateDisplayFn" autoActiveFirstOption>
-        <dt-option *ngFor="let state of filteredStates" [value]="state">{{state.name}}</dt-option>
-      </dt-autocomplete>
-      <dt-autocomplete *ngIf="filteredCities" autoActiveFirstOption>
-        <dt-option *ngFor="let city of filteredCities" [value]="city">{{city}}</dt-option>
+      <dt-autocomplete *ngIf="filteredAutocompleteItems" [displayWith]="displayFn" autoActiveFirstOption>
+        <ng-container *ngFor="let item of filteredAutocompleteItems">
+          <dt-optgroup *ngIf="item.group; else options" [label]="item.name">
+            <dt-option *ngFor="let groupItem of item.group" [value]="groupItem">{{displayFn(groupItem)}}</dt-option>
+          </dt-optgroup>
+          <ng-template #options>
+            <dt-option [value]="item">{{displayFn(item)}}</dt-option>
+          </ng-template>
+        </ng-container>
       </dt-autocomplete>
     </dt-filter-field>
   `,
@@ -62,34 +39,33 @@ const STATES = [
 export class DefaultFilterFieldExample {
   _inputValue = '';
 
-  private _states: State[] | null = STATES;
-  private _cities: string[] | null = null;
+  private _currentItem: ComplexType | null = FILTER_FIELD_EXAMPLE_DATA;
 
-  get filteredStates(): State[] | null {
-    const value = this._inputValue.toUpperCase();
-    return this._states && value.length ?
-      this._states.filter((state) => state.name.toUpperCase().indexOf(value) !== -1) : this._states;
+  get filteredAutocompleteItems(): AutocompleteItemType[] | null {
+    if (isAutocomplete(this._currentItem)) {
+      return filterItems(this._currentItem.autocomplete, this._inputValue);
+    } else if (isFreeText(this._currentItem) && this._currentItem.suggestions.length) {
+      return filterItems(this._currentItem.suggestions, this._inputValue);
+    }
+    return null;
   }
 
-  get filteredCities(): string[] | null {
-    const value = this._inputValue.toUpperCase();
-    return this._cities && value.length ? this._cities.filter((city) => city.toUpperCase().indexOf(value) !== -1) : this._cities;
-  }
-
-  stateDisplayFn = (value: State) => value.name;
+  // tslint:disable-next-line:no-any
+  displayFn = (value: any) => getViewValue(value);
 
   _handleActiveFilterChange(event: DtActiveFilterChangeEvent): void {
     const activeNode = event.activeNode as DtFilterFieldFilterNode;
-    if (activeNode.properties.length === 1) {
-      const item = (activeNode.properties[0] as DtFilterFieldNodeValue<State>).value;
-      if (item.cities) {
-        this._cities = item.cities;
-        this._states = null;
+    if (activeNode.properties.length) {
+      const item = (activeNode.properties[activeNode.properties.length - 1] as DtFilterFieldNodeValue<ItemType>).value;
+      if (isAutocomplete(item)) {
+        this._currentItem = item;
+        return;
+      } else if (isFreeText(item)) {
+        this._currentItem = item;
         return;
       }
     }
     event.submitActiveFilter();
-    this._cities = null;
-    this._states = STATES;
+    this._currentItem = FILTER_FIELD_EXAMPLE_DATA;
   }
 }
