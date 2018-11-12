8bb99c26fdfd1de9e0fce4be3719fab9fd289fd6
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-11-12 09:59:09 +0100

    APM-127611 chore(table): add correct comments, remove unused code

--- src/lib/table/sort/sort-errors.ts
+++ src/lib/table/sort/sort-errors.ts
@@ -1,9 +1,9 @@
 /** @internal */
-export function getSortHeaderNotContainedWithinSortError(): Error {
+export function getDtSortHeaderNotContainedWithinSortError(): Error {
   return Error(`DtSortHeader must be placed within a parent element with the DtSort directive.`);
 }
 
 /** @internal */
-export function getSortInvalidDirectionError(direction: string): Error {
+export function getDtSortInvalidDirectionError(direction: string): Error {
   return Error(`${direction} is not a valid sort direction ('asc' or 'desc').`);
 }
