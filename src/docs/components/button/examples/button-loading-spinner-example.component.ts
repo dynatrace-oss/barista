2324d5e8acacfeefc128226c5961c62b3be36118
Lukas Holzer <lukas.holzer@typeflow.cc>
2018-11-12 15:32:06 +0100

    APM-153125 fix(button): pull request feedback for the loading spinner button

--- src/docs/components/button/examples/button-loading-spinner-example.component.ts
+++ src/docs/components/button/examples/button-loading-spinner-example.component.ts
@@ -4,8 +4,8 @@ import { OriginalClassName } from '../../../core/decorators';
 @Component({
   moduleId: module.id,
   template: `
-  <button dt-loading-button disabled><dt-loading-spinner></dt-loading-spinner>Waiting for response</button>
-  <button dt-loading-button disabled variant="secondary"><dt-loading-spinner></dt-loading-spinner>Waiting for response</button>`,
+  <button dt-button disabled><dt-loading-spinner></dt-loading-spinner>Waiting for response</button>
+  <button dt-button disabled variant="secondary"><dt-loading-spinner></dt-loading-spinner>Waiting for response</button>`,
   styles: ['.dt-button + .dt-button { margin-left: 8px; }'],
 })
 @OriginalClassName('LoadingSpinnerButtonExampleComponent')
