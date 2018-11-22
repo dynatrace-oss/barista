a22f8c05ac05d135d0f0ce90baef18d5a1f598ee
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-11-22 13:15:41 +0100

    APM-151039 feat(table): add problem capabilities

--- src/lib/table/table-problem.ts
+++ src/lib/table/table-problem.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation, ElementRef } from '@angular/core';
+import { CanColor, mixinColor, Constructor } from '@dynatrace/angular-components/core';
+
+export type DtTableProblemThemePalette = 'error' | 'warning';
+
+// Boilerplate for applying mixins to DtButton.
+export class DtTableProblemBase {
+  constructor(public _elementRef: ElementRef) { }
+}
+export const _DtTableProblemBase =
+  mixinColor<Constructor<DtTableProblemBase>, DtTableProblemThemePalette>(DtTableProblemBase, 'error');
 
 @Component({
   selector: 'dt-table-problem',
   templateUrl: './table-problem.html',
   styleUrls: ['./table-problem.scss'],
+  inputs: ['color'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  preserveWhitespaces: false,
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class DtTableProblem {
-  color: 'error' | 'warning' = 'error';
-}
+export class DtTableProblem extends _DtTableProblemBase implements CanColor { }
