37e9b3a05578ecafcfd2ad6bcd06e6bb24bb86d0
Thomas Pink <thomas.pink@me.com>
2018-11-22 14:31:00 +0100

    APM-154712 chore: linting issues in ui-tests, disabled deprecated linting rule

--- ui-tests/chart-ui.spec.ts
+++ ui-tests/chart-ui.spec.ts
@@ -15,7 +15,7 @@ describe('chart', () => {
 
     it('should only trigger docheck once on init', async () => {
       expect(await element(by.id('change-detection-counter')).getText()).toBe('1');
-      await browser.actions().mouseMove(await element(by.css('.dt-chart')), { x: 100, y: 100 }).perform();
+      await browser.actions().mouseMove(element(by.css('.dt-chart')), { x: 100, y: 100 }).perform();
       expect(await element(by.id('change-detection-counter')).getText()).toBe('1');
     });
   });
--- ui-tests/tabs-ui.spec.ts
+++ ui-tests/tabs-ui.spec.ts
@@ -4,18 +4,18 @@ describe('Tabs', () => {
   beforeEach(async () => browser.get('/tabs'));
 
   it('should execute click handlers when not disabled', async () => {
-    element(by.id('packets')).click();
+    await element(by.id('packets')).click();
     const packetsTab = element(by.id('dt-tab-group-1-content-packets'));
     const qualityTab = element(by.id('dt-tab-group-1-content-quality'));
     expect(await packetsTab.getText()).toEqual('Packets');
     expect(await packetsTab.getAttribute('class')).toContain('dt-tab-body-active');
-    element(by.id('quality')).click();
+    await element(by.id('quality')).click();
     expect(await qualityTab.getText()).toEqual('Quality');
     expect(await qualityTab.getAttribute('class')).toContain('dt-tab-body-active');
     expect(await packetsTab.getAttribute('class')).not.toContain('dt-tab-body-active');
   });
   it('should not execute click handlers when item disabled', async () => {
-    element(by.id('traffic')).click();
+    await element(by.id('traffic')).click();
     const packetsTab = element(by.id('dt-tab-group-1-content-packets'));
     const trafficTab = element(by.id('dt-tab-group-1-content-traffic'));
     expect(await packetsTab.getText()).toEqual('Packets');
