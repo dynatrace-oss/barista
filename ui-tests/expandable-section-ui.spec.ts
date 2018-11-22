5718834484cd096aad8aa774b44175ebf5eef64a
Thomas Pink <thomas.pink@me.com>
2018-11-22 14:31:00 +0100

    APM-154712 chore: linting issues in ui-tests, disabled deprecated linting rule

--- ui-tests/expandable-section-ui.spec.ts
+++ ui-tests/expandable-section-ui.spec.ts
@@ -2,36 +2,36 @@ import { browser, by, element } from 'protractor';
 
 describe('expandable-section', () => {
   describe('behavior', () => {
-    beforeEach(() => browser.get('/expandable-section'));
+    beforeEach(async () => browser.get('/expandable-section'));
 
     it('should toggle', async () => {
-      element(by.id('btnToggle')).click();
+      await element(by.id('btnToggle')).click();
       expect(await element(by.id('open')).getText()).toEqual('1');
-      element(by.id('btnToggle')).click();
+      await element(by.id('btnToggle')).click();
       expect(await element(by.id('open')).getText()).toEqual('0');
     });
 
     it('should execute click handlers when not disabled', async () => {
       expect(await element(by.id('open')).getText()).toEqual('0');
-      element(by.id('btnOpen')).click();
+      await element(by.id('btnOpen')).click();
       expect(await element(by.id('open')).getText()).toEqual('1');
     });
 
     it('should not execute click handlers when disabled', async () => {
-      element(by.id('btnDisable')).click();
-      element(by.id('btnOpen')).click();
+      await element(by.id('btnDisable')).click();
+      await element(by.id('btnOpen')).click();
       expect(await element(by.id('open')).getText()).toEqual('0');
     });
 
     it('should close after disabling open section', async () => {
-      element(by.id('btnOpen')).click();
+      await element(by.id('btnOpen')).click();
       expect(await element(by.id('open')).getText()).toEqual('1');
-      element(by.id('btnDisable')).click();
+      await element(by.id('btnDisable')).click();
       expect(await element(by.id('open')).getText()).toEqual('0');
     });
 
     it('should open on label click', async () => {
-      element(by.id('lblHeader')).click();
+      await element(by.id('lblHeader')).click();
       expect(await element(by.id('open')).getText()).toEqual('1');
     });
 
