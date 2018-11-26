3f22e33664f312116edc54ac4f693fc815e9402d
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-11-26 13:19:47 +0100

    APM-151039 feat(table): add indicator support

---
+++ src/lib/core/indicator/indicator.spec.ts
@@ -0,0 +1,116 @@
+import { Component, ViewChild } from '@angular/core';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DtCoreModule, DtIndicator } from '@dynatrace/angular-components';
+
+fdescribe('DtIndicator without table', () => {
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        DtCoreModule,
+      ],
+      declarations: [
+        DtIndicatorBasic,
+        DtIndicatorWithActive,
+        DtIndicatorColor,
+        DtIndicatorWarning,
+      ],
+    });
+
+    TestBed.compileComponents();
+  }));
+
+  it('should add the dt-indicator-active class by default', () => {
+    const fixture = TestBed.createComponent(DtIndicatorBasic);
+    fixture.detectChanges();
+
+    const indicator: HTMLSpanElement =
+      fixture.debugElement.query(By.css('.dt-indicator')).nativeElement;
+
+    expect(indicator.classList.contains('dt-indicator')).toBe(true);
+    expect(indicator.classList.contains('dt-indicator-active')).toBe(true);
+  });
+
+  it('should be able to set the active state', () => {
+    const fixture = TestBed.createComponent(DtIndicatorWithActive);
+    fixture.detectChanges();
+
+    const indicator: HTMLSpanElement =
+      fixture.debugElement.query(By.css('.dt-indicator')).nativeElement;
+
+    expect(indicator.classList.contains('dt-indicator-active')).toBe(false);
+
+    fixture.componentInstance.active = true;
+    fixture.detectChanges();
+    expect(indicator.classList.contains('dt-indicator-active')).toBe(true);
+  });
+
+  it('should set the color to warning', () => {
+    const fixture = TestBed.createComponent(DtIndicatorWarning);
+    fixture.detectChanges();
+
+    const indicator: HTMLSpanElement =
+      fixture.debugElement.query(By.css('.dt-indicator')).nativeElement;
+
+    expect(indicator.classList.contains('dt-indicator-active')).toBe(true);
+    expect(indicator.classList.contains('dt-color-warning')).toBe(true);
+  });
+
+  it('should set the color on a binding', () => {
+    const fixture = TestBed.createComponent(DtIndicatorColor);
+    fixture.detectChanges();
+
+    const indicator: HTMLSpanElement =
+      fixture.debugElement.query(By.css('.dt-indicator')).nativeElement;
+
+    expect(indicator.classList.contains('dt-indicator-active')).toBe(true);
+    expect(indicator.classList.contains('dt-color-warning')).toBe(true);
+    expect(indicator.classList.contains('dt-color-error')).toBe(false);
+
+    fixture.componentInstance.color = 'error';
+    fixture.detectChanges();
+
+    expect(indicator.classList.contains('dt-color-error')).toBe(true);
+    expect(indicator.classList.contains('dt-color-warning')).toBe(false);
+  });
+
+  it('should complete the `stateChanges` stream for the dtIndicator on destroy', () => {
+    const fixture = TestBed.createComponent(DtIndicatorBasic);
+    fixture.detectChanges();
+
+    const indicator = fixture.componentInstance.indicator;
+    const completeSpy = jasmine.createSpy('complete spy');
+    const subscription = indicator._stateChanges.subscribe(undefined, undefined, completeSpy);
+
+    fixture.destroy();
+    expect(completeSpy).toHaveBeenCalled();
+    subscription.unsubscribe();
+  });
+
+});
+
+@Component({
+  template: `<span dtIndicator></span>`,
+})
+class DtIndicatorBasic {
+  @ViewChild(DtIndicator) indicator: DtIndicator;
+}
+
+@Component({
+  template: `<span [dtIndicator]="active"></span>`,
+})
+class DtIndicatorWithActive {
+  active = false;
+}
+
+@Component({
+  template: `<span dtIndicator dtIndicatorColor="warning"></span>`,
+})
+class DtIndicatorWarning { }
+
+@Component({
+  template: `<span dtIndicator [dtIndicatorColor]="color"></span>`,
+})
+class DtIndicatorColor {
+  color = 'warning';
+}
