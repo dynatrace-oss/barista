7a73b7574928f8e9116a35974ae66fa891fc727c
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-11-26 16:44:04 +0100

    APM-151039 chore(table): rename to indicator module

--- src/lib/core/indicator/indicator.ts
+++ src/lib/core/indicator/indicator.ts
@@ -1,17 +1,17 @@
 import { Directive, ElementRef, Input, OnDestroy, OnChanges } from '@angular/core';
-import { Constructor, mixinColor, CanColor } from '../common-behaviours';
+import { Constructor, mixinColor, CanColor } from '../common-behaviours/index';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Subject } from 'rxjs';
 
 export type DtIndicatorThemePalette = 'error' | 'warning' | undefined;
 
 // Boilerplate for applying mixins to DtIndicator.
-export class DtIndicatornBase {
+export class DtIndicatorBase {
   constructor(public _elementRef: ElementRef) { }
 }
 
 export const _DtIndicatorMixinBase =
-  mixinColor<Constructor<DtIndicatornBase>, DtIndicatorThemePalette>(DtIndicatornBase);
+  mixinColor<Constructor<DtIndicatorBase>, DtIndicatorThemePalette>(DtIndicatorBase);
 
 @Directive({
   selector: '[dtIndicator]',
