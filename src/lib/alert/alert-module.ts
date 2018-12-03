4154e4da07c1c63bdbf3ce03b4fdc14fbca048fc
Thomas Pink <thomas.pink@dynatrace.com>
2018-12-03 15:38:13 +0100

    APM-151152 fix(alert): Replaced inline svg with dtIcons

--- src/lib/alert/alert-module.ts
+++ src/lib/alert/alert-module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {DtAlert} from './alert';
 import {CommonModule} from '@angular/common';
+import { DtIconModule } from '@dynatrace/angular-components/icon';
 
 @NgModule({
   imports: [
     CommonModule,
+    DtIconModule,
   ],
   exports: [
     DtAlert,
