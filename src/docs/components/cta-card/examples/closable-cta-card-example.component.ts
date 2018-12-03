d66271959f68a2d3dab8d9ebfb2ea0c5b1e71d9c
Alexander Lagler <alexander.lagler@dynatrace.com>
2018-12-03 10:25:20 +0100

    APM-154732 rework based on thomas' review

--- src/docs/components/cta-card/examples/closable-cta-card-example.component.ts
+++ src/docs/components/cta-card/examples/closable-cta-card-example.component.ts
@@ -7,12 +7,12 @@ import {OriginalClassName} from '../../../core/decorators';
     <div class="demo-card">
       <dt-cta-card>
         <dt-cta-card-title i18n>You're missing out on log analytics!</dt-cta-card-title>
-        <dt-cta-card-action>
+        <dt-cta-card-footer-actions>
           <a dt-button color="cta" i18n>Enable log analysis</a>
-        </dt-cta-card-action>
-        <dt-cta-card-title-action>
+        </dt-cta-card-footer-actions>
+        <dt-cta-card-title-actions>
           <a dt-button variant="secondary" color="cta" style="padding: 0 3px 0 7px;"><dt-icon name="abort"></dt-icon></a>
-        </dt-cta-card-title-action>
+        </dt-cta-card-title-actions>
         <dt-cta-card-image><img src="/assets/cta-noagent.svg"></dt-cta-card-image>
         <span i18n>
       Enable Dynatrace log analytics to automatically correlate host-process log data with problems detected in your
