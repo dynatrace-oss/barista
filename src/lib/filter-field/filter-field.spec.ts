121e93ad021a42ecfc36246f276e8989b077d97e
Thomas Pink <thomas.pink@dynatrace.com>
2018-12-05 15:08:13 +0100

    APM-153102 test(filter-field): More unit test

--- src/lib/filter-field/filter-field.spec.ts
+++ src/lib/filter-field/filter-field.spec.ts
@@ -1,17 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed, async, ComponentFixture, flushMicrotasks, fakeAsync } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture, flushMicrotasks, fakeAsync, flush } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DtFilterFieldModule, DtIconModule } from '@dynatrace/angular-components';
-import { DtFilterField } from '@dynatrace/angular-components/filter-field/filter-field';
+import { ENTER } from '@angular/cdk/keycodes';
+import { DtFilterFieldModule, DtFilterField, DtIconModule, DtAutocomplete } from '@dynatrace/angular-components';
 import { typeInElement } from '../../testing/type-in-element';
 import { dispatchKeyboardEvent } from '../../testing/dispatch-events';
-import { ENTER } from '@angular/cdk/keycodes';
-import { DtAutocomplete } from '@dynatrace/angular-components/autocomplete';
 
 // tslint:disable:no-use-before-declare
-fdescribe('DtFilterField', () => {
+describe('DtFilterField', () => {
 
+  // tslint:disable-next-line:no-any
   function configureDtSelectTestingModule(declarations: any[]): void {
     TestBed.configureTestingModule({
       imports: [
@@ -26,13 +25,17 @@ fdescribe('DtFilterField', () => {
   describe('core', () => {
     let fixture: ComponentFixture<DtFormFieldBasic>;
     let filterFieldEl: HTMLElement;
+    let inputEl: HTMLInputElement;
 
     beforeEach(async(() => {
       configureDtSelectTestingModule([
         DtFormFieldBasic,
       ]);
       fixture = TestBed.createComponent(DtFormFieldBasic);
+      fixture.detectChanges();
+
       filterFieldEl = fixture.debugElement.query(By.css('dt-filter-field')).nativeElement;
+      inputEl = fixture.debugElement.query(By.css('.dt-filter-field-input')).nativeElement;
     }));
 
     it('should set the filter-by label', () => {
@@ -59,12 +62,43 @@ fdescribe('DtFilterField', () => {
 
       expect(label.textContent).toBe('Filter');
     });
+
+    it('should focus the input field when focusing the host', () => {
+      const input = fixture.debugElement.query(By.css('.dt-filter-field-input')).nativeElement;
+      fixture.componentInstance.filterField.focus();
+      expect(document.activeElement).toBe(input);
+    });
+
+    it('should not emit an activeFilterChange when focusing the filter field', () => {
+      const spy = jasmine.createSpy('active filter spy');
+      const subscription = fixture.componentInstance.filterField.activeFilterChange.subscribe(spy);
+
+      fixture.componentInstance.filterField.focus();
+
+      expect(spy).not.toHaveBeenCalled();
+      subscription.unsubscribe();
+    });
+
+    it('should emit the inputChange event when typing into the input field', () => {
+      const spy = jasmine.createSpy('filter input spy');
+      const subscription = fixture.componentInstance.filterField.inputChange.subscribe(spy);
+
+      typeInElement('x', inputEl);
+      fixture.detectChanges();
+
+      expect(spy).toHaveBeenCalledWith('x');
+
+      typeInElement('xy', inputEl);
+      fixture.detectChanges();
+
+      expect(spy).toHaveBeenCalledWith('xy');
+      subscription.unsubscribe();
+    });
   });
 
   describe('with free text', () => {
     let fixture: ComponentFixture<DtFormFieldBasic>;
-    let filterFieldEl: HTMLElement;
-    let freeTextInputField: HTMLInputElement;
+    let inputEl: HTMLInputElement;
 
     beforeEach(async(() => {
       configureDtSelectTestingModule([
@@ -72,57 +106,15 @@ fdescribe('DtFilterField', () => {
       ]);
       fixture = TestBed.createComponent(DtFormFieldBasic);
       fixture.detectChanges();
-      filterFieldEl = fixture.debugElement.query(By.css('dt-filter-field')).nativeElement;
-      freeTextInputField = fixture.debugElement.query(By.css('.dt-filter-field-free-text')).nativeElement;
+      inputEl = fixture.debugElement.query(By.css('.dt-filter-field-input')).nativeElement;
     }));
 
-    it('should focus the text input field when no other input type has been provided', () => {
-      fixture.componentInstance.filterField.focus();
-      expect(document.activeElement).toBe(freeTextInputField);
-    });
-
-    it('should emit the inputChange event when typing into free text input field', () => {
-      const spy = jasmine.createSpy('filter input spy');
-      const subscription = fixture.componentInstance.filterField.inputChange.subscribe(spy);
-
-      typeInElement('x', freeTextInputField);
-      fixture.detectChanges();
-
-      expect(spy).toHaveBeenCalledWith('x');
-
-      typeInElement('xy', freeTextInputField);
-      fixture.detectChanges();
-
-      expect(spy).toHaveBeenCalledWith('xy');
-      subscription.unsubscribe();
-    });
-
-    it('should not emit an activeFilterChange when focusing the filter field with active free text', () => {
+    it('should emit an activeFilterChange event when hitting enter on a filled input field', () => {
       const spy = jasmine.createSpy('active filter spy');
       const subscription = fixture.componentInstance.filterField.activeFilterChange.subscribe(spy);
 
-      fixture.componentInstance.filterField.focus();
-
-      expect(spy).not.toHaveBeenCalled();
-      subscription.unsubscribe();
-    });
-
-    it('should not emit an activeFilterChange event when hitting enter on an empty input field', () => {
-      const spy = jasmine.createSpy('active filter spy');
-      const subscription = fixture.componentInstance.filterField.activeFilterChange.subscribe(spy);
-
-      dispatchKeyboardEvent(freeTextInputField, 'keyup', ENTER);
-
-      expect(spy).not.toHaveBeenCalled();
-      subscription.unsubscribe();
-    });
-
-    it('should emit an activeFilterChange event when hitting enter on an non empty input field', () => {
-      const spy = jasmine.createSpy('active filter spy');
-      const subscription = fixture.componentInstance.filterField.activeFilterChange.subscribe(spy);
-
-      typeInElement('x', freeTextInputField);
-      dispatchKeyboardEvent(freeTextInputField, 'keyup', ENTER);
+      typeInElement('x', inputEl);
+      dispatchKeyboardEvent(inputEl, 'keyup', ENTER);
 
       expect(spy).toHaveBeenCalledTimes(1);
       subscription.unsubscribe();
@@ -131,9 +123,7 @@ fdescribe('DtFilterField', () => {
 
   describe('with autocomplete', () => {
     let fixture: ComponentFixture<DtFormFieldWithAutocomplete>;
-    let filterFieldEl: HTMLElement;
-    let autocomplete: DtAutocomplete<number>;
-    let autocompleteTextInputField: HTMLInputElement;
+    let inputEl: HTMLInputElement;
 
     beforeEach(async(() => {
       configureDtSelectTestingModule([
@@ -141,32 +131,25 @@ fdescribe('DtFilterField', () => {
       ]);
       fixture = TestBed.createComponent(DtFormFieldWithAutocomplete);
       fixture.detectChanges();
-      autocomplete = fixture.componentInstance.autocomplete;
-      filterFieldEl = fixture.debugElement.query(By.css('dt-filter-field')).nativeElement;
-      autocompleteTextInputField = fixture.debugElement.query(By.css('.dt-filter-field-autocomplete')).nativeElement;
+      inputEl = fixture.debugElement.query(By.css('.dt-filter-field-input')).nativeElement;
     }));
 
-    it('should focus the text input of the autocomplete if an autocomplete has been provided', () => {
-      fixture.componentInstance.filterField.focus();
-      expect(document.activeElement).toBe(autocompleteTextInputField);
-    });
-
     it('should open the autocomplete if filter field is focused', () => {
-      expect(autocomplete.isOpen).toBe(false);
+      expect(fixture.componentInstance.autocomplete.first.isOpen).toBe(false);
       fixture.componentInstance.filterField.focus();
-      expect(autocomplete.isOpen).toBe(true);
+      expect(fixture.componentInstance.autocomplete.first.isOpen).toBe(true);
     });
 
-    it('should emit the inputChange event when typing into the autocomplete input field', () => {
+    it('should emit the inputChange event when typing into the input field with autocomplete', () => {
       const spy = jasmine.createSpy('autocomplete input spy');
       const subscription = fixture.componentInstance.filterField.inputChange.subscribe(spy);
 
-      typeInElement('x', autocompleteTextInputField);
+      typeInElement('x', inputEl);
       fixture.detectChanges();
 
       expect(spy).toHaveBeenCalledWith('x');
 
-      typeInElement('xy', autocompleteTextInputField);
+      typeInElement('xy', inputEl);
       fixture.detectChanges();
 
       expect(spy).toHaveBeenCalledWith('xy');
@@ -198,9 +181,9 @@ fdescribe('DtFilterField', () => {
     });
   });
 
-  describe('with changing input types', () => {
+  describe('with changing input types (free text, autocomplete)', () => {
     let fixture: ComponentFixture<DtFormFieldWithAutocomplete>;
-    let autocomplete: DtAutocomplete<number>;
+    let inputEl: HTMLInputElement;
 
     beforeEach(async(() => {
       configureDtSelectTestingModule([
@@ -208,26 +191,21 @@ fdescribe('DtFilterField', () => {
       ]);
       fixture = TestBed.createComponent(DtFormFieldWithAutocomplete);
       fixture.detectChanges();
-      autocomplete = fixture.componentInstance.autocomplete;
+      inputEl = fixture.debugElement.query(By.css('.dt-filter-field-input')).nativeElement;
     }));
 
-    it('should focus and free text input again after submitting a free text (freetext -> freetext)', () => {
+    it('should focus the input again after submitting a free text', () => {
       fixture.componentInstance.showAutocomplete = false;
       fixture.detectChanges();
-
-      let freeTextInput = fixture.debugElement.query(By.css('.dt-filter-field-free-text'));
 
       fixture.componentInstance.filterField.focus();
       fixture.detectChanges();
 
-      typeInElement('x', freeTextInput.nativeElement);
-      dispatchKeyboardEvent(freeTextInput.nativeElement, 'keyup', ENTER);
+      typeInElement('x', inputEl);
+      dispatchKeyboardEvent(inputEl, 'keyup', ENTER);
       fixture.detectChanges();
 
-      freeTextInput = fixture.debugElement.query(By.css('.dt-filter-field-free-text'));
-
-      expect(freeTextInput).toBeDefined('free text input should be visible again');
-      expect(freeTextInput.nativeElement).toBe(document.activeElement, 'free text input element should be focused again');
+      expect(document.activeElement).toBe(inputEl, 'input element should be focused again');
     });
 
     it('should focus and open autocomplete again when selecting an option in autocomplete (autocomplete -> autocomplete)',
@@ -240,53 +218,43 @@ fdescribe('DtFilterField', () => {
       fixture.detectChanges();
       flushMicrotasks();
 
-      const autocompleteInput = fixture.debugElement.query(By.css('.dt-filter-field-autocomplete'));
-      expect(autocompleteInput).toBeDefined('autocomplete input should be visible again');
-      expect(autocompleteInput.nativeNode).toBe(document.activeElement, 'autocomplete input element should be focused again');
-      expect(autocomplete.isOpen).toBe(true);
+      expect(document.activeElement).toBe(inputEl, 'input element should be focused again');
+      expect(fixture.componentInstance.autocomplete.first.isOpen).toBe(true);
     }));
 
-    // it('should focus and open autocomplete after submitting a free text (freetext -> autocomplete)', fakeAsync(() => {
-    //   fixture.componentInstance.showAutocomplete = false;
-    //   fixture.detectChanges();
+    it('should focus and open autocomplete after submitting a free text (freetext -> autocomplete)', fakeAsync(() => {
+      fixture.componentInstance.showAutocomplete = false;
+      fixture.detectChanges();
 
-    //   let freeTextInput = fixture.debugElement.query(By.css('.dt-filter-field-free-text'));
+      fixture.componentInstance.filterField.focus();
+      fixture.detectChanges();
 
-    //   fixture.componentInstance.filterField.focus();
-    //   fixture.detectChanges();
+      typeInElement('x', inputEl);
+      dispatchKeyboardEvent(inputEl, 'keyup', ENTER);
 
-    //   typeInElement('x', freeTextInput.nativeElement);
-    //   dispatchKeyboardEvent(freeTextInput.nativeElement, 'keyup', ENTER);
+      // switching to autocomplete
+      fixture.componentInstance.showAutocomplete = true;
+      fixture.detectChanges();
 
-    //   fixture.componentInstance.showAutocomplete = true; // switching to autocomplete
-    //   fixture.detectChanges();
-    //   flushMicrotasks();
+      expect(document.activeElement).toBe(inputEl, 'input element should be focused again');
+      expect(fixture.componentInstance.autocomplete.first.isOpen).toBe(true);
+    }));
 
-    //   const autocompleteInput = fixture.debugElement.query(By.css('.dt-filter-field-autocomplete'));
-    //   freeTextInput = fixture.debugElement.query(By.css('.dt-filter-field-free-text'));
+    it('should focus the input element after selecting an option in autocomplete (autocomplete -> freetext)', fakeAsync(() => {
+      fixture.componentInstance.filterField.focus();
+      fixture.detectChanges();
 
-    //   expect(freeTextInput).toBeNull('free text input should not be visible');
-    //   expect(autocompleteInput).toBeDefined('autocomplete input should be visible now');
-    //   expect(autocompleteInput.nativeNode).toBe(document.activeElement, 'autocomplete input element should be focused now');
-    //   expect(autocomplete.isOpen).toBe(true);
-    // }));
+      const firstOption = fixture.debugElement.query(By.css('.dt-option')).nativeElement;
+      firstOption.click();
+      fixture.detectChanges();
+      flushMicrotasks();
 
-    // it('should focus the free text input after selection an option in autocomplete (autocomplete -> freetext)', fakeAsync(() => {
-    //   fixture.componentInstance.filterField.focus();
-    //   fixture.detectChanges();
+      // switching to free text
+      fixture.componentInstance.showAutocomplete = false;
+      fixture.detectChanges();
 
-    //   const firstOption = fixture.debugElement.query(By.css('.dt-option')).nativeElement;
-    //   firstOption.click();
-    //   fixture.detectChanges();
-    //   flushMicrotasks();
-
-    //   const freeTextInput = fixture.debugElement.query(By.css('.dt-filter-field-free-text'));
-    //   const autocompleteInput = fixture.debugElement.query(By.css('.dt-filter-field-autocomplete'));
-
-    //   expect(autocompleteInput).toBeNull('autocomplete input should not be visible');
-    //   expect(freeTextInput).toBeDefined('free text input should be visible now');
-    //   expect(freeTextInput.nativeElement).toBe(document.activeElement, 'free text input element should be focused now');
-    // }));
+      expect(document.activeElement).toBe(inputEl, 'input element should be focused again');
+    }));
   });
 });
 
@@ -314,7 +282,7 @@ class DtFormFieldBasic {
 })
 class DtFormFieldWithAutocomplete {
   @ViewChild(DtFilterField) filterField: DtFilterField;
-  @ViewChild(DtAutocomplete) autocomplete: DtAutocomplete<number>;
+  @ViewChildren(DtAutocomplete) autocomplete: QueryList<DtAutocomplete<number>>;
 
   showAutocomplete = true;
 
