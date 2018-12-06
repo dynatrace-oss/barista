fe0f533682c94ac4acd65085102ed379b0df45c5
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-12-06 16:39:28 +0100

    APM-146161 refactor(selection-area): make it easier

---
+++ src/lib/selection-area/positioning-utils.spec.ts
@@ -0,0 +1,96 @@
+import { calculatePosition, DtSelectionAreaEventTarget } from './positioning-utils';
+
+// tslint:disable:no-magic-numbers
+
+describe('PositioningUtils', () => {
+
+  describe('calculatePosition', () => {
+    describe('selectedArea', () => {
+      it('should move the position to the right', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, 10, 100, 100, 300))
+          .toEqual({ left: 110, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, 10, 190, 100, 300))
+          .toEqual({ left: 200, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+      });
+      it('should not set the position outside the boundries when moving to the right', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, 10, 100, 100, 200))
+          .toEqual({ left: 100, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, 10, 100, 100, 205))
+          .toEqual({ left: 105, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+      });
+      it('should move the position to the left', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, -10, 100, 100, 300))
+          .toEqual({ left: 90, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+      });
+      it('should not set the position outside the boundries when moving to the left', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, -10, 0, 100, 200))
+          .toEqual({ left: 0, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+        expect(calculatePosition(DtSelectionAreaEventTarget.SelectedArea, -10, 5, 100, 205))
+          .toEqual({ left: 0, width: 100, nextTarget: DtSelectionAreaEventTarget.SelectedArea });
+      });
+    });
+
+    describe('left handle', () => {
+      it('should move the position to the left', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, -10, 100, 100, 300))
+          .toEqual({ left: 90, width: 110, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+      });
+
+      it('should not set the position outside the boundaries when moving to the left', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, -10, 0, 100, 300))
+          .toEqual({ left: 0, width: 100, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, -10, 5, 100, 300))
+          .toEqual({ left: 0, width: 105, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+      });
+
+      it('should move the position to the right', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, 10, 100, 100, 300))
+          .toEqual({ left: 110, width: 90, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+      });
+
+      it('should move the left handle over the right one', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, 10, 100, 5, 300))
+          .toEqual({ left: 105, width: 5, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+      });
+
+      it('should move the left handle over the right one incl clamp', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, 10, 299, 1, 300))
+          .toEqual({ left: 300, width: 0, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+        expect(calculatePosition(DtSelectionAreaEventTarget.LeftHandle, 10, 290, 5, 300))
+          .toEqual({ left: 295, width: 5, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+      });
+    });
+
+    describe('right 🐔', () => {
+      it('should move the position to the right', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, 10, 100, 100, 300))
+          .toEqual({ left: 100, width: 110, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+      });
+
+      it('should not set the position outside the boundaries when moving to the right', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, 10, 200, 100, 300))
+          .toEqual({ left: 200, width: 100, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, 10, 195, 100, 300))
+          .toEqual({ left: 195, width: 105, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+      });
+
+      it('should move the position to the left', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, -10, 100, 100, 300))
+          .toEqual({ left: 100, width: 90, nextTarget: DtSelectionAreaEventTarget.RightHandle });
+      });
+
+      it('should move the right handle over the left one', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, -10, 100, 5, 300))
+          .toEqual({ left: 95, width: 5, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+      });
+
+      it('should move the right handle over the left one incl clamp', () => {
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, -10, 1, 5, 300))
+          .toEqual({ left: 0, width: 1, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+        expect(calculatePosition(DtSelectionAreaEventTarget.RightHandle, -10, 0, 5, 300))
+          .toEqual({ left: 0, width: 0, nextTarget: DtSelectionAreaEventTarget.LeftHandle });
+      });
+    });
+  });
+
+});
---
+++ src/lib/selection-area/positioning-utils.ts
@@ -0,0 +1,85 @@
+import { LEFT_ARROW, UP_ARROW, RIGHT_ARROW, DOWN_ARROW, PAGE_UP, PAGE_DOWN, HOME, END } from '@angular/cdk/keycodes';
+import { clamp } from '@dynatrace/angular-components/core';
+
+/** The step size for the keyboard interaction on PAGE UP and PAGE DOWN */
+const DT_SELECTION_AREA_KEYBOARD_BIG_STEP = 10;
+
+/** @internal Eventtarget for the mouse events on the selection area */
+export enum DtSelectionAreaEventTarget {
+  SelectedArea = 'selected-area',
+  LeftHandle = 'left-handle',
+  RightHandle = 'right-handle',
+  Origin = 'origin',
+}
+
+/** Returns the offset for a keycode */
+export function getOffsetForKeyCode(keyCode: number, boundaryWidth: number): number {
+  switch (keyCode) {
+    case LEFT_ARROW:
+    case UP_ARROW:
+      return -1;
+    case RIGHT_ARROW:
+    case DOWN_ARROW:
+      return 1;
+    case PAGE_UP:
+      return -DT_SELECTION_AREA_KEYBOARD_BIG_STEP;
+    case PAGE_DOWN:
+      return DT_SELECTION_AREA_KEYBOARD_BIG_STEP;
+    case HOME:
+      return -boundaryWidth;
+    case END:
+      return boundaryWidth;
+    default:
+      return 0;
+  }
+}
+
+export function calculatePosition(
+  target: DtSelectionAreaEventTarget,
+  deltaX: number,
+  selectedAreaLeft: number,
+  selectedAreaWidth: number,
+  boundaryWidth: number
+): { left: number; width: number; nextTarget: DtSelectionAreaEventTarget } {
+  let left = 0;
+  let width = 0;
+  let nextTarget = target;
+  // tslint:disable-next-line:switch-default
+  switch (target) {
+    case DtSelectionAreaEventTarget.SelectedArea:
+      left = clamp(selectedAreaLeft + deltaX, 0, boundaryWidth - selectedAreaWidth);
+      return { left, width: selectedAreaWidth, nextTarget };
+    case DtSelectionAreaEventTarget.LeftHandle:
+      if (selectedAreaLeft + deltaX >= selectedAreaLeft + selectedAreaWidth) {
+        // left handle is moved over the right handle
+        left = selectedAreaLeft + selectedAreaWidth;
+        width = left >= boundaryWidth ? 0 : left - selectedAreaLeft;
+        nextTarget = DtSelectionAreaEventTarget.RightHandle;
+      } else {
+        // left handle is moved left of the right handle
+        left = selectedAreaLeft + deltaX;
+        width = left <= 0 ? selectedAreaWidth - (deltaX - left) : selectedAreaWidth - deltaX;
+        width = clamp(width, 0, boundaryWidth);
+      }
+
+      left = clamp(left, 0, boundaryWidth);
+      return { left, width, nextTarget };
+    case DtSelectionAreaEventTarget.RightHandle:
+      if (selectedAreaWidth + deltaX < 0) {
+        left = clamp(selectedAreaLeft + selectedAreaWidth + deltaX, 0, boundaryWidth);
+        width = selectedAreaLeft - left;
+        nextTarget = DtSelectionAreaEventTarget.LeftHandle;
+      } else {
+        left = selectedAreaLeft;
+        width = clamp(selectedAreaWidth + deltaX, 0, boundaryWidth - selectedAreaLeft);
+      }
+      return { left, width, nextTarget };
+    case DtSelectionAreaEventTarget.Origin:
+      nextTarget = deltaX >= 0 ? DtSelectionAreaEventTarget.RightHandle : DtSelectionAreaEventTarget.LeftHandle;
+      return calculatePosition(nextTarget, deltaX, selectedAreaLeft, selectedAreaWidth, boundaryWidth);
+  }
+}
+
+export function calculateDeltaX(oldX: number, newX: number): number {
+  return newX - oldX;
+}
