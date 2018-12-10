4327a6b8693f8898c7dd9321abe5c6b44b7807cc
Thomas Pink <thomas.pink@dynatrace.com>
2018-12-10 17:35:27 +0100

    APM-157007 build: added new version recommandation

--- tools/release/new-version-prompt.ts
+++ tools/release/new-version-prompt.ts
@@ -1,6 +1,12 @@
 import { ChoiceType, prompt } from 'inquirer';
 import { Version, parseVersionName } from './parse-version';
 import { createNewVersion, ReleaseType } from './create-version';
+import preset from './conventional-changelog-preset';
+
+// These imports lack type definitions.
+// tslint:disable:no-var-requires no-require-imports
+const conventionalRecommendedBump = require(`conventional-recommended-bump`);
+// tslint:enable:no-var-requires no-require-imports
 
 /** Answers that will be prompted for. */
 interface VersionPromptAnswers {
@@ -12,7 +18,14 @@ interface VersionPromptAnswers {
  * validated to be a proper increment of the specified current version.
  */
 export async function promptForNewVersion(currentVersion: Version): Promise<Version> {
-  const versionChoices: ChoiceType[] = [
+  const recommendedBump = await recommendBump(currentVersion);
+  const recommendVersion = createNewVersion(currentVersion, recommendedBump.releaseType);
+
+  console.log(recommendedBump.reason);
+  console.log(`Proposed Version based on commits: ${recommendVersion.format()}`);
+  console.log();
+
+  const versionChoices: Array<{ value: string; name: string; releaseType: ReleaseType }> = [
     createVersionChoice(currentVersion, 'major', 'Major release'),
     createVersionChoice(currentVersion, 'minor', 'Minor release'),
     createVersionChoice(currentVersion, 'patch', 'Patch release'),
@@ -23,17 +36,32 @@ export async function promptForNewVersion(currentVersion: Version): Promise<Vers
     name: 'proposedVersion',
     message: `What's the type of the new release?`,
     choices: versionChoices,
+    default: versionChoices.findIndex((choice) => choice.releaseType === recommendedBump.releaseType),
   }]);
 
-  return parseVersionName(answers.proposedVersion)!;
+  return parseVersionName(answers.proposedVersion);
 }
 
 /** Creates a new choice for selecting a version inside of an Inquirer list prompt. */
-function createVersionChoice(currentVersion: Version, releaseType: ReleaseType, message: string): { value: string; name: string } {
+function createVersionChoice(currentVersion: Version, releaseType: ReleaseType, message: string, recommended = false):
+  { value: string; name: string; releaseType: ReleaseType } {
   const versionName = createNewVersion(currentVersion, releaseType).format();
-
   return {
     value: versionName,
-    name: `${message} (${versionName})`,
+    name: `${message} (${versionName})${recommended ? ' [recommended]' : ''}`,
+    releaseType,
   };
+}
+
+/** Returns an object containing information for the recommended version bump. */
+async function recommendBump(currentVersion: Version): Promise<{ level: number; reason: string; releaseType: ReleaseType}> {
+  return new Promise<any>((resolve, reject) => {
+    conventionalRecommendedBump({ config: preset }, (error, recommendation) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(recommendation);
+      }
+    });
+  });
 }
