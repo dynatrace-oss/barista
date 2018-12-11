163dea3a4cc25fcf686b118636d9bdb595d7c5bf
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-12-11 12:21:57 +0100

    APM-146526 chore(tooltip): export it in the index

--- src/lib/chart/index.ts
+++ src/lib/chart/index.ts
@@ -4,3 +4,4 @@ export * from './chart-colors';
 export * from './chart-config';
 export * from './heatfield/index';
 export * from './selection-area/chart-selection-area-origin';
+export * from './tooltip/chart-tooltip';
