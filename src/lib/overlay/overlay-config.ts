5897c87f4f7ee89ad62c9904616b1ab4fd665aad
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-12-11 10:43:55 +0100

    APM-146526 chore(chart): cleanup tooltip code

--- src/lib/overlay/overlay-config.ts
+++ src/lib/overlay/overlay-config.ts
@@ -13,4 +13,11 @@ export class DtOverlayConfig {
    * if the overlay fits below the origin element and the top edge otherwise.
    */
   originY?: 'edge' | 'center' = 'center';
+
+  /** @internal Positiones passed to the overlay if you need to override the default positioning system */
+  _positions?: ConnectedPosition[];
+
+  /** Data passed to the overlay as the $implicit context object */
+  // tslint:disable-next-line:no-any
+  data?: any;
 }
