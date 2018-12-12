268e2d2306fb8dae9a016b8813cc6ebe1564d38e
Bartosz Bobin <bartosz.bobin@dynatrace.com>
2018-12-12 15:19:06 +0100

    APM-157575 feat(key-value-list): Add possibility to use html for key and value

--- src/lib/key-value-list/key-value-list.spec.ts
+++ src/lib/key-value-list/key-value-list.spec.ts
@@ -1,13 +1,17 @@
 
-import {async, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {Component} from '@angular/core';
 import {DtKeyValueListModule} from '@dynatrace/angular-components';
 
 describe('DtKeyValueList', () => {
+
+  const getKeyElement = (element: HTMLElement) => element.querySelector('.dt-key-value-list-item-key span');
+  const getValueElement = (element: HTMLElement) => element.querySelector('.dt-key-value-list-item-value span');
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [DtKeyValueListModule],
-      declarations: [TestApp, TestApp2, TestApp3],
+      declarations: [TestApp, TestApp2, TestApp3, TestAppContent],
     });
 
     TestBed.compileComponents();
@@ -40,6 +44,64 @@ describe('DtKeyValueList', () => {
           .toBeTruthy('Key Value list must contain 3 columns');
       });
   });
+
+  describe('attributes key value', () => {
+    let fixture: ComponentFixture<TestApp>;
+    let items: HTMLElement[];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestApp);
+      fixture.detectChanges();
+      items = fixture.debugElement.nativeElement.querySelectorAll('dt-key-value-list-item');
+    });
+
+    it('should display 1st item data properly', () => {
+      expect(getKeyElement(items[0])!.textContent).toBe('Temp');
+      expect(getValueElement(items[0])!.textContent).toBe('1');
+    });
+
+    it('should display 2nd item data properly', () => {
+      expect(getKeyElement(items[1])!.textContent).toBe('Temp1');
+      expect(getValueElement(items[1])!.textContent).toBe('13');
+    });
+
+    it('should display 3rd item data properly', () => {
+      expect(getKeyElement(items[2])!.textContent).toBe('Temp2');
+      expect(getValueElement(items[2])!.textContent).toBe('28');
+    });
+  });
+
+  describe('content key value', () => {
+    let fixture: ComponentFixture<TestAppContent>;
+    let items: HTMLElement[];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestAppContent);
+      fixture.detectChanges();
+      items = fixture.debugElement.nativeElement.querySelectorAll('.dt-key-value-list-item');
+    });
+
+    it('should display 1st item data properly', () => {
+      expect(getKeyElement(items[0])!.textContent).toBe('1st item content key');
+      expect(getValueElement(items[0])!.textContent).toBe('1st item content value');
+    });
+
+    it('should display 2nd item data properly', () => {
+      expect(getKeyElement(items[1])!.textContent).toBe('2nd item content key');
+      expect(getValueElement(items[1])!.textContent).toBe('2nd item content value');
+    });
+
+    it('should not display content key when attribute key is provided', () => {
+      expect(getKeyElement(items[2])!.textContent).toBe('3rd item attribute key');
+      expect(getValueElement(items[2])!.textContent).toBe('3rd item content value');
+    });
+
+    it('should not display content value when attribute value is provided', () => {
+      expect(getKeyElement(items[3])!.textContent).toBe('4th item content key');
+      expect(getValueElement(items[3])!.textContent).toBe('4th item attribute value');
+    });
+  });
+
 });
 
   /** Test component that contains an DtKeyValueList. */
@@ -108,4 +170,29 @@ class TestApp2 {
     `,
   })
 class TestApp3 {
+}
+
+@Component({
+    selector: 'dt-test-content',
+    template: `<dt-key-value-list>
+     <dt-key-value-list-item>
+       <ng-container key>1st item content key</ng-container>
+       <ng-container value>1st item content value</ng-container>
+     </dt-key-value-list-item>
+     <dt-key-value-list-item>
+       <ng-container key>2nd item content key</ng-container>
+       <ng-container value>2nd item content value</ng-container>
+     </dt-key-value-list-item>
+     <dt-key-value-list-item key="3rd item attribute key">
+       <ng-container key>3rd item content key</ng-container>
+       <ng-container value>3rd item content value</ng-container>
+     </dt-key-value-list-item>
+     <dt-key-value-list-item value="4th item attribute value">
+       <ng-container key>4th item content key</ng-container>
+       <ng-container value>4th item content value</ng-container>
+     </dt-key-value-list-item>
+  </dt-key-value-list>
+    `,
+  })
+class TestAppContent {
 }
