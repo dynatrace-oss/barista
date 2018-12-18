ebfc3e938f617f1b5a8a34883191e0f76c19947b
Thomas Pink <thomas.pink@dynatrace.com>
2018-12-18 16:18:04 +0100

    APM-157007 build: Fixes issue where breaking changes are not detected

--- tools/release/conventional-changelog-preset/parser-opts.ts
+++ tools/release/conventional-changelog-preset/parser-opts.ts
@@ -6,7 +6,6 @@ export default {
     `scope`,
     `subject`,
   ],
-  noteKeywords: [`BREAKING CHANGE`],
   revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
   revertCorrespondence: [`header`, `hash`],
 };
