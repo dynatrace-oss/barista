7e0705c2c57d9cbcacd9ace38a563850c4162386
Manfred DelFabro <manfred.delfabro@dynatrace.com>
2018-12-18 16:26:58 +0100

    APM-154685 feat(micro-chart): improve highcharts options, add labelFormatter input property, minor refactoring

--- src/docs/components/micro-chart/examples/micro-chart-default-example.component.ts
+++ src/docs/components/micro-chart/examples/micro-chart-default-example.component.ts
@@ -2,11 +2,11 @@
 
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
-import { DtChartOptions, DtChartSeries } from '@dynatrace/angular-components';
+import { DtChartOptions, DtChartSeries, formatCount } from '@dynatrace/angular-components';
 import { generateData } from './docs-micro-chart.service';
 
 @Component({
-  template: '<dt-micro-chart [series]="series"></dt-micro-chart>',
+  template: '<dt-micro-chart [series]="series" [labelFormatter]="_formatterFn"></dt-micro-chart>',
 })
 @OriginalClassName('MicroChartDefaultExampleComponent')
 export class MicroChartDefaultExampleComponent {
@@ -14,4 +14,6 @@ export class MicroChartDefaultExampleComponent {
     name: 'Requests',
     data: generateData(40, 1000, 2000, 1370304000000, 900000),
   };
+
+  _formatterFn = (input: number) => formatCount(input).toString();
 }
--- src/docs/components/micro-chart/examples/micro-chart-stream-example.component.ts
+++ src/docs/components/micro-chart/examples/micro-chart-stream-example.component.ts
@@ -3,14 +3,19 @@
 import { Component } from '@angular/core';
 import { OriginalClassName } from '../../../core/decorators';
 import { Observable } from 'rxjs';
-import { DtChartSeries } from '@dynatrace/angular-components';
+import { DtChartOptions, DtChartSeries } from '@dynatrace/angular-components';
 import { MicroChartService } from './docs-micro-chart.service';
 
 @Component({
-  template: '<dt-micro-chart [series]="series$"></dt-micro-chart>',
+  template: '<dt-micro-chart [options]="options" [series]="series$"></dt-micro-chart>',
 })
 @OriginalClassName('MicroChartStreamExampleComponent')
 export class MicroChartStreamExampleComponent {
+  options: DtChartOptions = {
+    chart: {
+      type: 'column',
+    },
+  };
   series$: Observable<DtChartSeries>;
 
   constructor(private _chartService: MicroChartService) {
