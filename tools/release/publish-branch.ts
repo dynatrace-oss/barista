2a84033d9e907c25a7ac0dd393c2d19e4d0a93c6
Thomas Pink <thomas.pink@me.com>
2018-12-18 09:56:15 +0100

    APM-157007 build: Added release version determination script for a release build

--- tools/release/publish-branch.ts
+++ tools/release/publish-branch.ts
@@ -1,4 +1,6 @@
 import {Version} from './parse-version';
+import { red, italic, bold } from 'chalk';
+import { GitClient } from './git-client';
 
 export type VersionType = 'major' | 'minor' | 'patch';
 
@@ -11,4 +13,18 @@ export function getSemverVersionType(version: Version): VersionType {
   } else {
     return 'patch';
   }
+}
+
+/** Verifies that the user is on the specified publish branch. */
+export function verifyPublishBranch(expectedPublishBranch: string, git: GitClient): boolean {
+  const currentBranchName = git.getCurrentBranch();
+
+  // Check if current branch matches the expected publish branch.
+  if (expectedPublishBranch !== currentBranchName) {
+    console.error(red(
+      `  ✘ Cannot stage release from "${italic(currentBranchName)}". Please ` +
+      `stage the release from "${bold(expectedPublishBranch)}".`));
+    return false;
+  }
+  return true;
 }
