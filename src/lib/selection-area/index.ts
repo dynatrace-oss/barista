746a951ebdadebfc01a8a223bedd35c44dcc7ac3
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-12-18 15:51:39 +0100

    APM-157877 chore(selection-area): add @dynamic so the build odes not break

--- src/lib/selection-area/index.ts
+++ src/lib/selection-area/index.ts
@@ -2,3 +2,4 @@ export * from './selection-area-module';
 export * from './selection-area';
 export * from './selection-area-actions';
 export * from './selection-area-origin';
+export * from './selection-area-container';
