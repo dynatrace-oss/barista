1b580d8a9f85afc56cec9089dbefaf5fa6f32241
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-01-03 14:30:52 +0100

    APM-150990 refactor(examples): add pure examples

---
+++ src/barista-examples/inline-editor/pure-inline-editor-example.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: '<em dt-inline-editor [(ngModel)]="sampleModel"></em>',
+})
+export class PureInlineEditorExample {
+}
