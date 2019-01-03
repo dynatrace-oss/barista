e3281266fc3895b7ce80a9a89e4710595ffccdc9
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-01-03 14:30:52 +0100

    APM-150990 refactor(examples): add pure examples

--- src/barista-examples/cta-card/default-cta-card-example.component.ts
+++ src/barista-examples/cta-card/default-cta-card-example.component.ts
@@ -9,12 +9,11 @@ import { Component } from '@angular/core';
       <a dt-button color="cta" i18n>View release</a>
     </dt-cta-card-footer-actions>
     <dt-cta-card-image><img src="/assets/cta-noagent.svg"></dt-cta-card-image>
-    <span i18n>
-      Deploy Dynatrace OneAgent via the Dynatrace OneAgent BOSH release to your Cloud Foundry foundation VMs. 
-      Get monitoring insights into all Cloud Foundry components including Diego cells, Gorouters, and more. 
-      Benefit from automatic monitoring of Cloud Foundry applications, down to the code and query level, thanks to built-in auto-injection for Garden-runC containers.
-      <a class="dt-link" color="cta" i18n>Read more...</a>
-    </span>
+    Deploy Dynatrace OneAgent via the Dynatrace OneAgent BOSH release to your Cloud Foundry foundation VMs.
+    Get monitoring insights into all Cloud Foundry components including Diego cells, Gorouters, and more.
+    Benefit from automatic monitoring of Cloud Foundry applications, down to the code and query level,
+    thanks to built-in auto-injection for Garden-runC containers.
+    <a class="dt-link" color="cta">Read more...</a>
   </dt-cta-card>
   </div>`,
 })
---
+++ src/barista-examples/switch/pure-switch-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: '<dt-switch>Default</dt-switch>',
+})
+export class PureSwitchExampleComponent { }
