33cfd6607a2c22cd02fe6395e6fd6da31b643a18
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-01-14 16:31:26 +0100

    APM-158287 fix(card, cta-card): fixes spacing between action buttons

--- src/dev-app/main.ts
+++ src/dev-app/main.ts
@@ -3,6 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { preserveWhitespaces: true })
+  .bootstrapModule(AppModule, { preserveWhitespaces: false })
   // tslint:disable-next-line:no-console typedef
   .catch((err): void => { console.log(err); });
