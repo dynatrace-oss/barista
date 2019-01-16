395e2ef4c8a3c72df5caa24e6650cf4a7d6da021
Katrin Freihofner <katrin.freihofner@dynatrace.com>
2019-01-16 14:30:42 +0100

    APM-158290 docs(context-dialog): added example with buttons -> context action dialog

--- src/dev-app/context-dialog/context-dialog-demo.component.ts
+++ src/dev-app/context-dialog/context-dialog-demo.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'context-dialog-demo',
   templateUrl: './context-dialog-demo.component.html',
   styleUrls: ['./context-dialog-demo.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContextDialogDemo {
 
