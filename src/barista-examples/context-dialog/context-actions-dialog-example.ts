1adb593ff17e4c8bf826c5a77bda0f8209ce1f31
anonymous <anonymous@dynatrace.com>
2019-01-16 14:30:42 +0100

    APM-158290 docs(context-dialog): added example with buttons -> context action dialog

---
+++ src/barista-examples/context-dialog/context-actions-dialog-example.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: `
+  <dt-context-dialog>
+  <button dt-button>First button</button>
+  <button dt-button variant="secondary">Second button</button>
+  <button dt-button variant="secondary">Third button</button>
+  </dt-context-dialog>`,
+  styles: [
+    `::ng-deep .dt-context-dialog-content {
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+    }
+    .dt-button + .dt-button {
+      margin-top: 8px;
+    }
+    `
+  ],
+})
+export class ContextActionsDialogExampleComponent {
+}
