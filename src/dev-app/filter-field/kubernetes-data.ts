b387ceee13bbb3c100598234199c2a8918991c19
Thomas Pink <thomas.pink@dynatrace.com>
2019-01-29 15:22:02 +0100

    APM-162767 feat(filter-field): added DataSource as main API entry point

---
+++ src/dev-app/filter-field/kubernetes-data.ts
@@ -0,0 +1,39 @@
+export const KUBERNETES_DATA = {
+  autocomplete: [
+    {
+      name: "Kubernetes Labels",
+      options: [
+        {
+          name: 'beta.kubernetes.io/arch',
+          value: 'beta.kubernetes.io/arch',
+          autocomplete: ['amd64', 'amd32'],
+          distinct: true,
+        },
+        {
+          name: 'beta.kubernetes.io/os',
+          value: 'beta.kubernetes.io/os',
+          autocomplete: ['linux', 'windows'],
+          distinct: true,
+        },
+        {
+          name: 'node-role.kubernetes.io/master (simple label)',
+          value: 'node-role.kubernetes.io/master',
+          distinct: true,
+        },
+      ],
+    },
+    {
+      name: 'Node',
+      options: [
+        {
+          name: 'Custom Simple Option',
+        },
+        {
+          name: 'Node Label',
+          key: 'MyKey',
+          suggestions: [],
+        },
+      ],
+    }
+  ]
+}
