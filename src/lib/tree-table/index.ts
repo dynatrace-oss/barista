0bbe00dcaa4b4157fb375386380b1135bfa67e9a
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-01-30 16:03:20 +0100

    APM-161042 chore(tree-table): add missing imports

--- src/lib/tree-table/index.ts
+++ src/lib/tree-table/index.ts
@@ -1,4 +1,5 @@
 export * from './tree-table-module';
 export * from './tree-table';
+export * from './tree-table-header-cell';
 export * from './tree-table-row';
 export * from './tree-table-toggle-cell';
