f6fb9d969c1e1a0da2515294028212a214a49e30
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-01-30 16:51:20 +0100

    chore(barista-examples): add pure example for radio button

---
+++ src/barista-examples/radio/pure-radio-example.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: '<dt-radio-button value="myValue">Radio button value</dt-radio-button>',
+})
+export class PureRadioExample { }
