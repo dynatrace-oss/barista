90c7cd31bdb70e926a736d3dad2d7885828c9f4e
anonymous <anonymous@dynatrace.com>
2019-02-06 13:54:33 +0100

    APM-158133 feat(info-group): adds new info group component

--- src/dev-app/info-group/info-group-demo.component.ts
+++ src/dev-app/info-group/info-group-demo.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <dt-info-group color="error">
-    <dt-info-group-icon>  <dt-icon name="agent"></dt-icon></dt-info-group-icon>
-    <dt-info-group-title>30%</dt-info-group-title>
-    CPU
-  </dt-info-group>`,
+  <dt-info-group>
+    <dt-info-group-icon><dt-icon name="agent"></dt-icon></dt-info-group-icon>
+    <dt-info-group-title>5 min 30 s</dt-info-group-title>
+    Session duration
+  </dt-info-group>
+  `,
 })
 export class InfoGroupDemo { }
