c7d4dedfa3f5ced391cf6bc766fb89b2172ae887
anonymous <anonymous@dynatrace.com>
2019-02-22 11:18:43 +0100

    APM-164885 refactor(info-group): replaces table specific info-group-cell component with info-group component

--- src/lib/table/info-group-cell/info-group-cell.ts
+++ src/lib/table/info-group-cell/info-group-cell.ts
@@ -1,5 +1,10 @@
 import { Component, ChangeDetectionStrategy, ViewEncapsulation, Directive } from '@angular/core';
 
+/**
+ * Defines the info group icon.
+ * @deprecated Use 'dt-info-group-icon' instead
+ * @breaking-change 3.0.0 To be removed
+ */
 @Directive({
   selector: 'dt-info-group-cell-icon, [dt-info-group-cell-icon], [dtInfoGroupCellIcon]',
   host: {
@@ -8,6 +13,11 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation, Directive } from
 })
 export class DtInfoGroupCellIcon {}
 
+/**
+ * Defines the info group title.
+ * @deprecated Use 'dt-info-group-title' instead
+ * @breaking-change 3.0.0 To be removed
+ */
 @Directive({
   selector: 'dt-info-group-cell-title, [dt-info-group-cell-title], [dtInfoGroupCellTitle]',
   host: {
@@ -16,6 +26,10 @@ export class DtInfoGroupCellIcon {}
 })
 export class DtInfoGroupCellTitle {}
 
+/**
+ * @deprecated Use 'dt-info-group' instead
+ * @breaking-change 3.0.0 To be removed
+ */
 @Component({
   selector: 'dt-info-group-cell',
   templateUrl: 'info-group-cell.html',
