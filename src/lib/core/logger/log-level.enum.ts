a79614cd5112e945a861572e2ae3abbc62875da4
Thomas Pink <thomas.pink@me.com>
2019-03-04 13:26:24 +0100

    fix(logger): removes log level WARN, use WARNING instead BREAKING CHANGE

--- src/lib/core/logger/log-level.enum.ts
+++ src/lib/core/logger/log-level.enum.ts
@@ -1,8 +1,5 @@
 export enum DtLogLevel {
   ERROR = 'ERROR',
-  // @deprecated Use `WARN` instead.
-  // @breaking-change 2.0.0 To be changed to `WARNING`
-  WARN = 'WARNING',
   WARNING = 'WARNING',
   INFO = 'INFO',
   DEBUG = 'DEBUG',
