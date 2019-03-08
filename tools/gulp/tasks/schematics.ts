00b0d91739315f231a2330fcc9aab9fb0a2f802c
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-03-08 13:47:03 +0100

    APM-167510 feat(linting): add setup to ship angular-components linting rules

--- tools/gulp/tasks/schematics.ts
+++ tools/gulp/tasks/schematics.ts
@@ -1,34 +1,15 @@
 import { task, src, dest, SrcOptions } from 'gulp';
-import { execNodeTask } from '../util/task-runner';
-import { join, resolve as resolvePath } from 'path';
+import { join } from 'path';
 import { buildConfig } from '../build-config';
-import { spawn } from 'child_process';
-import { readFile } from 'fs';
 import { red } from 'chalk';
 import { sequenceTask } from '../util/sequence-task';
+import { tscCompile } from '../util/tsc-compile';
 
 const gulpClean = require('gulp-clean');
 
 const SCHEMATICS_ROOT = join(buildConfig.projectDir, 'src/schematics');
 const SCHEMATICS_CONFIG_PATH = join(SCHEMATICS_ROOT, 'tsconfig.json');
 const SCHEMATICS_OUTDIR = join(buildConfig.projectDir, 'node_modules', '@dynatrace', 'components-schematics');
-
-/**
- * Spawns a child process that compiles using tsc.
- * @param flags Command-line flags to be passed to tsc.
- * @returns Promise that resolves/rejects when the child process exits.
- */
-export function tscCompile(flags: string[]) {
-  return new Promise((resolve, reject) => {
-    const ngcPath = resolvePath('./node_modules/.bin/tsc');
-    const childProcess = spawn(ngcPath, flags, {shell: true});
-
-    // Pipe stdout and stderr from the child process.
-    childProcess.stdout.on('data', (data: string|Buffer) => console.log(`${data}`));
-    childProcess.stderr.on('data', (data: string|Buffer) => console.error(red(`${data}`)));
-    childProcess.on('exit', (exitCode: number) => exitCode === 0 ? resolve() : reject());
-  });
-}
 
 task(':schematics:clean', () =>
   src(SCHEMATICS_OUTDIR, { read: false, allowEmpty: true } as SrcOptions)
