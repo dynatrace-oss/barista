2a7dca7b2ae77e365dec8537e6311606b77c7257
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-03-18 16:30:25 +0100

    APM-168690 chore(barista-examples): update text alternative for icon buttons

--- src/barista-examples/button/button-icon-only-example.component.ts
+++ src/barista-examples/button/button-icon-only-example.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-    <button dt-icon-button aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <a dt-icon-button href="#" aria-label="A disabled example button containing an agent icon"><dt-icon name="proxy"></dt-icon></a>
-    <button dt-icon-button variant="secondary" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button variant="nested" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="warning" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="warning" variant="secondary" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="warning" variant="nested" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="cta" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="cta" variant="secondary" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
-    <button dt-icon-button color="cta" variant="nested" aria-label="A disabled example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button aria-label="An example button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <a dt-icon-button href="#" aria-label="An example button containing a proxy icon"><dt-icon name="proxy"></dt-icon></a>
+    <button dt-icon-button variant="secondary" aria-label="An example secondary button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button variant="nested" aria-label="An example nested button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="warning" aria-label="An example warning button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="warning" variant="secondary" aria-label="An example warning button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="warning" variant="nested" aria-label="An example warning button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="cta" aria-label="An example CTA button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="cta" variant="secondary" aria-label="An example CTA button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
+    <button dt-icon-button color="cta" variant="nested" aria-label="An example CTA button containing an agent icon"><dt-icon name="agent"></dt-icon></button>
   `,
   styles: ['.dt-button + .dt-button { margin-left: 8px; }'],
 })
