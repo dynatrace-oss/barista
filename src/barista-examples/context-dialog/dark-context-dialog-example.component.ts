73723c6b81bc9b50967f21b04f4f72fc0d72f013
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-03-18 16:25:34 +0100

    APM-168690 chore(barista-examples): enable a11y linting rules and fix errors

--- src/barista-examples/context-dialog/dark-context-dialog-example.component.ts
+++ src/barista-examples/context-dialog/dark-context-dialog-example.component.ts
@@ -1,16 +1,20 @@
+// tslint:disable:max-line-length
 import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   template: `
   <div class="dark" dtTheme=":dark">
-  <dt-context-dialog>
-  <button dt-button variant="secondary">Edit</button>
-  </dt-context-dialog>
-    <button dt-icon-button [dtContextDialogTrigger]="darkIcondialog" variant="secondary"><dt-icon name="agent"></dt-icon></button>
+    <dt-context-dialog>
+      <button dt-button variant="secondary">Edit</button>
+    </dt-context-dialog>
+    <button dt-icon-button [dtContextDialogTrigger]="darkIcondialog" variant="secondary" aria-label="A disabled example button containing an agent icon">
+      <dt-icon name="agent"></dt-icon>
+    </button>
     <dt-context-dialog #darkIcondialog>
       <p>Your dashboard "real user monitoring"<br> is only visible to you</p>
     </dt-context-dialog>
   </div>`,
 })
 export class DarkContextDialogExampleComponent { }
+// tslint:enable:max-line-length
--- src/barista-examples/form-field/form-field-prefix-suffix-example.ts
+++ src/barista-examples/form-field/form-field-prefix-suffix-example.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <input type="text" dtInput placeholder="Please insert amout"/>
       <dt-icon dtPrefix name="filter">$</dt-icon>
       <dt-loading-spinner dtPrefix></dt-loading-spinner>
-      <button dt-icon-button dtSuffix variant="nested">
+      <button dt-icon-button dtSuffix variant="nested" aria-label="Submit changes">
         <dt-icon name="checkmark"></dt-icon>
       </button>
     </dt-form-field>
