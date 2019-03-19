7265a2f6b19a2fd71982e0a0983d2bb5f3b0e4b3
Katrin Freihofner <katrin.freihofner@dynatrace.com>
2019-03-19 15:17:25 +0100

    docs(filter-field): adds example for barista

--- src/barista-examples/filter-field/filter-field-default-example.ts
+++ src/barista-examples/filter-field/filter-field-default-example.ts
@@ -1,7 +1,59 @@
 import { Component } from '@angular/core';
+import { DtFilterFieldDefaultDataSource, DtFilterChangeEvent } from '@dynatrace/angular-components/filter-field';
 
 @Component({
   moduleId: module.id,
-  template: ``,
+  template: `
+  <dt-filter-field
+    [dataSource]="_dataSource"
+    label="Filter by">
+  </dt-filter-field>
+  `,
 })
-export class DefaultFilterFieldExample { }
+export class DefaultFilterFieldExample {
+
+  private TEST_DATA = {
+    autocomplete: [
+      {
+        name: 'Kubernetes Labels',
+        options: [
+          {
+            name: 'beta.kubernetes.io/arch',
+            value: 'beta.kubernetes.io/arch',
+            autocomplete: ['amd64', 'amd32'],
+            distinct: true,
+          },
+          {
+            name: 'beta.kubernetes.io/os',
+            value: 'beta.kubernetes.io/os',
+            autocomplete: ['linux', 'windows'],
+            distinct: true,
+          },
+          {
+            name: 'node-role.kubernetes.io/master (simple label)',
+            value: 'node-role.kubernetes.io/master',
+          },
+        ],
+      },
+      {
+        name: 'Node',
+        options: [
+          {
+            name: 'Custom Simple Option',
+          },
+          {
+            name: 'Node Label',
+            key: 'MyKey',
+            suggestions: [
+              'some cool',
+              'very weird',
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  _dataSource = new DtFilterFieldDefaultDataSource(this.TEST_DATA);
+
+}
