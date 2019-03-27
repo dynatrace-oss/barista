a95ef592775c153176c137742c5b99387f3e28dd
anonymous <anonymous@dynatrace.com>
2019-03-27 08:47:46 +0100

    docs(alert): adds example for error on dark bg

---
+++ src/barista-examples/alert/dark-error-alert-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: `<section class="dark" dtTheme=":dark">
+  <dt-alert severity="error">{{text}}</dt-alert>
+</section>`,
+})
+export class DarkErrorAlertExampleComponent {
+  text = `This is a error on dark background!`;
+}
