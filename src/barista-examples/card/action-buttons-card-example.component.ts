f5266c770c9e7ec74f937e46a46a85cf536bdebd
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-04 14:38:05 +0200

    APM-166163 docs(card): update example templates for better readability

--- src/barista-examples/card/action-buttons-card-example.component.ts
+++ src/barista-examples/card/action-buttons-card-example.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: `<div class="demo-card">
-<dt-card>
-  <dt-card-title>Top 3 JavaScript errors</dt-card-title>
-  <dt-card-subtitle>Some subtitle</dt-card-subtitle>
-  <dt-card-title-actions>
-    <button dt-button variant="secondary">Add to dashboard</button>
-    <button dt-button variant="secondary">Edit</button>
-  </dt-card-title-actions>
-  The card is not an interactive element, therefore, there are no hover, active and disabled card.
-</dt-card></div>`,
+  template: `
+<div class="demo-card">
+  <dt-card>
+    <dt-card-title>Top 3 JavaScript errors</dt-card-title>
+    <dt-card-subtitle>Some subtitle</dt-card-subtitle>
+    <dt-card-title-actions>
+      <button dt-button variant="secondary">Add to dashboard</button>
+      <button dt-button variant="secondary">Edit</button>
+    </dt-card-title-actions>
+    The card is not an interactive element, therefore there are no hover, active or disabled states.
+  </dt-card>
+</div>`,
   styles: ['.dt-button + .dt-button { margin-left: 8px;}; }'],
 })
 export class ActionButtonsCardExampleComponent { }
--- src/barista-examples/card/default-card-example.component.ts
+++ src/barista-examples/card/default-card-example.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: `<div class="demo-card">
-<dt-card>
-  <dt-card-title>Top 3 JavaScript errors</dt-card-title>
-  <dt-card-subtitle>Some subtitle</dt-card-subtitle>
-  <dt-card-title-actions><button dt-button variant="secondary">Some Action</button></dt-card-title-actions>
-  The card is not an interactive element, therefore, there are no hover, active and disabled card.
-</dt-card></div>`,
+  template: `
+<div class="demo-card">
+  <dt-card>
+    <dt-card-title>Top 3 JavaScript errors</dt-card-title>
+    <dt-card-subtitle>Some subtitle</dt-card-subtitle>
+    <dt-card-title-actions><button dt-button variant="secondary">Some Action</button></dt-card-title-actions>
+    The card is not an interactive element, therefore there are no hover, active or disabled states.
+  </dt-card>
+</div>`,
 })
 export class DefaultCardExampleComponent { }
