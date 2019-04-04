70efabe9566cffb9e396b9fb9b74a5a068ff13fa
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-04 14:38:05 +0200

    APM-166163 docs(card): update example templates for better readability

--- src/barista-examples/card/subtitle-card-example.component.ts
+++ src/barista-examples/card/subtitle-card-example.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: `<div class="demo-card">
-<dt-card>
-  <dt-card-title>Top 3 JavaScript errors</dt-card-title>
-  <dt-card-subtitle>Some subtitle</dt-card-subtitle>
-  This is some generic content
-</dt-card></div>`,
+  template: `
+<div class="demo-card">
+  <dt-card>
+    <dt-card-title>Top 3 JavaScript errors</dt-card-title>
+    <dt-card-subtitle>Some subtitle</dt-card-subtitle>
+    This is some generic content
+  </dt-card>
+</div>`,
 })
 export class SubtitleCardExampleComponent { }
