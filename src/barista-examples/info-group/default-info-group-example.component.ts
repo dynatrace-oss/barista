0bf72030d28d45d7f6de6439a93fb3d73e153e9c
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-04 15:31:50 +0200

    APM-166163 feat(linting): add dt-info-group-needs-title-and-icon rule

--- src/barista-examples/info-group/default-info-group-example.component.ts
+++ src/barista-examples/info-group/default-info-group-example.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <dt-info-group>
+<dt-info-group>
   <dt-info-group-icon><dt-icon name="agent"></dt-icon></dt-info-group-icon>
   <dt-info-group-title>5 min 30 s</dt-info-group-title>
   Session duration
