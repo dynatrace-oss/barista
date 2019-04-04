3ab81737ee08efa232b48c72466e799d47b85268
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-04 14:38:05 +0200

    APM-166163 docs(card): update example templates for better readability

--- src/barista-examples/card/title-card-example.component.ts
+++ src/barista-examples/card/title-card-example.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: `<div class="demo-card">
-<dt-card>
-  <dt-card-title>Top 3 JavaScript errors</dt-card-title>
-  This is some generic content
-  <dt-card-footer-actions>
-    <button dt-button variant="secondary">Analyse response time</button>
-    <button dt-button variant="primary">Details</button>
-  </dt-card-footer-actions>
-</dt-card></div>`,
+  template: `
+<div class="demo-card">
+  <dt-card>
+    <dt-card-title>Top 3 JavaScript errors</dt-card-title>
+    This is some generic content
+    <dt-card-footer-actions>
+      <button dt-button variant="secondary">Analyse response time</button>
+      <button dt-button variant="primary">Details</button>
+    </dt-card-footer-actions>
+  </dt-card>
+</div>`,
   styles: ['.dt-button + .dt-button { margin-left: 8px;}; }'],
 })
 export class TitleCardExampleComponent { }
