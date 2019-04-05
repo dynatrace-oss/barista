cd016039289f2c4c0c5c660b1cb6af4ac52c4753
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-05 12:13:59 +0200

    APM-166163 feat(linting): add dt-show-more-no-empty rule and refactor text alternative check

--- src/linting/helpers/altText.ts
+++ src/linting/helpers/altText.ts
@@ -1,4 +1,4 @@
-import { AttrAst, BoundElementPropertyAst } from '@angular/compiler';
+import { AttrAst, BoundElementPropertyAst, ElementAst } from '@angular/compiler';
 
 /**
  * Checks for aria-label attribute.
@@ -6,7 +6,7 @@ import { AttrAst, BoundElementPropertyAst } from '@angular/compiler';
  * @param inputs element inputs / attribute bindings
  * @returns true if aria-label attribute or binding is set, false otherwise.
  */
-export function hasAriaLabel(attrs: AttrAst[], inputs: BoundElementPropertyAst[]): boolean {
+function hasAriaLabel(attrs: AttrAst[], inputs: BoundElementPropertyAst[]): boolean {
   const hasAriaLabelAttr = attrs.some((attr) => attr.name === 'aria-label' && attr.value.trim().length > 0);
   const hasAriaLabelInput = inputs.some((input) => input.name === 'aria-label');
 
@@ -19,10 +19,25 @@ export function hasAriaLabel(attrs: AttrAst[], inputs: BoundElementPropertyAst[]
  * @param inputs element inputs / attribute bindings.
  * @returns true if aria-labelledby attribute or binding is set, false otherwise.
  */
-export function hasAriaLabelledby(attrs: AttrAst[], inputs: BoundElementPropertyAst[]): boolean {
+function hasAriaLabelledby(attrs: AttrAst[], inputs: BoundElementPropertyAst[]): boolean {
   const hasAriaLabelledbyAttr = attrs.some((attr) => attr.name === 'aria-labelledby' && attr.value.trim().length > 0);
   const hasAriaLabelledbyInput = inputs.some((input) => input.name === 'aria-labelledby');
   // TODO: check reference if aria-labelledby given
 
   return hasAriaLabelledbyAttr || hasAriaLabelledbyInput;
+}
+
+/**
+ * Checks if the given element provides text alternatives in form of an aria-label
+ * or aria-labelledby attribute.
+ * @param element the element to check.
+ * @returns whether a text alternative is given.
+ */
+export function hasTextContentAlternative(element: ElementAst): boolean {
+  const attrs: AttrAst[] = element.attrs;
+  const inputs: BoundElementPropertyAst[] = element.inputs;
+  if (hasAriaLabel(attrs, inputs) || hasAriaLabelledby(attrs, inputs)) {
+    return true;
+  }
+  return false;
 }
