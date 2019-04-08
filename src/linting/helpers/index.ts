5cfb51f962cb210d1206005a91f80ebf52187db4
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-08 13:05:01 +0200

    APM-166163 feat(linting): add rules to find direct children of dt-card and dt-tile

--- src/linting/helpers/index.ts
+++ src/linting/helpers/index.ts
@@ -1,5 +1,6 @@
 export * from './altText';
 export * from './button';
+export * from './findChild';
 export * from './hasContent';
 export * from './hasTextContent';
 export * from './isDirectChild';
