1b353dd2a0c4c7310fdb980c999e04dd9e20f183
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-08 11:18:43 +0200

    APM-166163 feat(linting): add rules to ensure that a dt-tile contains all required content elements

--- src/linting/rules/dtTileNeedsIconRule.ts
+++ src/linting/rules/dtTileNeedsIconRule.ts
@@ -2,7 +2,7 @@ import { ElementAst } from '@angular/compiler';
 import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
 import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
 import { SourceFile } from 'typescript';
-import { hasContentApartFrom } from '../helpers';
+import { isDirectChild } from '../helpers';
 
 class DtTileVisitor extends BasicTemplateAstVisitor {
 
@@ -14,29 +14,22 @@ class DtTileVisitor extends BasicTemplateAstVisitor {
   
   // tslint:disable-next-line no-any
   private _validateElement(element: ElementAst): any {
-    if (element.name !== 'dt-tile') {
-      return;
-    }
-
-    const tileChildren = [
-      'dt-tile-icon',
-      'dt-tile-title',
-      'dt-tile-subtitle',
-    ];
-
-    if (hasContentApartFrom(element, tileChildren)) {
+    if (
+      element.name !== 'dt-tile' ||
+      isDirectChild(element, 'dt-tile-icon')
+    ) {
       return;
     }
 
     const startOffset = element.sourceSpan.start.offset;
     const endOffset = element.sourceSpan.end.offset;
-    this.addFailureFromStartToEnd(startOffset, endOffset, 'A dt-tile must always contain content apart from title, subtitle and icon.');
+    this.addFailureFromStartToEnd(startOffset, endOffset, 'A dt-tile must contain a dt-tile-icon element, that must be a direct child.');
   }
 }
 
 /**
- * The dtCardNoEmptyRule ensures that a dt-tile always contains content
- * apart from a title, subtitle and an icon.
+ * The dtTileNeedsIconRule ensures that a dt-tile always has an icon,
+ * that is a direct child of dt-tile.
  *
  * The following example passes the check:
  * <dt-tile>
@@ -48,21 +41,21 @@ class DtTileVisitor extends BasicTemplateAstVisitor {
  *
  * For the following example the linter throws an error:
  * <dt-tile>
- *   <dt-tile-icon><dt-icon name="agent"></dt-icon></dt-tile-icon>
  *   <dt-tile-title>L-W8-64-APMDay3</dt-tile-title>
  *   <dt-tile-subtitle>Linux (x84, 64-bit)</dt-tile-subtitle>
+ *   Network traffic
  * </dt-tile>
  */
 // tslint:disable-next-line:max-classes-per-file
 export class Rule extends Rules.AbstractRule {
 
   static readonly metadata: IRuleMetadata = {
-    description: 'Ensures that a dt-tile always contains content apart from title, subtitle and icon.',
+    description: 'Ensures that a tile always has an icon, that is a direct child of dt-tile.',
     // tslint:disable-next-line no-null-keyword
     options: null,
     optionsDescription: 'Not configurable.',
-    rationale: 'A dt-tile must always contain content apart from title, subtitle and icon.',
-    ruleName: 'dt-tile-no-empty',
+    rationale: 'A tile must always contain a dt-tile-icon, that is a direct child of dt-tile.',
+    ruleName: 'dt-tile-needs-icon',
     type: 'maintainability',
     typescriptOnly: true,
   };
