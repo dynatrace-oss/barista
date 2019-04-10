147aca6441705bc45a1b0008392447f7e01f6ba5
Fabian Friedl <fabian.friedl@dynatrace.com>
2019-04-10 13:18:38 +0200

    APM-172868 fix(chart): fixes an issue with tooltip flickering, fixes an issue that the tooltip was not positioned correctly for category axis, correctly positions tooltip now vertically centered for all chart types except pie charts

--- src/lib/chart/tooltip/chart-tooltip.ts
+++ src/lib/chart/tooltip/chart-tooltip.ts
@@ -2,7 +2,6 @@ import {
   Component,
   SkipSelf,
   Inject,
-  forwardRef,
   TemplateRef,
   ContentChild,
   NgZone,
@@ -11,29 +10,37 @@ import {
   ViewEncapsulation,
   Renderer2,
   Optional,
+  ViewContainerRef,
 } from '@angular/core';
 import { DtChart, DT_CHART_RESOLVER, DtChartResolver } from '../chart';
-import { DtOverlay, DtOverlayRef } from '@dynatrace/angular-components/overlay';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, filter } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { ConnectedPosition } from '@angular/cdk/overlay';
+import { ConnectedPosition, Overlay, OverlayRef, OverlayConfig } from '@angular/cdk/overlay';
 import { DtChartTooltipData } from '../highcharts/highcharts-tooltip-types';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { isDefined } from '@dynatrace/angular-components/core';
+
+interface HighchartsPlotBackgroundInformation {
+  x: number;
+  y: number;
+  height: number;
+}
 
 /** Default horizontal offset for the tooltip */
-const DT_CHART_TOOLTIP_DEFAULT_OFFSET = 30;
+const DT_CHART_TOOLTIP_DEFAULT_OFFSET = 10;
 
 /** Positions for the chart tooltip  */
 const DEFAULT_DT_CHART_TOOLTIP_POSITIONS: ConnectedPosition[] = [
   {
     originX: 'start',
-    originY: 'top',
+    originY: 'center',
     overlayX: 'end',
     overlayY: 'center',
     offsetX: -DT_CHART_TOOLTIP_DEFAULT_OFFSET,
   },
   {
     originX: 'end',
-    originY: 'top',
+    originY: 'center',
     overlayX: 'start',
     overlayY: 'center',
     offsetX: DT_CHART_TOOLTIP_DEFAULT_OFFSET,
@@ -47,21 +54,29 @@ const DEFAULT_DT_CHART_TOOLTIP_POSITIONS: ConnectedPosition[] = [
   host: {
     class: 'dt-chart-tooltip',
   },
+  styleUrls: ['chart-tooltip.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.Emulated,
+  encapsulation: ViewEncapsulation.None,
   preserveWhitespaces: false,
 })
 export class DtChartTooltip<T> implements OnDestroy {
+  /** @deprecated @breaking-change To be removed with 3.0.0. */
   @ContentChild(TemplateRef) overlay: TemplateRef<T>;
 
+  // tslint:disable-next-line:no-any
+  @ContentChild(TemplateRef) _overlayTemplate: TemplateRef<any>;
+
   private readonly _destroy = new Subject<void>();
-  private _dtOverlayRef: DtOverlayRef<T> | null;
-  private _origin: HTMLElement | null;
+  private _overlayRef: OverlayRef | null;
+  private _portal: TemplatePortal;
+  private _origin: SVGCircleElement | null;
+  private _plotBackgroundInfo: HighchartsPlotBackgroundInformation;
 
   constructor(
-    private _dtOverlay: DtOverlay,
+    private _overlay: Overlay,
     private _ngZone: NgZone,
     private _renderer: Renderer2,
+    private _viewContainerRef: ViewContainerRef,
     @Inject(DT_CHART_RESOLVER) @Optional() @SkipSelf() private _resolveParentChart: DtChartResolver
   ) {
   }
@@ -75,7 +90,11 @@ export class DtChartTooltip<T> implements OnDestroy {
             // create the overlay here when the tooltip should be open
             // because we need to have the event information to attach the overlay to the correct element
             if (event && event.data && checkHasPointData(event.data)) {
+              if (!isDefined(this._overlayRef)) {
                 this._createOverlay(event.data, parentChart);
+              } else {
+                this._updateOverlayContext(event.data, parentChart);
+              }
             } else {
               // dismiss if no data is given
               this._dismiss();
@@ -91,6 +110,15 @@ export class DtChartTooltip<T> implements OnDestroy {
           }
         });
       });
+
+      parentChart._plotBackground$.pipe(takeUntil(this._destroy))
+      .pipe(filter((background) => !!background))
+      .subscribe((background) => {
+        const x = parseInt(background!.getAttribute('x')!, 10);
+        const y = parseInt(background!.getAttribute('y')!, 10);
+        const height = parseInt(background!.getAttribute('height')!, 10);
+        this._plotBackgroundInfo = { x, y, height };
+      });
     }
   }
 
@@ -101,57 +129,112 @@ export class DtChartTooltip<T> implements OnDestroy {
 
   /** Create a new overlay for the tooltip */
   private _createOverlay(data: DtChartTooltipData, parentChart: DtChart): void {
-    if (this._origin) {
-      this._renderer.removeChild(this._origin.parentNode, this._origin);
-      this._origin = null;
-    }
     if (parentChart._chartObject) {
-      this._origin = createOriginPoint(parentChart, this._renderer, data);
-      const originPoint = this._origin.getBoundingClientRect();
-      this._dtOverlayRef = this._dtOverlay.create<T>(
-        { x: originPoint.left, y: originPoint.top },
-        this.overlay,
-        { data, _positions: DEFAULT_DT_CHART_TOOLTIP_POSITIONS });
-      this._dtOverlayRef.updatePosition(0, 0);
+
+      const positionStrategy = this._overlay.position()
+        .flexibleConnectedTo(this._createTooltipMarker(parentChart, this._renderer, data))
+        .withPositions(DEFAULT_DT_CHART_TOOLTIP_POSITIONS);
+
+      const overlayConfig = new OverlayConfig({
+        positionStrategy,
+        backdropClass: 'dt-no-pointer',
+        hasBackdrop: true,
+        panelClass: ['dt-chart-tooltip-overlay', 'dt-no-pointer'],
+        scrollStrategy: this._overlay.scrollStrategies.close(),
+      });
+
+      const overlayRef = this._overlay.create(overlayConfig);
+      // tslint:disable-next-line:no-any
+      this._portal = new TemplatePortal<any>(this._overlayTemplate, this._viewContainerRef, { $implicit: data });
+
+      overlayRef.attach(this._portal);
+
+      this._overlayRef = overlayRef;
+    }
+  }
+
+  /** Updates the overlay content */
+  private _updateOverlayContext(data: DtChartTooltipData, parentChart: DtChart): void {
+    if (this._portal && this._overlayRef) {
+      this._portal.context.$implicit = data;
+      const positionStrategy = this._overlay.position()
+        .flexibleConnectedTo(this._createTooltipMarker(parentChart, this._renderer, data))
+        .withPositions(DEFAULT_DT_CHART_TOOLTIP_POSITIONS);
+      this._overlayRef.updatePositionStrategy(positionStrategy);
     }
   }
 
   /** Dismisses the overlay and cleans up the ref */
   private _dismiss(): void {
-    if (this._dtOverlayRef) {
-      this._dtOverlayRef.dismiss();
-      this._dtOverlayRef = null;
+    if (this._overlayRef) {
+      this._overlayRef.dispose();
+      this._overlayRef = null;
     }
   }
+
+  /**
+   * Creates a dummy origin point that can be used to position the tooltip
+   */
+  private _createTooltipMarker(chart: DtChart, renderer: Renderer2, data: DtChartTooltipData): { x: number; y: number } {
+    if (this._origin) {
+      this._renderer.removeChild(this._origin.parentNode, this._origin);
+      this._origin = null;
+    }
+
+    const circle: SVGCircleElement = renderer.createElement('circle', 'svg');
+    const { x, y } = getHighchartsTooltipPosition(data, this._plotBackgroundInfo);
+
+    renderer.setAttribute(circle, 'class', 'dt-tooltip-position-marker');
+    renderer.setAttribute(circle, 'cx', x.toString());
+    renderer.setAttribute(circle, 'cy', y.toString());
+    renderer.setAttribute(circle, 'r', '1');
+    renderer.setAttribute(circle, 'fill', 'transparent');
+    renderer.appendChild(chart._chartObject!.container.querySelector('svg'), circle);
+
+    this._origin = circle;
+    const boundingClientRect = circle.getBoundingClientRect();
+    return {
+      x: boundingClientRect.left,
+      y: boundingClientRect.top,
+    };
+  }
+
 }
 
 function checkHasPointData(data: DtChartTooltipData): boolean {
   return !!data.points || !!data.point;
 }
 
-/** Creates a dummy origin point that can be used to position the tooltip */
-function createOriginPoint(chart: DtChart, renderer: Renderer2, data: DtChartTooltipData): HTMLElement {
-  const circle: HTMLElement = renderer.createElement('circle', 'svg');
-  const xAxis = data.points ? data.points[0].series.xAxis : data.point!.series.xAxis;
-  const yAxis = data.points ? data.points[0].series.yAxis : data.point!.series.yAxis;
-  const x = data.points ? data.points[0].x : data.point!.point.x;
-  const y = data.points ? data.points[0].y : data.point!.point.y;
-  let xPixel;
-  let yPixel;
-  if (xAxis) {
-    xPixel = xAxis.toPixels(x);
-    yPixel = yAxis.toPixels(y);
-  } else {
-    // is Pie chart
+/**
+ * highcharts provides the tooltip position differently depending on the series type
+ * Pie chart: data.point.point.tooltipPos[x, y]
+ * Category: data.points[0].point.tooltipPos[x, whatever, whatever]
+ * Mixed multiple series(line, column): data.points[0].point.tooltipPos[x, whatever, whatever]
+ * Area as first: data.points[0].point.x => xAxis.toPixel(x)
+ */
+function getHighchartsTooltipPosition(
+  data: DtChartTooltipData,
+  plotBackgroundInfo: HighchartsPlotBackgroundInformation
+): { x: number; y: number } {
+  const isPieChart = !isDefined(data.points);
+  const hasAreaFirstSeries = data.points && data.points[0].point && !data.points[0].point.tooltipPos;
+  let x: number;
+  // set y position for all charts in the middle of the plotbackground vertically
+  // tslint:disable-next-line:no-magic-numbers
+  let y = plotBackgroundInfo.height / 2 + plotBackgroundInfo.y;
+  if (isPieChart) {
     const tooltipPos = data.point!.point.tooltipPos;
-    xPixel = tooltipPos[0];
-    yPixel = tooltipPos[1];
+    x = tooltipPos![0];
+    // override the y position for pie charts
+    y = tooltipPos![1];
+  } else if (hasAreaFirstSeries) {
+    const point = data.points![0].point;
+    const xAxis = data.points![0].series.xAxis;
+    x = xAxis.toPixels(point.x);
+  } else {
+    const point = data.points![0].point;
+    x = point.tooltipPos![0] + plotBackgroundInfo.x;
   }
-  renderer.setAttribute(circle, 'class', 'dt-tooltip-position-marker');
-  renderer.setAttribute(circle, 'cx', xPixel.toString());
-  renderer.setAttribute(circle, 'cy', yPixel.toString());
-  renderer.setAttribute(circle, 'r', '1');
-  renderer.setAttribute(circle, 'fill', 'transparent');
-  renderer.appendChild(chart._chartObject!.container.querySelector('svg'), circle);
-  return circle;
+
+  return { x, y };
 }
