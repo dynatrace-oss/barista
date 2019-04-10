98428915666c2042e9296064194b8b0e4fba7b9d
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-10 11:10:08 +0200

    APM-166163 feat(linting): add dt-tab-content-no-empty rule

---
+++ src/linting/rules/dtTabContentNoEmptyRule.ts
@@ -0,0 +1,68 @@
+import { EmbeddedTemplateAst } from '@angular/compiler';
+import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
+import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
+import { SourceFile } from 'typescript';
+import { hasContent } from '../helpers';
+
+class DtTabVisitor extends BasicTemplateAstVisitor {
+
+  visitEmbeddedTemplate(template: EmbeddedTemplateAst, context: any): any {
+    this._validateElement(template);
+    super.visitEmbeddedTemplate(template, context);
+  }
+  
+  // tslint:disable-next-line no-any
+  private _validateElement(element: EmbeddedTemplateAst): any {
+    if (!this._isTabContent(element)) {
+      return;
+    }
+
+    if (hasContent(element)) {
+      return;
+    }
+
+    const startOffset = element.sourceSpan.start.offset;
+    const endOffset = element.sourceSpan.end.offset;
+    this.addFailureFromStartToEnd(startOffset, endOffset, 'A dtTabContent must always contain content.');
+  }
+
+  private _isTabContent(element: EmbeddedTemplateAst) {
+    return element.attrs &&
+      element.attrs.some((attr) => attr.name === 'dtTabContent');
+  }
+}
+
+/**
+ * The dtTabContentNoEmptyRule ensures that a dtTabContent always contains content
+ *
+ * The following example passes the check:
+ * <ng-template dtTabContent>
+ *   <h3>Traffic</h3>
+ * </ng-template>
+ *
+ * For the following example the linter throws an error:
+ * <ng-template dtTabContent>
+ * </ng-template>
+ */
+// tslint:disable-next-line:max-classes-per-file
+export class Rule extends Rules.AbstractRule {
+
+  static readonly metadata: IRuleMetadata = {
+    description: 'Ensures that a dtTabContent always contains content.',
+    // tslint:disable-next-line no-null-keyword
+    options: null,
+    optionsDescription: 'Not configurable.',
+    rationale: 'A dtTabContent must always contain content.',
+    ruleName: 'dt-tab-content-no-empty',
+    type: 'maintainability',
+    typescriptOnly: true,
+  };
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    return this.applyWithWalker(
+      new NgWalker(sourceFile, this.getOptions(), {
+        templateVisitorCtrl: DtTabVisitor,
+      }),
+    );
+  }
+}
