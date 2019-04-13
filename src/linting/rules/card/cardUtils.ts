e1bad170cc7cfcf89bf4969ddff9f0845617559f
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-04-13 16:29:06 +0200

    APM-166163 refactor(linting): refactor folder structure according to feedback

---
+++ src/linting/rules/card/cardUtils.ts
@@ -0,0 +1,7 @@
+export const cardChildren = [
+  'dt-card-title',
+  'dt-card-subtitle',
+  'dt-card-icon',
+  'dt-card-title-actions',
+  'dt-card-footer-actions',
+];
--- src/linting/rules/tile/dtTileNoEmptyRule.ts
+++ src/linting/rules/tile/dtTileNoEmptyRule.ts
@@ -2,7 +2,7 @@ import { ElementAst } from '@angular/compiler';
 import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
 import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
 import { SourceFile } from 'typescript';
-import { addFailure, ChildNode, findChild, isElementWithName } from '../helpers';
+import { addFailure, hasContentApartFrom, isElementWithName } from '../../utils';
 
 class DtTileVisitor extends BasicTemplateAstVisitor {
 
@@ -16,60 +16,48 @@ class DtTileVisitor extends BasicTemplateAstVisitor {
       return;
     }
 
-    const directChildren = [
+    const tileChildren = [
+      'dt-tile-icon',
       'dt-tile-title',
       'dt-tile-subtitle',
-      'dt-tile-icon',
     ];
 
-    const childNodes: ChildNode[] = [];
-    directChildren.forEach((childName) => {
-      findChild(element, childName, 0, childNodes);
-    });
-
-    const filteredChildren: string[] = childNodes
-      .filter((el) => el.level > 1)
-      .map((el) => el.name);
-
-    if (filteredChildren.length < 1) {
+    if (hasContentApartFrom(element, tileChildren)) {
       return;
     }
 
-    const childrenNames = Array.from(new Set(filteredChildren));
-
-    addFailure(this, element, `The following elements must be direct children of a dt-tile: ${childrenNames.join(', ')}`);
+    addFailure(this, element, 'A dt-tile must always contain content apart from title, subtitle and icon.');
   }
 }
 
 /**
- * The dtTileDirectChildrenRule ensures that the defined elements are always direct children of a dt-tile.
+ * The dtCardNoEmptyRule ensures that a dt-tile always contains content
+ * apart from a title, subtitle and an icon.
  *
  * The following example passes the check:
  * <dt-tile>
  *   <dt-tile-icon><dt-icon name="agent"></dt-icon></dt-tile-icon>
  *   <dt-tile-title>L-W8-64-APMDay3</dt-tile-title>
- *   // ...
+ *   <dt-tile-subtitle>Linux (x84, 64-bit)</dt-tile-subtitle>
+ *   Network traffic
  * </dt-tile>
  *
  * For the following example the linter throws an error:
  * <dt-tile>
  *   <dt-tile-icon><dt-icon name="agent"></dt-icon></dt-tile-icon>
  *   <dt-tile-title>L-W8-64-APMDay3</dt-tile-title>
- *   <div>
- *     <dt-tile-subtitle>Linux (x84, 64-bit)</dt-tile-subtitle>
- *   </div>
- *   // ...
+ *   <dt-tile-subtitle>Linux (x84, 64-bit)</dt-tile-subtitle>
  * </dt-tile>
  */
 export class Rule extends Rules.AbstractRule {
 
   static readonly metadata: IRuleMetadata = {
-    description: 'Ensures that a tile\'s child components are direct children of a dt-tile.',
+    description: 'Ensures that a dt-tile always contains content apart from title, subtitle and icon.',
     // tslint:disable-next-line:no-null-keyword
     options: null,
     optionsDescription: 'Not configurable.',
-    rationale: 'A tile\'s child components (title, subtitle, icon) must always be direct children of the dt-tile.',
-    ruleName: 'dt-tile-direct-children',
+    rationale: 'A dt-tile must always contain content apart from title, subtitle and icon.',
+    ruleName: 'dt-tile-no-empty',
     type: 'maintainability',
     typescriptOnly: true,
   };
