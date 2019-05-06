52eaaefa93b1c3ba7d06c4f04d028cbb6e275ff5
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-05-06 14:45:52 +0200

    APM-173330 feat(linting): add alt text rule for context dialog and adapt examples

--- src/barista-examples/context-dialog/default-context-dialog-example.component.ts
+++ src/barista-examples/context-dialog/default-context-dialog-example.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-  <dt-context-dialog>
-  <p>Your dashboard "real user monitoring" is only visible to you</p>
+  <dt-context-dialog aria-label="Show more details" aria-label-close-button="Close context dialog">
+    <p>Your dashboard "real user monitoring" is only visible to you</p>
   </dt-context-dialog>
   `,
 })
