ff95a92f809ffd7777935cd46c18876d6ea33e82
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-05-07 13:02:20 +0200

    APM-173330 feat(linting): add dt-radio-button-name-required rule

--- src/barista-examples/radio/pure-radio-example.ts
+++ src/barista-examples/radio/pure-radio-example.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: '<dt-radio-button value="myValue">Radio button value</dt-radio-button>',
+  template: '<dt-radio-button value="myValue" name="myName">Radio button value</dt-radio-button>',
 })
 export class PureRadioExample { }
