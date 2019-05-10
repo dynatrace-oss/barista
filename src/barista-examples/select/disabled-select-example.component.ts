b8f9d8da825918c8696ecce43a71be391e10955b
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-05-10 11:49:57 +0200

    APM-173330 feat(linting): add dt-select-requires-label rule

--- src/barista-examples/select/disabled-select-example.component.ts
+++ src/barista-examples/select/disabled-select-example.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   template: `
-    <dt-select placeholder="Choose your coffee" [disabled]="disabled">
+    <dt-select placeholder="Choose your coffee" [disabled]="disabled" aria-label="Choose your coffee">
       <dt-option value="ThePerfectPour">ThePerfectPour</dt-option>
       <dt-option disabled value="Affogato">Affogato</dt-option>
       <dt-option value="Americano">Americano</dt-option>
