ffc4c30e505fba336157d3384cc7ff8765709c6c
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-05-10 11:49:57 +0200

    APM-173330 feat(linting): add dt-select-requires-label rule

---
+++ src/linting/rules/dtSelectRequiresLabelRule.ts
@@ -0,0 +1,94 @@
+import { ElementAst } from '@angular/compiler';
+import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
+import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
+import { SourceFile } from 'typescript';
+import { addFailure,
+  addParentElement,
+  hasFormFieldParentWithLabel,
+  hasTextContentAlternative,
+  isElementWithName,
+  ParentElement,
+} from '../utils';
+
+class DtSelectVisitor extends BasicTemplateAstVisitor {
+
+  formFields: ParentElement[] = [];
+
+  visitElement(element: ElementAst, context: any): any {
+    this._validateElement(element);
+    super.visitElement(element, context);
+  }
+
+  private _validateElement(element: ElementAst): any {
+    // If the element is a form field, remember it.
+    if (isElementWithName(element, 'dt-form-field')) {
+      this.formFields = addParentElement(element, this.formFields, element.name);
+      return;
+    }
+
+    if (!isElementWithName(element, 'dt-select')) {
+      return;
+    }
+
+    // If the select has a form field parent element with a label, return.
+    if (hasFormFieldParentWithLabel(element, this.formFields)) {
+      return;
+    }
+
+    // If the select has no form field wrapper or a wrapper without label check for an aria-label.
+    if (hasTextContentAlternative(element)) {
+      return;
+    }
+
+    addFailure(this, element, 'A dt-select requires a wrapping form field with a dt-label or an aria-label or aria-labelledby attribute.');
+  }
+}
+
+/**
+ * The dtSelectRequiresLabelRule ensures that a label or text alternatives are given for a dt-select.
+ *
+ * The following example passes the lint checks:
+ * <dt-select placeholder="Choose your coffee" aria-label="Choose your coffee">
+ *   <dt-option value="ThePerfectPour">ThePerfectPour</dt-option>
+ *   <dt-option value="Affogato">Affogato</dt-option>
+ * // ...
+ * </dt-select>
+ * 
+ * <dt-form-field>
+ *   <dt-label>Your Coffee:</dt-label>
+ *   <dt-select placeholder="Choose your coffee" required [(ngModel)]="selectedValue">
+ *     <dt-option>No Coffee (Triggers an error)</dt-option>
+ *     <dt-option value="ThePerfectPour">ThePerfectPour</dt-option>
+ *     <dt-option value="Affogato">Affogato</dt-option>
+ *     <dt-option value="Americano">Americano</dt-option>
+ *   </dt-select>
+ *  </dt-form-field>
+ *
+ * For the following example the linter throws errors:
+ * <dt-select placeholder="Choose your coffee">
+ *   <dt-option value="ThePerfectPour">ThePerfectPour</dt-option>
+ *   <dt-option value="Affogato">Affogato</dt-option>
+ * // ...
+ * </dt-select>
+ */
+export class Rule extends Rules.AbstractRule {
+
+  static readonly metadata: IRuleMetadata = {
+    description: 'Ensures that a label or text alternatives are given for a dt-select.',
+    // tslint:disable-next-line:no-null-keyword
+    options: null,
+    optionsDescription: 'Not configurable.',
+    rationale: 'A dt-select must have a label when used with the form field component or an aria-label or aria-labelledby attribute.',
+    ruleName: 'dt-select-requires-label',
+    type: 'maintainability',
+    typescriptOnly: true,
+  };
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    return this.applyWithWalker(
+      new NgWalker(sourceFile, this.getOptions(), {
+        templateVisitorCtrl: DtSelectVisitor,
+      })
+    );
+  }
+}
