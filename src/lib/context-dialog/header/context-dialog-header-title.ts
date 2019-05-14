b3a7a093d4b760410e672d096403e690fac6ccfc
thomas.heller <thomas.heller@dynatrace.com>
2019-05-14 14:24:31 +0200

    APM-175331 feat(context-dialog): Added the capability to add a header section to the context-dialog component

---
+++ src/lib/context-dialog/header/context-dialog-header-title.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: `dt-context-dialog-header-title, [dt-context-dialog-header-title], [dtContextDialogHeaderTitle]`,
+  host: {
+    class: 'dt-context-dialog-header-title',
+    role: 'heading',
+  },
+})
+export class DtContextDialogHeaderTitle {}
