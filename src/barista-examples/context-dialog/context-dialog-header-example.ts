a044e0d64367b5856d926ab190ce56c8bc24d31c
thomas.heller <thomas.heller@dynatrace.com>
2019-05-14 14:24:31 +0200

    APM-175331 feat(context-dialog): Added the capability to add a header section to the context-dialog component

---
+++ src/barista-examples/context-dialog/context-dialog-header-example.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: `
+<dt-context-dialog aria-label="Show more actions" aria-label-close-button="Close context dialog">
+  <dt-context-dialog-header>
+    <dt-context-dialog-header-title>Analyse</dt-context-dialog-header-title>
+    Today, 12:03 - 14:08
+  </dt-context-dialog-header>
+  <p>Your dashboard "real user monitoring" is only visible to you</p>
+</dt-context-dialog>`,
+  styles: [
+    `p { margin-top: 0px; }`,
+  ],
+})
+export class ContextDialogHeaderExampleComponent {
+}
