09b7b8f390e11ede95978a833dc9aa1c3eb24079
thomas.heller <thomas.heller@dynatrace.com>
2019-05-15 13:29:09 +0200

    APM-161610 feat(core): Publicly provide compare functions for string and number values

---
+++ src/lib/core/util/compare-util.spec.ts
@@ -0,0 +1,106 @@
+import { compareValues, compareStrings, compareNumbers } from './compare-util';
+
+describe('CompareUtil', () => {
+
+  describe('compareValues', () => {
+    it('should sort number values ascending', () => {
+      const numbers = [8, 27, 5000, 17, 123, 591, 182];
+      const sorted = numbers.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual([8, 17, 27, 123, 182, 591, 5000]);
+    });
+
+    it('should sort number values descending', () => {
+      const numbers = [8, 27, 5000, 17, 123, 591, 182];
+      const sorted = numbers.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual([5000, 591, 182, 123, 27, 17, 8]);
+    });
+
+    it('should sort numbers and null values ascending', () => {
+      const numbers = [8, 27, 5000, null, 17, 123, null, 591, 182];
+      const sorted = numbers.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual([8, 17, 27, 123, 182, 591, 5000, null, null]);
+    });
+
+    it('should sort number and null values descending', () => {
+      const numbers = [8, 27, 5000, null, 17, 123, null, 591, 182];
+      const sorted = numbers.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual([null, null, 5000, 591, 182, 123, 27, 17, 8]);
+    });
+
+    it('should sort string values ascending', () => {
+      const strings = ['host', 'memory', 'metric', 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual(['center', 'host', 'memory', 'metric']);
+    });
+
+    it('should sort string values descending', () => {
+      const strings = ['host', 'memory', 'metric', 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual(['metric', 'memory', 'host', 'center']);
+    });
+
+    it('should sort strings and null values ascending', () => {
+      const strings = ['host', 'memory', 'metric', null, 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual(['center', 'host', 'memory', 'metric', null]);
+    });
+
+    it('should sort string and null values descending', () => {
+      const strings = ['host', 'memory', 'metric', null, 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual([null, 'metric', 'memory', 'host', 'center']);
+    });
+
+    it('should sort strings correctly even with different cases ascending', () => {
+      const strings = ['Host', 'memory', 'Metric', 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual(['center', 'Host', 'memory', 'Metric']);
+    });
+
+    it('should sort strings correctly even with different cases descending', () => {
+      const strings = ['Host', 'memory', 'Metric', 'center'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual(['Metric', 'memory', 'Host', 'center']);
+    });
+
+    it('should sort strings correctly even with custom characters ascending', () => {
+      const strings = ['Entity', 'éntity', 'èlaborate', 'çonstant'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'asc'));
+      expect(sorted).toEqual(['çonstant', 'èlaborate', 'Entity', 'éntity']);
+    });
+
+    it('should sort strings correctly even with custom characters descending', () => {
+      const strings = ['Entity', 'éntity', 'èlaborate', 'çonstant'];
+      const sorted = strings.sort((a, b) => compareValues(a, b, 'desc'));
+      expect(sorted).toEqual(['éntity', 'Entity', 'èlaborate', 'çonstant']);
+    });
+  });
+
+  describe('compareNumbers', () => {
+    it('should sort number values by descending default', () => {
+      const numbers = [8, 27, 5000, 17, 123, 591, 182];
+      const sorted = numbers.sort((a, b) => compareNumbers(a, b));
+      expect(sorted).toEqual([5000, 591, 182, 123, 27, 17, 8]);
+    });
+
+    it('should sort number and null values by descending default', () => {
+      const numbers = [8, 27, 5000, null, 17, 123, null, 591, 182];
+      const sorted = numbers.sort((a, b) => compareNumbers(a, b));
+      expect(sorted).toEqual([null, null, 5000, 591, 182, 123, 27, 17, 8]);
+    });
+  });
+
+  describe('compareStrings', () => {
+    it('should sort string values ascending default', () => {
+      const strings = ['host', 'memory', 'metric', 'center'];
+      const sorted = strings.sort((a, b) => compareStrings(a, b));
+      expect(sorted).toEqual(['center', 'host', 'memory', 'metric']);
+    });
+
+    it('should sort strings and null values ascending default', () => {
+      const strings = ['host', 'memory', 'metric', null, 'center'];
+      const sorted = strings.sort((a, b) => compareStrings(a, b));
+      expect(sorted).toEqual(['center', 'host', 'memory', 'metric', null]);
+    });
+  });
+});
---
+++ src/lib/core/util/compare-util.ts
@@ -0,0 +1,65 @@
+import { isString, isDefined } from './type-util';
+
+/** Sort directions */
+export type DtSortDirection = 'asc' | 'desc' | '';
+
+/**
+ * Compares two string values and returns a comparison value (-1, 0 or 1) which can be used within a
+ * sort function.
+ * Null or undefined values are being handled and sorted to the start/end based on the sort direction.
+ * Sort direction is defaulted to 'asc' but can be passed as well.
+ * @example
+ * const stringValues = ['host', 'memory', 'metric', 'center'];
+ * const sortedStrings = stringValues.sort((a, b) => compareStrings(a, b));
+ */
+export function compareStrings(valueA: string|null, valueB: string|null, direction: DtSortDirection = 'asc'): number {
+  return compareValues(valueA, valueB, direction);
+}
+
+/**
+ * Compares two number values and returns a comparison value (-1, 0 or 1) which can be used within a
+ * sort function.
+ * Null or undefined values are being handled and sorted to the start/end based on the sort direction.
+ * Sort direction is defaulted to 'desc' but can be passed as well.
+ * @example
+ * const numberValues = [1, 15, 8, 19, 23];
+ * const sortednumbers = numberValues.sort((a, b) => compareStrings(a, b));
+ */
+export function compareNumbers(valueA: number|null, valueB: number|null, direction: DtSortDirection = 'desc'): number {
+  return compareValues(valueA, valueB, direction);
+}
+
+/**
+ * Compares two values and returns a comparison value (-1, 0 or 1) which can be used within a
+ * sort function.
+ * Null or undefined values are being handled and sorted to the start/end based on the sort direction.
+ * @example
+ * const numberValues = [1, 15, 8, 19, 23];
+ * const sortedNumbers = numberValues.sort((a, b) => compareValues(a, b, 'asc'));
+ * // or with string values
+ * const stringValues = ['host', 'memory', 'metric', 'center'];
+ * const sortedStrings = stringValues.sort((a, b) => compareStrings(a, b));
+ */
+export function compareValues(
+    valueA: string|number|null,
+    valueB: string|number|null,
+    direction: DtSortDirection): number {
+  let comparatorResult = 0;
+  if (isDefined(valueA) && isDefined(valueB)) {
+    if (isString(valueA) && isString(valueB)) {
+      comparatorResult = (valueA as string).localeCompare(valueB as string);
+    } else {
+      // Check if one value is greater than the other; if equal, comparatorResult should remain 0.
+      if (valueA! > valueB!) {
+        comparatorResult = 1;
+      } else if (valueA! < valueB!) {
+        comparatorResult = -1;
+      }
+    }
+  } else if (isDefined(valueA)) {
+    comparatorResult = -1;
+  } else if (isDefined(valueB)) {
+    comparatorResult = 1;
+  }
+  return comparatorResult * (direction === 'asc' ? 1 : -1);
+}
