a9c510e00e86d20fb24a4860e9c2b6ffa6ff14db
thomas.heller <thomas.heller@dynatrace.com>
2019-05-15 13:29:09 +0200

    APM-161610 feat(core): Publicly provide compare functions for string and number values

--- src/lib/core/util/index.ts
+++ src/lib/core/util/index.ts
@@ -2,3 +2,4 @@ export * from './platform-util';
 export * from './type-util';
 export * from './sanitize-svg';
 export * from './number-util';
+export * from './compare-util';
--- src/lib/table/sort/sort-direction.ts
+++ src/lib/table/sort/sort-direction.ts
@@ -1,2 +1,7 @@
-/** Sort directions */
-export type DtSortDirection = 'asc' | 'desc' | '';
+import { DtSortDirection } from '@dynatrace/angular-components/core';
+/**
+ * Sort directions
+ * @deprecated DtSortDirection will move into the angular-components core comparison, please import from there
+ * @breaking-change 3.0.0 Export from the table will be removed.
+ */
+export { DtSortDirection };
