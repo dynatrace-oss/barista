ef53e50c22ec729c2b3c1d00acf9cb19021c0290
thomas.heller <thomas.heller@dynatrace.com>
2019-05-15 13:29:09 +0200

    APM-161610 feat(core): Publicly provide compare functions for string and number values

--- src/lib/table/sort/sort-helpers.ts
+++ src/lib/table/sort/sort-helpers.ts
@@ -3,6 +3,8 @@
  * TODO: There would be room for extension here based on the documentation of
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
  * we should be able to provide a stable text-sort helper function.
+ * @deprecated Will be removed, please use the compareStrings function from angular-components/core.
+ * @breaking-change 3.0.0 will be removed.
  */
 export function compareString(a: string, b: string): number {
   return a.localeCompare(b);
