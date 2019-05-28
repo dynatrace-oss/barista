f4163a69c8948cbc8fdc901f5d754993f3637e47
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-05-28 15:12:29 +0200

    APM-176476 build(prettier): Integrated Prettier into project for automated code formatting

--- src/barista-examples/main.ts
+++ src/barista-examples/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule, { preserveWhitespaces: true })
-  // tslint:disable-next-line:no-console typedef
-  .catch((err): void => { console.log(err); });
+  .catch(
+    // tslint:disable-next-line:typedef
+    (err): void => {
+      console.log(err); // tslint:disable-line:no-console
+    }
+  );
