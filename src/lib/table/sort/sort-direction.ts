93fd7827cff54177faafed86dd7e771b4bce5314
Thomas Pink <thomas.pink@me.com>
2019-06-18 15:44:53 +0200

    feat(table): Removed deprecated type `DtSortDirection`. Import it directly from the core. BREAKING CHANGE

--- src/lib/table/sort/sort-direction.ts
+++
@@ -1,7 +0,0 @@
-import { DtSortDirection } from '@dynatrace/angular-components/core';
-/**
- * Sort directions
- * @deprecated DtSortDirection will move into the angular-components core comparison, please import from there
- * @breaking-change 3.0.0 Export from the table will be removed.
- */
-export { DtSortDirection };
