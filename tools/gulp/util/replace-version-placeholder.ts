2b866ad3d6c9d5b090e9d56e8b525f41752de068
Thomas Pink <thomas.pink@me.com>
2019-06-19 14:06:11 +0200

    build: Fixes issue where angular and cdk version did not update in the build artifact when the version numbers in root package.json did change.

--- tools/gulp/util/replace-version-placeholder.ts
+++ tools/gulp/util/replace-version-placeholder.ts
@@ -6,22 +6,38 @@ const SPACES_FOR_PACKAGE_JSON = 2;
 
 /** RegExp that matches Angular version placeholders within a file. */
 const ngVersionPlaceholder = new RegExp('0.0.0-NG', 'g');
+const ngCdkVersionPlaceholder = new RegExp('0.0.0-NG-CDK', 'g');
 
 const readPackageJson = (dirPath: string) =>
   readFileSync(join(dirPath, 'package.json'), 'utf-8');
 
 export const replaceVersionPlaceholders = () => {
-  /* read the package json in the dist directory for the library
-    and replace the NG-version placeholder */
-  const packageJson = readPackageJson(buildConfig.libOutputDir)
-    .replace(ngVersionPlaceholder, buildConfig.angularVersion);
-  /* read root package json in the projects root dir */
+
+  // read root package json in the projects root dir
   const rootPackageJson = readPackageJson(buildConfig.projectDir);
-  /* parse it as json */
+  // parse it as json
   const rootPackageJsonParsed = JSON.parse(rootPackageJson);
-  /* parse the package json and add the version from the root package json */
+
+  let ngVersion = rootPackageJsonParsed.dependencies['@angular/core'];
+  ngVersion = ngVersion.replace(/~|\^/, '');
+
+  let ngCdkVersion = rootPackageJsonParsed.dependencies['@angular/cdk'];
+  ngCdkVersion = ngCdkVersion.replace(/~|\^/, '');
+
+  if (!ngVersion || !ngCdkVersion) {
+    throw new Error('Version replacement failed. Could not find version of @angular/core or @angular/cdk in the root package.json');
+  }
+
+  // read the package json in the dist directory for the library
+  // and replace the NG-version and NG-CDK placeholder
+  let packageJson = readPackageJson(buildConfig.libOutputDir);
+  packageJson = packageJson.replace(ngCdkVersionPlaceholder, ngCdkVersion);
+  packageJson = packageJson.replace(ngVersionPlaceholder, ngVersion);
+
+  // parse the package json and add the version from the root package json
   const parsed = JSON.parse(packageJson);
   parsed.version = rootPackageJsonParsed.version;
-  /* write file to the dist folder */
+
+  // write file to the dist folder
   writeFileSync(join(buildConfig.libOutputDir, 'package.json'), JSON.stringify(parsed, null, SPACES_FOR_PACKAGE_JSON));
 };
