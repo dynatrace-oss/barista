1990c74a765607f115357997478b82553c682ef4
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-06-27 14:13:38 +0200

    APM-179088 feat(tag): Deprecated disabled state for DtTag since there is no proper use case for it

--- src/dev-app/tag/tag-demo.component.ts
+++ src/dev-app/tag/tag-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DtTag } from '@dynatrace/angular-components/tag';
 
 @Component({
   selector: 'tag-demo',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tag-demo.component.scss'],
 })
 export class TagDemo {
+  value1 = 'My value 1';
+  value2 = 'My value 2';
+  canRemove = false;
+  hasKey = false;
 
+  doRemove(tag: DtTag<string>): void {
+    window.alert(`Tag removed: ${tag.value}`);
+  }
 }
