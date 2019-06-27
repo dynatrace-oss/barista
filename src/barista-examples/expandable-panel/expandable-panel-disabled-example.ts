b53c686e4ccde4926054a22190769c4a1871e7e0
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-06-27 17:38:45 +0200

    APM-156144 feat(expandable-panel): Deprecated unnecessary inputs (opened, disabled) as these values should be handled by the panel itself

--- src/barista-examples/expandable-panel/expandable-panel-disabled-example.ts
+++ src/barista-examples/expandable-panel/expandable-panel-disabled-example.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'demo-component',
-  template: `<dt-expandable-panel #panel>
+  template: `
+<dt-expandable-panel #panel disabled>
   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
   dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
   Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
@@ -11,7 +12,6 @@ import { Component } from '@angular/core';
   magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
   Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 </dt-expandable-panel>
-<button dt-button (click)="panel.toggle()">Toggle</button>`,
+<button dt-button [dtExpandablePanel]="panel">Toggle expandable panel</button>`,
 })
-export class DefaultExpandablePanelExampleComponent {
-}
+export class DisabledExpandablePanelExample {}
---
+++ src/barista-examples/expandable-panel/expandable-panel-dynamic-trigger-example.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'demo-component',
+  template: `
+<dt-expandable-panel #panel>
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+  Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+  magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+  Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</dt-expandable-panel>
+<button dt-button [dtExpandablePanel]="panel">
+ Toggle trigger (<span *ngIf="panel.expanded">close panel</span><span *ngIf="!panel.expanded">open panel</span>)
+</button>`,
+})
+export class DynamicTriggerExpandablePanelExample {}
