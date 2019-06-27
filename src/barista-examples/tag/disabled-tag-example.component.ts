2f02b4f5310c9ea68e25fd62c77c0b5418500d25
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-06-27 14:13:38 +0200

    APM-179088 feat(tag): Deprecated disabled state for DtTag since there is no proper use case for it

--- src/barista-examples/tag/disabled-tag-example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'demo-component',
-  template: '<dt-tag disabled>Disabled tag</dt-tag>',
-})
-export class DisabledTagExampleComponent { }
