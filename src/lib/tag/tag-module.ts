5731d08a398b245eef014faa0cd0b7bea145f83c
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-06-28 15:19:18 +0200

    APM-185367 fix(tag): Made the remove button focusable instead of the tag itself

--- src/lib/tag/tag-module.ts
+++ src/lib/tag/tag-module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
+import { DtButtonModule } from '@dynatrace/angular-components/button';
 import { DtTag, DtTagKey } from './tag';
 import { CommonModule } from '@angular/common';
 import { DtIconModule } from '@dynatrace/angular-components/icon';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DtIconModule,
-  ],
-  exports: [
-    DtTag,
-    DtTagKey,
-    DtIconModule,
-  ],
-  declarations: [
-    DtTag,
-    DtTagKey,
-  ],
+  imports: [CommonModule, DtIconModule, DtButtonModule],
+  exports: [DtTag, DtTagKey],
+  declarations: [DtTag, DtTagKey],
 })
-export class DtTagModule { }
+export class DtTagModule {}
