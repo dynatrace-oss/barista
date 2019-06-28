fad8b674defd54c0817c58f2906614e230c501a7
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-06-28 13:12:59 +0200

    APM-156144 feat(expandable-section): Added expanded input and expandChange, expanded and collapsed outputs, according to the updates of the expandable panel component

--- src/barista-examples/expandable-panel/expandable-panel-disabled-example.ts
+++ src/barista-examples/expandable-panel/expandable-panel-disabled-example.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'demo-component',
   template: `
-<dt-expandable-panel #panel disabled>
+<dt-expandable-panel #panel disabled [expanded]="true">
   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
   dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
   Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
