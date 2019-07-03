9ec519c63ca03e2b2601e86a13b1179fd3acf7ba
Lukas Holzer <lukas.holzer@typeflow.cc>
2019-07-03 15:07:13 +0200

    APM-186060 fix(highlight): Fixes an issue where the output text casing was wrongfully transformed when highlighting.

--- src/lib/highlight/highlight.ts
+++ src/lib/highlight/highlight.ts
@@ -30,6 +30,11 @@ function removeNodeChildren(parent: Element): void {
   }
 }
 
+/** Escapes all characters that could be dangerous for a regular expression */
+function escapeRegExp(text: string): string {
+  return text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
 @Component({
   moduleId: module.id,
   selector: 'dt-highlight, [dt-highlight]',
@@ -66,15 +71,18 @@ export class DtHighlight implements AfterContentChecked, OnChanges {
   @Input() term: string;
 
   /** @internal */
-  @ViewChild('source', { static: true }) _sourceElement: ElementRef<HTMLElement>;
+  @ViewChild('source', { static: true })
+  _sourceElement: ElementRef<HTMLElement>;
 
   /** @internal */
-  @ViewChild('transformed', { static: true }) _transformedElement: ElementRef<HTMLElement>;
+  @ViewChild('transformed', { static: true })
+  _transformedElement: ElementRef<HTMLElement>;
 
   constructor(
     private _renderer: Renderer2,
     private _zone: NgZone,
-    @Inject(PLATFORM_ID) private _platformId: string) {}
+    @Inject(PLATFORM_ID) private _platformId: string
+  ) {}
 
   /** Highlight if either the term or the caseSensitive input changes. */
   ngOnChanges(): void {
@@ -83,16 +91,14 @@ export class DtHighlight implements AfterContentChecked, OnChanges {
 
   /** Highlight if the content of the highlight component changes. */
   ngAfterContentChecked(): void {
-    this._zone.onMicrotaskEmpty
-      .pipe(take(1))
-      .subscribe(() => {
-        // check if the text has actually changed, if not skip the highlight.
-        const textContent = this._getTextContent();
-        if (textContent === this._lastTextContent) {
-          return;
-        }
-        this._highlight(textContent);
-      });
+    this._zone.onMicrotaskEmpty.pipe(take(1)).subscribe(() => {
+      // check if the text has actually changed, if not skip the highlight.
+      const textContent = this._getTextContent();
+      if (textContent === this._lastTextContent) {
+        return;
+      }
+      this._highlight(textContent);
+    });
   }
 
   /** Get the textContent of the highlight component. */
@@ -106,7 +112,6 @@ export class DtHighlight implements AfterContentChecked, OnChanges {
 
   /** The highlight function triggers the highlighting process if we are in a browser context. */
   private _highlight(content?: string): void {
-
     const textContent = content || this._getTextContent();
     // clear the transformed element output
     removeNodeChildren(this._transformedElement.nativeElement);
@@ -117,50 +122,21 @@ export class DtHighlight implements AfterContentChecked, OnChanges {
     }
   }
 
-  /** Split the incoming text into text tokens by the search term. */
-  private _getTextTokens(content: string): string[] {
-    const testContent = this._caseSensitive ? content : content.toLowerCase();
-    const term = this._caseSensitive ? this.term : this.term.toLowerCase();
-    const textTokens: string[] = [];
-    let termIndex = testContent.indexOf(term);
-
-    if (termIndex > -1) {
-      // If the term is found within the input string, continue searching.
-      let cursor = 0;
-      while (termIndex > -1) {
-        // Push the string between the last found index and the termIndex and move the cursor forward.
-        if (termIndex !== 0) {
-          textTokens.push(content.slice(cursor, cursor + termIndex));
-          cursor = cursor + termIndex;
-        }
-        // Push the actual matched term and move the cursor forward.
-        textTokens.push(content.slice(cursor, cursor + term.length));
-        cursor = cursor + term.length;
-        // Search the next occurrence of the term in the remainder of the string.
-        termIndex = testContent.slice(cursor).indexOf(term);
-      }
-      // Push the remainder of the content string.
-      if (cursor < content.length) {
-        textTokens.push(testContent.slice(cursor));
-      }
-    } else {
-      // If the term is not found in the input string, return the whole string as a single token.
-      textTokens.push(content);
-    }
-    return textTokens;
-  }
-
   /**
    * The transform function wraps all occurrences of the provided string
    * in inline elements that are highlighted.
    */
   private _transformAndDisplay(content: string): void {
-    // Generate the textTokens to match
-    const textTokens = (this.term && this.term.length)
-      ? this._getTextTokens(content)
-      : [content];
+    let textTokens = [content];
+
+    if (this.term && this.term.length) {
+      const flags = this._caseSensitive ? 'gm' : 'gmi';
+      const regExp = new RegExp(`(${escapeRegExp(this.term)})`, flags);
+      textTokens = content.split(regExp).filter((s) => s.length);
+    }
 
     const max = textTokens.length;
+
     for (let i = 0; i < max; i += 1) {
       const node = textTokens[i];
       const text = this._renderer.createText(node);
@@ -170,9 +146,15 @@ export class DtHighlight implements AfterContentChecked, OnChanges {
         addCssClass(span, HIGHLIGHTED_CLASS);
         // append the created span with the class
         this._renderer.appendChild(span, text);
-        this._renderer.appendChild(this._transformedElement.nativeElement, span);
+        this._renderer.appendChild(
+          this._transformedElement.nativeElement,
+          span
+        );
       } else {
-        this._renderer.appendChild(this._transformedElement.nativeElement, text);
+        this._renderer.appendChild(
+          this._transformedElement.nativeElement,
+          text
+        );
       }
     }
   }
