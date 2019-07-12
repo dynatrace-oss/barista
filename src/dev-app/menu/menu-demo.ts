4504971a69e4567b1c92fce5730f002f4b6f669a
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-07-12 12:51:35 +0200

    APM-175335 feat(menu): Added menu component

---
+++ src/dev-app/menu/menu-demo.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { DtDrawer } from '@dynatrace/angular-components/drawer';
+
+@Component({
+  moduleId: module.id,
+  selector: 'demo-component',
+  templateUrl: 'menu-demo.html',
+  styleUrls: ['menu-demo.scss'],
+})
+export class MenuDemo {
+  @ViewChild(DtDrawer, { static: true }) drawer: DtDrawer;
+
+  toggle(): void {
+    this.drawer.toggle();
+  }
+
+  doSomething(): void {
+    window.alert('Did something!');
+  }
+}
--- src/linting/rules/dtMenuAltTextRule.ts
+++ src/linting/rules/dtMenuAltTextRule.ts
@@ -2,35 +2,51 @@ import { ElementAst } from '@angular/compiler';
 import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
 import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
 import { SourceFile } from 'typescript';
-import { addFailure } from '<%= category ? '../../' : '../' %>utils';
+import {
+  addFailure,
+  hasTextContentAlternative,
+  isElementWithName,
+} from '../utils';
 
-class <%= classify(name) %>Visitor extends BasicTemplateAstVisitor {
+class DtMenuVisitor extends BasicTemplateAstVisitor {
   visitElement(element: ElementAst, context: any): void {
     this._validateElement(element);
     super.visitElement(element, context);
   }
 
   private _validateElement(element: ElementAst): any {
-    addFailure(this, element, 'Something is wrong here.');
+    if (!isElementWithName(element, 'dt-menu')) {
+      return;
+    }
+
+    if (hasTextContentAlternative(element, 'aria-label')) {
+      return;
+    }
+
+    addFailure(this, element, 'A dt-menu must have an aria-label.');
   }
 }
 
 /**
- * The <%= camelize(name) %>Rule ensures that ~~~insert description here~~~.
+ * The dtMenuAltTextRule ensures that a dt-menu always has an aria-label set.
  *
  * The following example passes the lint checks:
- * ~~~Add example code here~~~
+ * <dt-menu aria-label="Example Menu">
+ *   <a dtMenuItem>Menu item</a>
+ * </dt-menu>
  *
  * For the following example the linter throws errors:
- * ~~~Add example code here~~~
+ * <dt-menu>
+ *   <a dtMenuItem>Menu item</a>
+ * </dt-menu>
  */
 export class Rule extends Rules.AbstractRule {
   static readonly metadata: IRuleMetadata = {
-    description: 'Ensures that ~~~insert description here~~~.',
+    description: 'Ensures that a dt-menu always has an aria-label set.',
     options: null, // tslint:disable-line:no-null-keyword
     optionsDescription: 'Not configurable.',
-    rationale: '~~~insert rationale here~~~.',
-    ruleName: '<%= name %>',
+    rationale: 'A dt-menu must have an aria-label that describes the menu.',
+    ruleName: 'dt-menu-alt-text',
     type: 'maintainability',
     typescriptOnly: true,
   };
@@ -38,7 +54,7 @@ export class Rule extends Rules.AbstractRule {
   apply(sourceFile: SourceFile): RuleFailure[] {
     return this.applyWithWalker(
       new NgWalker(sourceFile, this.getOptions(), {
-        templateVisitorCtrl: <%= classify(name) %>Visitor,
+        templateVisitorCtrl: DtMenuVisitor,
       }),
     );
   }
