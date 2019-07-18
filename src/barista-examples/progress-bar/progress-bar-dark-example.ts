1c19d5b7c3087461545214ed56e752c585a3b1e2
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-07-18 13:53:25 +0200

    APM-186242 docs(progress-bar): add dark theme example

---
+++ src/barista-examples/progress-bar/progress-bar-dark-example.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'demo-component',
+  template: `
+    <div class="dark" dtTheme=":dark">
+      <dt-progress-bar [value]="value">
+        <dt-progress-bar-description>
+          We found more than 100 results. This may take a while, consider
+          narrowing your search.
+        </dt-progress-bar-description>
+        <dt-progress-bar-count>{{ value }}% loaded</dt-progress-bar-count>
+      </dt-progress-bar>
+    </div>
+  `,
+})
+export class ProgressBarDarkExample {
+  value = 67;
+}
