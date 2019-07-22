786933ca6cdb82a0be2d6471f839dca459bb100b
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-07-22 13:11:49 +0200

    APM-186679 chore(prettier): Reformat codebase with htmlWhitespaceSensitivity set to 'css'

--- src/barista-examples/copy-to-clipboard/copy-to-clipboard-default-example.ts
+++ src/barista-examples/copy-to-clipboard/copy-to-clipboard-default-example.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'barista-demo',
   template: `
     <dt-copy-to-clipboard>
+      <!-- prettier-ignore -->
       <textarea dtInput aria-label="Text that is copied to clipboard">
-https://defaultcopy.dynatrace.com/</textarea
-      >
+https://defaultcopy.dynatrace.com/</textarea>
       <dt-copy-to-clipboard-label>Copy</dt-copy-to-clipboard-label>
     </dt-copy-to-clipboard>
   `,
