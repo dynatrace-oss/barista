4a8106b9cc784c956bde5ca5de512572e4935788
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-07-22 16:41:46 +0200

    APM-186018 chore(show-more): prepare refactoring and deprecate changed output and show less label directive

--- src/lib/show-more/show-more-module.ts
+++ src/lib/show-more/show-more-module.ts
@@ -3,9 +3,11 @@ import { DtShowMore, DtShowLessLabel } from './show-more';
 import { DtIconModule } from '@dynatrace/angular-components/icon';
 import { CommonModule } from '@angular/common';
 
+// tslint:disable:deprecation
 @NgModule({
   imports: [CommonModule, DtIconModule],
   exports: [DtShowMore, DtShowLessLabel, DtIconModule],
   declarations: [DtShowMore, DtShowLessLabel],
 })
 export class DtShowMoreModule {}
+// tslint:enable:deprecation
