14fb4bba6fbdbb930484eeb51c63f175f5cb1bc1
Thomas Pink <thomas.pink@me.com>
2019-07-23 11:04:27 +0200

    APM-177950 feat(timeline-chart): Added possibility to set key timing marker

--- src/lib/timeline-chart/timeline-chart-directives.ts
+++ src/lib/timeline-chart/timeline-chart-directives.ts
@@ -1,8 +1,15 @@
-import { Directive, Input, Component, ViewChild } from '@angular/core';
+import {
+  Input,
+  Component,
+  ViewChild,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+} from '@angular/core';
 import { coerceNumberProperty } from '@angular/cdk/coercion';
 import { CdkPortal } from '@angular/cdk/portal';
 
 export class DtTimelineChartMarker {
+  /** The actual value of the marker. */
   @Input()
   get value(): number {
     return this._value;
@@ -12,25 +19,34 @@ export class DtTimelineChartMarker {
   }
   private _value = 0;
 
+  /** The identifier character. E.g. "V" */
   @Input() identifier: string;
 
+  /** Internal portal for projecting the content of the marker to other places in the timeline chart (e.g. the legend) */
   @ViewChild(CdkPortal, { static: true }) _contentPortal: CdkPortal;
 }
 
 @Component({
   selector: 'dt-timeline-chart-timing-marker',
+  templateUrl: `./timeline-chart-marker.html`,
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
   providers: [
     {
       provide: DtTimelineChartMarker,
       useExisting: DtTimelineChartTimingMarker,
     },
   ],
-  templateUrl: `./timeline-chart-marker.html`,
 })
 export class DtTimelineChartTimingMarker extends DtTimelineChartMarker {}
 
-@Directive({
+@Component({
   selector: 'dt-timeline-chart-key-timing-marker',
+  templateUrl: `./timeline-chart-marker.html`,
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
   providers: [
     {
       provide: DtTimelineChartMarker,
--- src/lib/timeline-chart/timeline-chart.ts
+++ src/lib/timeline-chart/timeline-chart.ts
@@ -8,12 +8,17 @@ import {
   QueryList,
   AfterContentInit,
   OnDestroy,
+  ElementRef,
 } from '@angular/core';
 import { coerceNumberProperty } from '@angular/cdk/coercion';
 import { scaleLinear, ScaleLinear } from 'd3-scale';
-import { DtTimelineChartTimingMarker } from './timeline-chart-directives';
-import { Subject } from 'rxjs';
+import { Subject, merge } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { DtViewportResizer } from '@dynatrace/angular-components/core';
+import {
+  DtTimelineChartTimingMarker,
+  DtTimelineChartKeyTimingMarker,
+} from './timeline-chart-directives';
 
 const POSITION_PRECISION = 10000;
 
@@ -31,20 +36,28 @@ const POSITION_PRECISION = 10000;
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class DtTimelineChart implements AfterContentInit, OnDestroy {
+  /** The unit of the provided values. Needs to be a time-unit like "s". */
   @Input() unit = '';
 
+  /**
+   * The value (length) of the timeline chart.
+   * In most cases it is the same value as the last timing marker.
+   */
   @Input()
   get value(): number {
     return this._value;
   }
   set value(value: number) {
     this._value = coerceNumberProperty(value);
-    this._update();
+    this._updateRenderValues();
   }
   private _value = 0;
 
   @ContentChildren(DtTimelineChartTimingMarker)
   _timingMarkers: QueryList<DtTimelineChartTimingMarker>;
+
+  @ContentChildren(DtTimelineChartKeyTimingMarker)
+  _keyTimingMarkers: QueryList<DtTimelineChartKeyTimingMarker>;
 
   _renderBars: number[] = [];
   _renderTicks: { position: number; value: number }[] = [];
@@ -52,16 +65,31 @@ export class DtTimelineChart implements AfterContentInit, OnDestroy {
     position: number;
     marker: DtTimelineChartTimingMarker;
   }[] = [];
+  _renderKeyTimingMarkers: {
+    position: number;
+    marker: DtTimelineChartKeyTimingMarker;
+  }[] = [];
 
   private _destroy = new Subject<void>();
 
-  constructor(private _changeDetectorRef: ChangeDetectorRef) {}
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef,
+    private _viewportResizer: DtViewportResizer,
+    private _elementRef: ElementRef
+  ) {}
 
   ngAfterContentInit(): void {
-    this._timingMarkers.changes.pipe(takeUntil(this._destroy)).subscribe(() => {
-      this._update();
-    });
-    this._update();
+    merge(
+      this._timingMarkers.changes,
+      this._keyTimingMarkers,
+      this._viewportResizer.change()
+    )
+      .pipe(takeUntil(this._destroy))
+      .subscribe(() => {
+        this._updateRenderValues();
+      });
+
+    this._updateRenderValues();
   }
 
   ngOnDestroy(): void {
@@ -69,31 +97,37 @@ export class DtTimelineChart implements AfterContentInit, OnDestroy {
     this._destroy.complete();
   }
 
-  private _update(): void {
+  /** Updates all the values (bars, ticks, markers) that are rendered to the DOM. */
+  private _updateRenderValues(): void {
+    const tickAmount = this._getTickAmount();
+
     // tslint:disable: no-magic-numbers
     const scale = scaleLinear()
       .domain([0, this._value])
       .range([0, POSITION_PRECISION])
       .nice();
-    let ticks = scale.ticks(4);
+    let ticks = scale.ticks(tickAmount);
     const last = ticks[ticks.length - 1];
     if (last < this._value && ticks.length > 1) {
       scale.domain([0, last + ticks[1]]);
-      ticks = scale.ticks(4);
+      ticks = scale.ticks(tickAmount);
     }
     // tslint:enable: no-magic-numbers
 
     this._updateRenderBars(scale);
     this._updateRenderTicks(scale, ticks);
     this._updateRenderTimingMarkers(scale);
+    this._updateRenderKeyTimingMarkers(scale);
 
     this._changeDetectorRef.markForCheck();
   }
 
+  /** Calculates the position and length of the bar objects that are actually rendered. */
   private _updateRenderBars(scale: ScaleLinear<number, number>): void {
     this._renderBars = [valueToPercentage(this._value, scale)];
   }
 
+  /** Calculates the position and updates the tick objects that are actually rendered. */
   private _updateRenderTicks(
     scale: ScaleLinear<number, number>,
     ticks: number[]
@@ -104,6 +138,7 @@ export class DtTimelineChart implements AfterContentInit, OnDestroy {
     }));
   }
 
+  /** Calculates the position and updates the timing marker objects that are actually rendered. */
   private _updateRenderTimingMarkers(scale: ScaleLinear<number, number>): void {
     this._renderTimingMarkers = this._timingMarkers
       ? this._timingMarkers.toArray().map(marker => ({
@@ -111,6 +146,32 @@ export class DtTimelineChart implements AfterContentInit, OnDestroy {
           marker,
         }))
       : [];
+  }
+
+  /** Calculates the position and updates the key timing marker objects that are actually rendered. */
+  private _updateRenderKeyTimingMarkers(
+    scale: ScaleLinear<number, number>
+  ): void {
+    this._renderKeyTimingMarkers = this._keyTimingMarkers
+      ? this._keyTimingMarkers.toArray().map(marker => ({
+          position: valueToPercentage(marker.value, scale),
+          marker,
+        }))
+      : [];
+  }
+
+  /** Returns the amount of ticks based on the component with. More space means more ticks. */
+  private _getTickAmount() {
+    if (document && this._elementRef.nativeElement) {
+      const width = (this._elementRef.nativeElement as HTMLElement).clientWidth;
+      if (width > 1200) {
+        return 10;
+      }
+      if (width > 600) {
+        return 6;
+      }
+    }
+    return 4;
   }
 }
 
