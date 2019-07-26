1a1071eebf13e5fe3c5e501d3aa8433d4b90cac5
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-07-26 13:28:05 +0200

    APM-186682 feat(schematics): Create new schematic for generating linting rules

---
+++ src/schematics/dt-linting-rule/files/linting/rules/__name@camelize__Rule.ts
@@ -0,0 +1,44 @@
+import { ElementAst } from '@angular/compiler';
+import { BasicTemplateAstVisitor, NgWalker } from 'codelyzer';
+import { IRuleMetadata, RuleFailure, Rules } from 'tslint';
+import { SourceFile } from 'typescript';
+
+class <%=classify(name)%>Visitor extends BasicTemplateAstVisitor {
+  visitElement(element: ElementAst, context: any): void {
+    this._validateElement(element);
+    super.visitElement(element, context);
+  }
+
+  private _validateElement(element: ElementAst): any {
+    // addFailure(this, element, 'Something is wrong.');
+  }
+}
+
+/**
+ * The <%=camelize(name)%>Rule ensures that [insert description here].
+ *
+ * The following example passes the lint checks:
+ * [Add example code here]
+ *
+ * For the following example the linter throws errors:
+ * [Add example code here]
+ */
+export class Rule extends Rules.AbstractRule {
+  static readonly metadata: IRuleMetadata = {
+    description: 'Ensures that [insert description here].',
+    options: null, // tslint:disable-line:no-null-keyword
+    optionsDescription: 'Not configurable.',
+    rationale: '[insert rationale here].',
+    ruleName: '<%=name%>',
+    type: 'maintainability',
+    typescriptOnly: true,
+  };
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    return this.applyWithWalker(
+      new NgWalker(sourceFile, this.getOptions(), {
+        templateVisitorCtrl: <%=classify(name)%>Visitor,
+      }),
+    );
+  }
+}
