94ae69f54ed8d0467b07c38e35880e3a419fdba9
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-08-09 16:52:51 +0200

    APM-162175 feat(tslint): Added tslint rule to check whether public fields have associated JSDoc

---
+++ tools/tslint-rules/utils.ts
@@ -0,0 +1,98 @@
+import { performance, PerformanceObserver } from 'perf_hooks';
+import { WalkContext } from 'tslint';
+import * as ts from 'typescript';
+
+export type MemberDeclaration =
+  | ts.GetAccessorDeclaration
+  | ts.SetAccessorDeclaration
+  | ts.PropertyDeclaration
+  | ts.MethodDeclaration;
+
+// tslint:disable-next-line:no-any
+export type ContextWalker = (context: WalkContext<any>) => void;
+
+export type MemberDeclarationVerifier = (
+  context: WalkContext<any>, // tslint:disable-line:no-any
+  verifyDeclaration: MemberDeclaration,
+) => void;
+
+export function createMemberDeclarationWalker(
+  verifyDeclaration: MemberDeclarationVerifier,
+): ContextWalker {
+  // tslint:disable-next-line:no-any
+  return (context: WalkContext<any>) => {
+    const verifyNode = (node: ts.Node) => {
+      switch (node.kind) {
+        case ts.SyntaxKind.GetAccessor:
+          verifyDeclaration(context, node as ts.GetAccessorDeclaration);
+          break;
+        case ts.SyntaxKind.SetAccessor:
+          verifyDeclaration(context, node as ts.SetAccessorDeclaration);
+          break;
+        case ts.SyntaxKind.PropertyDeclaration:
+          verifyDeclaration(context, node as ts.PropertyDeclaration);
+          break;
+        case ts.SyntaxKind.MethodDeclaration:
+          verifyDeclaration(context, node as ts.MethodDeclaration);
+          break;
+        case ts.SyntaxKind.ClassDeclaration:
+        case ts.SyntaxKind.InterfaceDeclaration:
+          ts.forEachChild(node, verifyNode);
+          break;
+        default: // ignore other node types
+      }
+    };
+
+    ts.forEachChild(context.sourceFile, verifyNode);
+  };
+}
+
+const globalWithProfiling: NodeJS.Global & {
+  _tslintRulesTotalDurations?: { [ruleName: string]: number };
+} = global;
+
+function setUpProfiling(): void {
+  globalWithProfiling._tslintRulesTotalDurations = {};
+
+  process.on('exit', () => {
+    const totalDurations = globalWithProfiling._tslintRulesTotalDurations;
+
+    for (const ruleName in totalDurations) {
+      if (totalDurations.hasOwnProperty(ruleName)) {
+        // tslint:disable-next-line:no-console
+        console.log(
+          `Total duration for "${ruleName}": ${totalDurations[ruleName]}ms`,
+        );
+      }
+    }
+  });
+}
+
+export function startProfiling(ruleName: string): void {
+  if (globalWithProfiling._tslintRulesTotalDurations === undefined) {
+    setUpProfiling();
+  }
+
+  const totalDurations = globalWithProfiling._tslintRulesTotalDurations!;
+
+  totalDurations[ruleName] = 0;
+
+  const performanceObserver = new PerformanceObserver(items => {
+    const duration = items.getEntries()[0].duration;
+
+    totalDurations[ruleName] += duration;
+    performance.clearMarks();
+  });
+
+  performanceObserver.observe({ entryTypes: ['measure'] });
+  performance.mark(`${ruleName}-start`);
+}
+
+export function stopProfiling(ruleName: string): void {
+  performance.mark(`${ruleName}-stop`);
+  performance.measure(
+    `${ruleName}-start to ${ruleName}-stop`,
+    `${ruleName}-start`,
+    `${ruleName}-stop`,
+  );
+}
