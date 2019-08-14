aeec83194069592e32cb18f94a031436fc9fd382
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-08-14 13:54:50 +0200

    APM-189172 chore(dev-app): Remove options for bootstrapModule() in dev-app

--- src/dev-app/main.ts
+++ src/dev-app/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { preserveWhitespaces: false })
+  .bootstrapModule(AppModule)
   // tslint:disable-next-line:no-console typedef
   .catch((err): void => {
     console.log(err);
