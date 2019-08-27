5a571195fd72ac57bbebd7f25ba36b2d9d4c98ee
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-08-27 09:30:06 +0200

    APM-194471 chore(tslint): Implemented utility for creating a visitor class that checks an element for ARIA labels

--- src/linting/utils/altText.ts
+++ src/linting/utils/altText.ts
@@ -3,9 +3,14 @@ import {
   BoundElementPropertyAst,
   ElementAst,
 } from '@angular/compiler';
+import { BasicTemplateAstVisitor, TemplateAstVisitorCtr } from 'codelyzer';
+
+import { addFailure } from './addFailure';
+import { isElementWithName } from './isElementWithName';
 
 /**
  * Checks for attribute or bindings with given name.
+ *
  * @param attrs element attributes.
  * @param inputs element inputs / attribute bindings.
  * @returns true if aria-labelledby attribute or binding is set, false otherwise.
@@ -27,13 +32,14 @@ function hasAttributeValue(
 /**
  * Checks if the given element provides text alternatives in form of an aria-label
  * or aria-labelledby attribute.
+ *
  * @param element the element to check.
  * @param attribute the attribute to look for (optional); aria-label and aria-labelledby when not given
  * @returns whether a text alternative is given.
  */
 export function hasTextContentAlternative(
   element: ElementAst,
-  attribute?: string,
+  attribute?: string, // TODO ChMa: remove this parameter and always assume the latter
 ): boolean {
   const attrs: AttrAst[] = element.attrs;
   const inputs: BoundElementPropertyAst[] = element.inputs;
@@ -46,4 +52,35 @@ export function hasTextContentAlternative(
   }
 
   return hasAttributeValue(attribute, attrs, inputs);
+}
+
+/**
+ * Creates a TemplateAstVisitor class that fails if the given element does have
+ * the given element name and does not provide text alternatives in form of an
+ * aria-label or aria-labelledby attribute.
+ *
+ * @param elementName The name of the element to check
+ * @param attribute The attribute to look for (optional); aria-label and aria-labelledby when not specified TODO ChMa: remove this parameter and always assume the latter
+ */
+export function createAltTextVisitor(
+  elementName: string,
+  attribute?: string, // TODO ChMa: remove this parameter and always assume the latter
+): TemplateAstVisitorCtr {
+  return class AltTextVisitor extends BasicTemplateAstVisitor {
+    visitElement(element: ElementAst, context: any): void {
+      if (
+        isElementWithName(element, elementName) &&
+        !hasTextContentAlternative(element, attribute)
+      ) {
+        const errorMessage =
+          attribute !== undefined
+            ? `A ${elementName} must have an ${attribute} attribute.`
+            : `A ${elementName} must either have an aria-label or an aria-labelledby attribute.`;
+
+        addFailure(this, element, errorMessage);
+      }
+
+      super.visitElement(element, context);
+    }
+  };
 }
