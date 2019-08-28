5248174cb968777872dbbe2d75f6b92740d1fe1c
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-08-28 10:38:04 +0200

    APM-194742 chore(schematics): Improve dt-component schematic

--- src/schematics/dt-component/files/barista-examples/__name@dasherize__/__name@dasherize__-default-example.ts
+++ src/schematics/dt-component/files/barista-examples/__name@dasherize__/__name@dasherize__-default-example.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: '<%= dasherize(name) %>-demo',
-  template: '<<%= selector %>></<%= selector %>>',
+  selector: '<%= dasherize(name) %>-barista-example',
+  template: `
+    <<%= selector %>></<%= selector %>>
+  `,
 })
 export class <%= classify(name) %>DefaultExample {}
