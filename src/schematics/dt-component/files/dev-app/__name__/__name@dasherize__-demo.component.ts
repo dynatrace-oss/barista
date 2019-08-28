410b6884bc9d89406af1986c2483a1ac143910c2
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2019-08-28 10:38:04 +0200

    APM-194742 chore(schematics): Improve dt-component schematic

--- src/schematics/dt-component/files/dev-app/__name__/__name@dasherize__-demo.component.ts
+++ src/schematics/dt-component/files/dev-app/__name__/__name@dasherize__-demo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: '<%= dasherize(name) %>-demo',
+  selector: '<%= dasherize(name) %>-dev-app-demo',
   templateUrl: '<%= dasherize(name) %>-demo.component.html',
   styleUrls: ['<%= dasherize(name) %>-demo.component.scss'],
 })
