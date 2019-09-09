b08f75b20979965e547da807d005c1e2edac7f0d
Rowa Audil <rowaaudil@gmail.com>
2019-09-09 08:57:15 +0200

    APM-195508 feat(tag-add): added tag-add and tag-list component.

--- src/dev-app/tag/tag-demo.component.ts
+++ src/dev-app/tag/tag-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { DtTag } from '@dynatrace/angular-components/tag';
 
@@ -7,13 +7,32 @@ import { DtTag } from '@dynatrace/angular-components/tag';
   templateUrl: './tag-demo.component.html',
   styleUrls: ['./tag-demo.component.scss'],
 })
-export class TagDemo {
+export class TagDemo implements OnInit {
+  tags = new Set<string>();
+
   value1 = 'My value 1';
   value2 = 'My value 2';
   value3 = 'My value 3';
   isDisabled = false;
   canRemove = false;
   hasKey = false;
+
+  ngOnInit(): void {
+    this.tags
+      .add('Jelly')
+      .add('Fish')
+      .add('Pear')
+      .add('Oreo')
+      .add('KitKat')
+      .add('Beer')
+      .add('Raphaelo')
+      .add('Bean')
+      .add('Pine');
+  }
+
+  addTag(tag: string): void {
+    this.tags.add(tag);
+  }
 
   doRemove(tag: DtTag<string>): void {
     window.alert(`Tag removed: ${tag.value}`);
