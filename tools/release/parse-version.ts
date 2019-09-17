dc229daba2ba5d237f98a02f1d5b4c202724015d
Thomas Pink <thomas.pink@me.com>
2019-09-17 14:27:48 +0200

    build: Adds support for releasing on version branches

--- tools/release/parse-version.ts
+++ tools/release/parse-version.ts
@@ -1,15 +1,18 @@
-/** Regular expression that matches version names and the individual version segments. */
+/**
+ * Regular expression that matches version names and
+ * the individual version segments.
+ */
 const versionNameRegex = /^(\d+)\.(\d+)\.(\d+)?$/;
 
 export class Version {
-
   constructor(
     /** Major version number */
     public major: number,
     /** Minor version number */
     public minor: number,
     /** Patch version number */
-    public patch: number) { }
+    public patch: number,
+  ) {}
 
   /** Serializes the version info into a string formatted version name. */
   format(): string {
@@ -19,18 +22,25 @@ export class Version {
   clone(): Version {
     return new Version(this.major, this.minor, this.patch);
   }
+
+  equals(other: Version): boolean {
+    return (
+      this.major === other.major &&
+      this.minor === other.minor &&
+      this.patch === other.patch
+    );
+  }
 }
 
 /**
- * Parses the specified version and returns an object that represents the individual
- * version segments.
+ * Parses the specified version and returns an object that
+ * represents the individual version segments.
  */
 export function parseVersionName(version: string): Version | null {
   const matches = version.match(versionNameRegex);
-  return matches ? new Version(
-    Number(matches[1]),
-    Number(matches[2]),
-    Number(matches[3])) : null;
+  return matches
+    ? new Version(Number(matches[1]), Number(matches[2]), Number(matches[3]))
+    : null;
 }
 
 /** Serializes the specified version into a string. */
