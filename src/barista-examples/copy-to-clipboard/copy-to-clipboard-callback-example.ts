db01524435f4872fe9450ce21ede882e07de2efe
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-09-26 12:57:04 +0200

    APM-199111 docs(copy-to-clipboard): reworked examples, made the input-field example the default example

--- src/barista-examples/copy-to-clipboard/copy-to-clipboard-callback-example.ts
+++ src/barista-examples/copy-to-clipboard/copy-to-clipboard-callback-example.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { timer } from 'rxjs';
 
 @Component({
   selector: 'barista-demo',
   template: `
     <dt-copy-to-clipboard (copied)="copyCallback()">
-      <!-- prettier-ignore -->
-      <textarea dtInput aria-label="Text that is copied to clipboard">
-https://copyexample.dynatrace.com/</textarea>
+      <input
+        dtInput
+        [value]="_value"
+        aria-label="Text that is copied to clipboard"
+      />
       <dt-copy-to-clipboard-label>Copy</dt-copy-to-clipboard-label>
     </dt-copy-to-clipboard>
     <br />
@@ -17,13 +19,17 @@ https://copyexample.dynatrace.com/</textarea>
   `,
 })
 export class CopyToClipboardCallbackExample {
-  _copyHint = 'Will change after copy';
+  _value = 'https://barista.dynatrace.com/';
+  _copyHint = 'Will change after copy.';
+
+  constructor(private _changeDetectorRef: ChangeDetectorRef) {}
 
   copyCallback(): void {
-    this._copyHint = 'Copy was done';
+    this._copyHint = `Copied "${this._value}" to clipboard.`;
     // tslint:disable-next-line:no-magic-numbers
     timer(2500).subscribe((): void => {
-      this._copyHint = 'Will change after copy';
+      this._copyHint = 'Will change after copy.';
+      this._changeDetectorRef.markForCheck();
     });
   }
 }
