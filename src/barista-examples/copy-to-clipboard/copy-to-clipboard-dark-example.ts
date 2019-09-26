ac28ffd60f8eb761864dbf82057e72511b451aee
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-09-26 12:57:04 +0200

    APM-199111 docs(copy-to-clipboard): reworked examples, made the input-field example the default example

--- src/barista-examples/copy-to-clipboard/copy-to-clipboard-dark-example.ts
+++ src/barista-examples/copy-to-clipboard/copy-to-clipboard-dark-example.ts
@@ -5,9 +5,11 @@ import { Component } from '@angular/core';
   template: `
     <section class="dark" dtTheme=":dark">
       <dt-copy-to-clipboard>
-        <!-- prettier-ignore -->
-        <textarea dtInput aria-label="Text that is copied to clipboard">
-https://dark.dynatrace.com/</textarea>
+        <input
+          dtInput
+          value="https://barista.dynatrace.com/"
+          aria-label="The value of this input field will be copied to clipboard."
+        />
         <dt-copy-to-clipboard-label>Copy</dt-copy-to-clipboard-label>
       </dt-copy-to-clipboard>
     </section>
