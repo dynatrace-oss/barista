6f82cfabc51d505136bb32d75e6fd78a5938b60a
Lara Aigmueller <lara.aigmueller@dynatrace.com>
2019-10-02 15:23:32 +0200

    APM-199057 barista: initial application setup

---
+++ src/barista/pages/page-content.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'ba-page-content',
+  template: '',
+})
+export class BaPageContent {
+  @Input()
+  get data(): string {
+    return this._data;
+  }
+  set data(value: string) {
+    this._data = value;
+    // TODO: is this okay?
+    // tslint:disable-next-line dt-ban-inner-html
+    this._elementRef.nativeElement.innerHTML = value;
+  }
+  private _data = '';
+
+  constructor(private _elementRef: ElementRef<HTMLElement>) {}
+}
