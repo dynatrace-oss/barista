12b5d622b6a34f555a0590ae158914a41f940db8
Todd Baert <toddbaert@gmail.com>
2019-10-03 15:56:56 -0400

    APM-201262 fix(confirmation-dialog): Fixes an issue where the dark theme was not applied.

--- src/lib/confirmation-dialog/confirmation-dialog-module.ts
+++ src/lib/confirmation-dialog/confirmation-dialog-module.ts
@@ -2,12 +2,14 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { DtThemingModule } from '@dynatrace/angular-components/theming';
+
 import { DtConfirmationDialog } from './confirmation-dialog';
 import { DtConfirmationDialogActions } from './confirmation-dialog-actions';
 import { DtConfirmationDialogState } from './confirmation-dialog-state/confirmation-dialog-state';
 
 @NgModule({
-  imports: [OverlayModule, CommonModule],
+  imports: [OverlayModule, DtThemingModule, CommonModule],
   exports: [
     DtConfirmationDialog,
     DtConfirmationDialogState,
