15a1fa3067fa09ae559c0e46ded7acc7d23f98fb
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-10-03 15:58:31 +0200

    barista(icon-color-wheel): Added icon color wheel component

---
+++ src/barista/components/icon-color-wheel/icon-color-wheel.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+
+import { BaIconColorWheel } from './icon-color-wheel';
+
+describe ('IconColorWheelComponent', () => {
+  let component: BaIconColorWheel;
+  let fixture: ComponentFixture<BaIconColorWheel>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BaIconColorWheel],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BaIconColorWheel);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
