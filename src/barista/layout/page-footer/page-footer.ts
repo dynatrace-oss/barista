b3dd01e4608d2a2213328603e52a37d10cbe177c
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-10-09 13:20:13 +0200

    APM-201970 barista(page-footer): add page footer component

---
+++ src/barista/layout/page-footer/page-footer.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ba-page-footer',
+  templateUrl: 'page-footer.html',
+  styleUrls: ['page-footer.scss'],
+  host: {
+    class: 'ba-page-footer',
+  },
+})
+export class BaPageFooter {
+  /** links to pages, which are related to the current page */
+  @Input() relatedTopics: string[];
+
+  /** keywords with which the current page is tagged */
+  @Input() tags: string[];
+
+  /** @internal */
+  get _hasTags(): boolean {
+    return this.tags.length > 0;
+  }
+
+  /** @internal */
+  get _hasRelatedTopics(): boolean {
+    return this.relatedTopics.length > 0;
+  }
+}
