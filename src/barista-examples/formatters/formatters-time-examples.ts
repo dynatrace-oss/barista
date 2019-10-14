4a02212dde8aa40f0a68d34fcdd50282b2bd0efa
Rowa Audil <rowa.audil@dynatrace.com>
2019-10-14 13:22:00 +0200

    APM-202569 feat(time-formatter): Added time formatter functionality

---
+++ src/barista-examples/formatters/formatters-time-examples.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'component-barista-example',
+  template: `
+    <dt-form-field>
+      <dt-label>Value to be transformed</dt-label>
+      <input dtInput [(ngModel)]="exampleValue" />
+    </dt-form-field>
+    <p>Ms: {{ exampleValue | dtTime }}</p>
+    <p>Hour: {{ exampleValue | dtTime: 'h' }}</p>
+    <p>Day to Second: {{ exampleValue | dtTime: 'd':'s' }}</p>
+  `,
+})
+export class FormattersTimeExample {
+  exampleValue: number;
+}
