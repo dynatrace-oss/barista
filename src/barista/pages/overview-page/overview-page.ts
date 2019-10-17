44eb600f9aa8f3ec5a64973f0ca74b15d4e98f9f
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-10-17 15:05:11 +0200

    APM-202274 barista(overview-page): create overview pages, wip

--- src/barista/pages/overview-page/overview-page.ts
+++ src/barista/pages/overview-page/overview-page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BaPage } from 'pages/page-outlet';
 import { BaOverviewPageContents } from 'shared/page-contents';
 
+const LOCALSTORAGEKEY = 'baristaGridview';
+
 @Component({
   selector: 'ba-overview-page',
   templateUrl: 'overview-page.html',
@@ -9,4 +11,22 @@ import { BaOverviewPageContents } from 'shared/page-contents';
 })
 export class BaOverviewPage implements BaPage {
   contents: BaOverviewPageContents;
+
+  _listViewActive = true;
+
+  constructor() {
+    if ('localStorage' in window) {
+      const localStorageState = localStorage.getItem(LOCALSTORAGEKEY);
+      this._listViewActive = localStorageState !== 'tiles';
+    }
+  }
+
+  _switchOverviewPageDisplay(): void {
+    this._listViewActive = !this._listViewActive;
+    if ('localStorage' in window) {
+      this._listViewActive
+        ? localStorage.setItem(LOCALSTORAGEKEY, 'list')
+        : localStorage.setItem(LOCALSTORAGEKEY, 'tiles');
+    }
+  }
 }
