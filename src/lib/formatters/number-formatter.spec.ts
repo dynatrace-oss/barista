e23dd9567347830a64eb76db71b67546a6aa9bae
Rowa Audil <rowaaudil@gmail.com>
2019-10-18 12:25:14 +0200

    APM-200736 fix(number-formatter): Fixed issue with negative inputs

--- src/lib/formatters/number-formatter.spec.ts
+++ src/lib/formatters/number-formatter.spec.ts
@@ -40,6 +40,26 @@ describe('FormatterUtil', () => {
         input: 987.654321,
         output: '988',
       },
+      {
+        input: 0.0001,
+        output: '< 0.001',
+      },
+      {
+        input: -123.45,
+        output: '-123',
+      },
+      {
+        input: -10,
+        output: '-10',
+      },
+      {
+        input: -0.987654321,
+        output: '-0.988',
+      },
+      {
+        input: -0.0001,
+        output: '-0.001',
+      },
     ].forEach((testCase: TestCase) => {
       it(`should return ${testCase.input} with adjusted precision`, () => {
         expect(adjustNumber(testCase.input).toString()).toEqual(
@@ -84,8 +104,56 @@ describe('FormatterUtil', () => {
         output: '123',
       },
       {
+        input: 0.1,
+        output: '0.1',
+      },
+      {
         input: 0.0001,
         output: '< 0.001',
+      },
+      {
+        input: -1234567890,
+        output: '-1.23bil',
+      },
+      {
+        input: -123456789,
+        output: '-123mil',
+      },
+      {
+        input: -12345678,
+        output: '-12.3mil',
+      },
+      {
+        input: -1234567,
+        output: '-1.23mil',
+      },
+      {
+        input: -123456,
+        output: '-123k',
+      },
+      {
+        input: -12345,
+        output: '-12.3k',
+      },
+      {
+        input: -1234,
+        output: '-1.23k',
+      },
+      {
+        input: -123,
+        output: '-123',
+      },
+      {
+        input: -12,
+        output: '-12',
+      },
+      {
+        input: -1,
+        output: '-1',
+      },
+      {
+        input: -0.0001,
+        output: '-0.001',
       },
     ].forEach((testCase: TestCase) => {
       it(`should return ${testCase.input} in abbreviated version`, () => {
