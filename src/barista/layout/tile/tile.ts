a6aa982b84c50ebfe34e080c24085f22e64ab715
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-10-25 08:32:40 +0200

    APM-202274 barista(overview-page): changes according to PR review

--- src/barista/layout/tile/tile.ts
+++ src/barista/layout/tile/tile.ts
@@ -14,21 +14,6 @@ export class BaTile {
   @Input() data: BaOverviewPageSectionItem;
   @Input() listView = true;
 
-  /** @internal get a css class according to the value of the badge */
-  get _cssClassComponentState(): string {
-    if (
-      this.data.badge &&
-      (this.data.badge === 'experimental' || this.data.badge === 'deprecated')
-    ) {
-      return `ba-tile-badge-warning`;
-    }
-
-    if (this.data.badge) {
-      return `ba-tile-badge-${this.data.badge}`;
-    }
-    return '';
-  }
-
   /** @internal whether the tile has the badge 'favorite' */
   get _favorite(): boolean {
     return (this.data.badge && this.data.badge === 'favorite') || false;
