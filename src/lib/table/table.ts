4db6bd97e66c3ff780a1066d07b2ed5282801d1a
Lukas Holzer <lukas.holzer@typeflow.cc>
2019-10-31 09:02:55 +0100

    APM-204760 fix(empty-state, table): Fixes an issue where the empty state was not layouted correctly inside a table on resizing.

--- src/lib/table/table.ts
+++ src/lib/table/table.ts
@@ -1,5 +1,6 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Platform } from '@angular/cdk/platform';
+import { CdkPortalOutlet, TemplatePortal } from '@angular/cdk/portal';
 import { DOCUMENT } from '@angular/common';
 import {
   Attribute,
@@ -14,12 +15,23 @@ import {
   NgZone,
   OnDestroy,
   QueryList,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
   ViewEncapsulation,
 } from '@angular/core';
-import { Observable, Subject, defer } from 'rxjs';
-import { map, startWith, switchMap, take, takeUntil } from 'rxjs/operators';
+import { Observable, Subject, Subscription, defer } from 'rxjs';
+import {
+  map,
+  mapTo,
+  startWith,
+  switchMap,
+  take,
+  takeUntil,
+} from 'rxjs/operators';
 
 import { isDefined } from '@dynatrace/angular-components/core';
+import { DtEmptyState } from '@dynatrace/angular-components/empty-state';
 
 import { _DtTableBase } from './base-table';
 import {
@@ -86,6 +98,17 @@ export class DtTable<T> extends _DtTableBase<T> implements OnDestroy {
     DtSimpleColumnBase<any> // tslint:disable-line:no-any
   >;
 
+  /** @internal The QueryList that holds the empty state component */
+  @ContentChildren(DtEmptyState) _emptyState: QueryList<DtEmptyState>;
+
+  /** @internal The template where the empty state component gets rendered inside */
+  @ViewChild('emptyStateTemplate', { static: true })
+  _emptyStateTemplate: TemplateRef<DtEmptyState>;
+
+  /** @internal The portal where the component will be projected in when we have to show the empty state. */
+  @ViewChild(CdkPortalOutlet, { static: true })
+  _portalOutlet: CdkPortalOutlet;
+
   /** @internal Stream of all simple dataAccessor functions for all SimpleColumns */
   readonly _dataAccessors: Observable<SimpleColumnsAccessorMaps<T>> = defer(
     () => {
@@ -140,6 +163,9 @@ export class DtTable<T> extends _DtTableBase<T> implements OnDestroy {
     },
   );
 
+  /** Subscription of attached stream of the portal outlet  */
+  private _portalOutletSubscription = Subscription.EMPTY;
+
   constructor(
     differs: IterableDiffers,
     changeDetectorRef: ChangeDetectorRef,
@@ -149,6 +175,7 @@ export class DtTable<T> extends _DtTableBase<T> implements OnDestroy {
     // tslint:disable-next-line: no-any
     @Inject(DOCUMENT) document: any,
     platform: Platform,
+    private _viewContainerRef: ViewContainerRef,
   ) {
     super(differs, changeDetectorRef, elementRef, document, platform, role);
   }
@@ -156,6 +183,7 @@ export class DtTable<T> extends _DtTableBase<T> implements OnDestroy {
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
+    this._portalOutletSubscription.unsubscribe();
   }
 
   /**
@@ -165,11 +193,39 @@ export class DtTable<T> extends _DtTableBase<T> implements OnDestroy {
    */
   renderRows(): void {
     super.renderRows();
-    if (this.isEmptyDataSource) {
-      this._changeDetectorRef.markForCheck();
+
+    // no need if there is no empty state provided via content projection
+    if (!this._emptyState.first) {
+      return;
+    }
+
+    // if we have a subscription we need to unsubscribe to re-subscribe later on
+    // we need to subscribe in this function in case that there is no other hook
+    // where we can hook into the cdk Table
+    if (this._portalOutletSubscription) {
+      this._portalOutletSubscription.unsubscribe();
+    }
+
+    this._portalOutletSubscription = this._portalOutlet.attached
+      .pipe(mapTo(this._emptyState.first))
+      .subscribe(emptyState => {
+        // Update the layout of the empty state after it was attached
+        emptyState._updateLayout();
+        emptyState._visible = true;
+      });
+
+    if (this.isEmptyDataSource && !this._portalOutlet.hasAttached()) {
+      const template = new TemplatePortal(
+        this._emptyStateTemplate,
+        this._viewContainerRef,
+      );
+      this._portalOutlet.attachTemplatePortal(template);
+    } else {
+      // ned to unset the visibility to have every time the component will be attached a fading animation.
+      this._emptyState.first._visible = false;
+      this._portalOutlet.detach();
     }
   }
-
   /** CSS class added to any row or cell that has sticky positioning applied. */
   protected stickyCssClass = 'dt-table-sticky';
 }
