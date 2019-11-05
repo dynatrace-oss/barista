1774d171e07ae8c4638efdcb126146ec588f68cf
Thomas Heller <thomas.heller@dynatrace.com>
2019-11-05 15:26:49 +0100

    docs(filter-field): Added a clear all example to the filter field

---
+++ src/barista-examples/filter-field/filter-field-clearall.ts
@@ -0,0 +1,62 @@
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  DtFilterField,
+  DtFilterFieldDefaultDataSource,
+} from '@dynatrace/angular-components/filter-field';
+
+@Component({
+  moduleId: module.id,
+  selector: 'component-barista-example',
+  template: `
+    <dt-filter-field
+      [dataSource]="_dataSource"
+      [filters]="_filters"
+      label="Filter by"
+      aria-label="Filter By Inputvalue"
+      clearAllLabel="Clear all"
+    ></dt-filter-field>
+  `,
+})
+export class FilterFieldClearAllExample<T> {
+  @ViewChild(DtFilterField, { static: true }) filterField: DtFilterField<T>;
+  private DATA = {
+    autocomplete: [
+      {
+        name: 'AUT',
+        autocomplete: ['Linz', 'Vienna', 'Graz'],
+      },
+      {
+        name: 'USA',
+        autocomplete: [
+          'San Francisco',
+          'Los Angeles',
+          'New York',
+          { name: 'Custom', suggestions: [] },
+        ],
+      },
+      {
+        name: 'Requests per minute',
+        range: {
+          operators: {
+            range: true,
+            equal: true,
+            greaterThanEqual: true,
+            lessThanEqual: true,
+          },
+          unit: 's',
+        },
+      },
+    ],
+  };
+
+  private _linzFilter = [
+    this.DATA.autocomplete[0],
+    this.DATA.autocomplete[0].autocomplete![0],
+  ];
+  _filters = [this._linzFilter];
+
+  _dataSource = new DtFilterFieldDefaultDataSource(this.DATA);
+
+  constructor() {}
+}
