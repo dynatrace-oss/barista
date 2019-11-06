321acd5efe14632c7c0f2d1094d5c461f4b5d8b8
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-11-06 11:09:07 +0100

    APM-204985 build: Rewrite protractor tests to testcafe.

---
+++ ui-tests/button/button.po.ts
@@ -0,0 +1,5 @@
+import { Selector } from 'testcafe';
+
+export const button = Selector('#test-button');
+export const disableButton = Selector('#disable-toggle');
+export const clickCounter = Selector('#click-counter');
