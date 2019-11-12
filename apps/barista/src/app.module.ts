18fee5350c9f21c3f46905262938c9ba479d5a1f
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-11-12 11:32:02 +0100

    barista(recently-ordered): added service for recently visited pages

--- apps/barista/src/app.module.ts
+++ apps/barista/src/app.module.ts
@@ -6,39 +6,39 @@ import {
   LocationStrategy,
   PathLocationStrategy,
 } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BaScrollSpyService } from './shared/scroll-spy.service';
-import { BaTocService } from './shared/toc.service';
 
+import { BaApp } from './app';
+import { BaContributors } from './layout/contributors/contributors';
+import { BaFooter } from './layout/footer/footer';
+import { BaIconColorWheel } from './components/icon-color-wheel/icon-color-wheel';
+import { BaIndexPage } from './pages/index-page/index-page';
+import { BaLocationService } from './shared/location.service';
+import { BaNav } from './layout/nav/nav';
+import { BaOverviewPage } from './pages/overview-page/overview-page';
+import { BaPageContent } from './pages/page-content';
+import { BaPageFooter } from './layout/page-footer/page-footer';
+import { BaPageHeader } from './layout/page-header/page-header';
+import { BaPageOutlet } from './pages/page-outlet';
+import { BaPageService } from './shared/page.service';
+import { BaRecentlyOrderedService } from './shared/recently-ordered.service';
+import { BaScrollSpyService } from './shared/scroll-spy.service';
+import { BaSearch } from './layout/search/search';
+import { BaSidenav } from './layout/sidenav/sidenav';
+import { BaSinglePage } from './pages/single-page/single-page';
+import { BaSmallTile } from './layout/smalltile/smalltile';
+import { BaTile } from './layout/tile/tile';
+import { BaToc } from './layout/toc/toc';
+import { BaTocService } from './shared/toc.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { DtIconModule } from '@dynatrace/barista-components/icon';
 import { DtOverlayModule } from '@dynatrace/barista-components/overlay';
 import { DtSwitchModule } from '@dynatrace/barista-components/switch';
 import { DtTagModule } from '@dynatrace/barista-components/tag';
-
-import { BaApp } from './app';
-import { BaIconColorWheel } from './components/icon-color-wheel/icon-color-wheel';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { environment } from './environments/environment';
-import { BaContributors } from './layout/contributors/contributors';
-import { BaFooter } from './layout/footer/footer';
-import { BaNav } from './layout/nav/nav';
-import { BaPageFooter } from './layout/page-footer/page-footer';
-import { BaPageHeader } from './layout/page-header/page-header';
-import { BaSearch } from './layout/search/search';
-import { BaSidenav } from './layout/sidenav/sidenav';
-import { BaSmallTile } from './layout/smalltile/smalltile';
-import { BaTile } from './layout/tile/tile';
-import { BaToc } from './layout/toc/toc';
-import { BaIndexPage } from './pages/index-page/index-page';
-import { BaOverviewPage } from './pages/overview-page/overview-page';
-import { BaPageContent } from './pages/page-content';
-import { BaPageOutlet } from './pages/page-outlet';
-import { BaSinglePage } from './pages/single-page/single-page';
-import { BaLocationService } from './shared/location.service';
-import { BaPageService } from './shared/page.service';
 
 @NgModule({
   imports: [
@@ -85,6 +85,7 @@ import { BaPageService } from './shared/page.service';
     BaLocationService,
     BaTocService,
     BaScrollSpyService,
+    BaRecentlyOrderedService,
   ],
   entryComponents: [
     BaSinglePage,
