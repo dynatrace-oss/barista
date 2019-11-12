b4686c990ba1142f58af208303c883efb65ed2f1
Nina Kammerlander <nina.kammerlander@dynatrace.com>
2019-11-12 08:32:51 +0100

    barista(build): Added missing frontmatter

--- tools/barista/src/types.ts
+++ tools/barista/src/types.ts
@@ -10,13 +10,25 @@ export interface BaPageBuildResult {
   pageContent: BaPageContent;
 }
 
+/** Contributors in page front matter */
+export interface BaContributors {
+  dev: string[];
+  ux: string[];
+}
+
 /** Structure of the generated JSON page output */
 export interface BaPageContent {
   title?: string;
   layout?: string;
   content?: string;
   description?: string;
+  public?: boolean;
+  contributors?: BaContributors;
+  toc?: boolean;
+  themable?: boolean;
+  wiki?: string;
   properties?: string[];
   tags?: string[];
+  related?: string[];
   nav_group?: string;
 }
