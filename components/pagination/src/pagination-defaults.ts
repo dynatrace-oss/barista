07fcd0005980b31a8893af6c528af9c8eb9685ff
Thomas Pink <thomas.pink@me.com>
2019-11-18 09:15:06 +0100

    chore: Added licence header to typescript files

--- components/pagination/src/pagination-defaults.ts
+++ components/pagination/src/pagination-defaults.ts
@@ -1,3 +1,19 @@
+/**
+ * @license
+ * Copyright 2019 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /** @internal The default page size if there is no page size and there are no provided page size options. */
 export const DEFAULT_PAGE_SIZE = 50;
 /** @internal The offset from the start and the end when it should start adding the ellipsis character */
