82d8740beca1929bb9615d209f1d0668786eb9c5
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-11-24 19:35:09 +0100

    build: Create own stylelint builder to get custom reports.

---
+++ tools/builders/stylelint/junit-formatter/existing-result.interface.ts
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2019 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface ExistingResult {
+  testsuites: ExistingTestSuites;
+}
+
+export interface ExistingTestSuites {
+  $: { package: string };
+  testsuite: ExisitingTestSuite[];
+}
+
+export interface ExisitingTestSuite {
+  $: {
+    name: string;
+    failures: string;
+    errors: string;
+    tests: string;
+  };
+  testcase: ExisitingTestCase[];
+}
+
+export interface ExisitingTestCase {
+  $: {
+    name: string;
+  };
+  failure?: ExisitingTestcaseFailure[];
+}
+
+export interface ExisitingTestcaseFailure {
+  _: string;
+  $: {
+    type: string;
+    message: string;
+  };
+}
---
+++ tools/builders/stylelint/junit-formatter/junit-formatter.ts
@@ -0,0 +1,61 @@
+/**
+ * @license
+ * Copyright 2019 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { dirname } from 'path';
+import { LintResult } from 'stylelint';
+import { parseStringPromise } from 'xml2js';
+import { create } from 'xmlbuilder';
+import { ExistingResult } from './existing-result.interface';
+import { parseExistingSuite } from './parse-exisiting-suite';
+import { parseSuite } from './parse-suite';
+import { ParsedSuite } from './parsed-suite.interface';
+
+export async function junitFormatter(
+  file: string,
+  stylelintResults: LintResult[],
+) {
+  const parsedResults = stylelintResults.map(result => parseSuite(result));
+
+  // if we have an existing xml add the linted files to the xml
+  if (existsSync(file)) {
+    const existing = readFileSync(file).toString();
+    const parsed: ExistingResult = await parseStringPromise(existing);
+    if (parsed.testsuites && parsed.testsuites.testsuite) {
+      const transformed = parsed.testsuites.testsuite.map(suite =>
+        parseExistingSuite(suite),
+      );
+      parsedResults.push(...transformed);
+    }
+  } else {
+    // create directory if it does not exist
+    mkdirSync(dirname(file), { recursive: true });
+  }
+
+  writeFileSync(file, createXML(parsedResults));
+}
+
+/** Creates an XML out of the provided object structure */
+function createXML(testSuites: ParsedSuite[]): string {
+  const xmlRoot = create('testsuites', { encoding: 'utf-8' }).att(
+    'package',
+    'stylelint.rules',
+  );
+
+  return testSuites.length > 0
+    ? xmlRoot.element(testSuites).end({ pretty: true })
+    : xmlRoot.end({ pretty: true });
+}
