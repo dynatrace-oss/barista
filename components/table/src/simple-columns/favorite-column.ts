4f87decb5e2331c38c3b7b94d15cdb036e33e14a
Yngrid Coello <yngrdyn@gmail.com>
2019-12-11 17:47:34 +0100

    feat(table): Added favourite-column

---
+++ components/table/src/simple-columns/favorite-column.ts
@@ -0,0 +1,67 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Optional,
+  Output,
+  ViewEncapsulation,
+} from '@angular/core';
+import { DtTable } from '../table';
+import { DtSimpleColumnBase } from './simple-column-base';
+
+@Component({
+  selector: 'dt-favorite-column',
+  templateUrl: 'favorite-column.html',
+  styleUrls: ['./favorite-column.scss'],
+  preserveWhitespaces: false,
+  encapsulation: ViewEncapsulation.Emulated,
+  /*
+   * Deliberatley set to Default because the embedded view that gets created for the
+   * dtColumDef can't handle onPush and results in ChangeAfterChecked error.
+   */
+  changeDetection: ChangeDetectionStrategy.Default,
+  providers: [{ provide: DtSimpleColumnBase, useExisting: DtFavoriteColumn }],
+})
+export class DtFavoriteColumn<T> extends DtSimpleColumnBase<T> {
+  /** Event that is emitted whenever the star icon for the favorite column is toggled. */
+  @Output() readonly favoriteToggled = new EventEmitter<T>();
+
+  constructor(@Optional() table: DtTable<T>) {
+    super(table);
+  }
+
+  /**
+   * @internal is Favorite returns whether the column specified by name in the datasource
+   * holds true or false.
+   */
+  _isFavorite(data: T): boolean {
+    return this.displayAccessor
+      ? this.displayAccessor(data, this.name)
+      : (data as any)[this.name]; // tslint:disable-line:no-any
+  }
+
+  /**
+   * @internal Toggle Favorite toggles the value of the favorite column in the
+   * datasource using the predefined name.
+   */
+  _toggleFavorite(data: T): void {
+    data[this.name] = !data[this.name];
+    this.favoriteToggled.emit(data);
+  }
+}
---
+++ libs/examples/src/table/table-favorite-column-no-header-example/table-favorite-column-no-header-example.ts
@@ -0,0 +1,84 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from '@angular/core';
+
+import { DtTableDataSource } from '@dynatrace/barista-components/table';
+
+@Component({
+  selector: 'dt-example-table-favorite-column-no-header',
+  templateUrl: './table-favorite-column-no-header-example.html',
+})
+export class DtExampleTableFavoriteColumnNoHeader {
+  data: Array<{
+    favorite: boolean;
+    host: string;
+    memoryPerc: number;
+    memoryTotal: number;
+  }> = [
+    {
+      favorite: true,
+      host: 'et-demo-2-win4',
+      memoryPerc: 38,
+      memoryTotal: 5830000000,
+    },
+    {
+      favorite: true,
+      host: 'et-demo-2-win3',
+      memoryPerc: 46,
+      memoryTotal: 6000000000,
+    },
+    {
+      favorite: false,
+      host: 'docker-host2',
+      memoryPerc: 35,
+      memoryTotal: 5810000000,
+    },
+    {
+      favorite: false,
+      host: 'et-demo-2-win1',
+      memoryPerc: 7.86,
+      memoryTotal: 5820000000,
+    },
+  ];
+
+  // Initialize the table's data source
+  dataSource: DtTableDataSource<{
+    favorite: boolean;
+    host: string;
+    memoryPerc: number;
+    memoryTotal: number;
+  }>;
+
+  constructor() {
+    this.dataSource = new DtTableDataSource(this.data);
+    this.dataSource.addSortAccessorFunction(
+      'memory',
+      row =>
+        // Any accessor computation that returns a comparable value.
+        (row.memoryPerc / 100) * row.memoryTotal,
+    );
+  }
+
+  doSomething(event: {
+    favorite: boolean;
+    host: string;
+    memoryPerc: number;
+    memoryTotal: number;
+  }): void {
+    // noop
+  }
+}
