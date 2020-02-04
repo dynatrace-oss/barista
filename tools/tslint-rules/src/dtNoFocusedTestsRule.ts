db884ad534aa5b22c16d56626b9ae9d28211f0b0
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-02-04 07:52:47 +0100

    chore: Update dtNoFocusedTestsRule to check for testcafe fixtures as well.

--- tools/tslint-rules/src/dtNoFocusedTestsRule.ts
+++ tools/tslint-rules/src/dtNoFocusedTestsRule.ts
@@ -1,3 +1,19 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // tslint:disable: deprecation
 import {
   IOptions,
@@ -8,7 +24,7 @@ import {
 } from 'tslint';
 import * as ts from 'typescript';
 
-const MATCHER = ['describe', 'it', 'test'];
+const MATCHER = ['describe', 'it', 'test', 'fixture'];
 const FORBIDDEN_FOCUS = ['only', 'skip'];
 
 class NoFocusedRuleWalker extends RuleWalker {
@@ -30,7 +46,7 @@ class NoFocusedRuleWalker extends RuleWalker {
   }
 }
 
-const MATCH_REGEX = /.+\.(spec|test|e2e)\.ts$/;
+const MATCH_REGEX = /.+\.(spec|test|e2e|a11y)\.ts$/;
 
 /**
  * Implementation of the dt-no-focused-tests rule
