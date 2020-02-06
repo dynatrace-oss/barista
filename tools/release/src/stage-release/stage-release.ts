29c725f8d0d436ec31b94335552b91fb8a3a3bac
Thomas Heller <thomas.heller@dynatrace.com>
2020-02-06 08:21:38 +0100

    chore(release): On stage release, bump package lock version as well.
    When starting a stage release, we can also update the version in the
    package-lock.json file.
    
    Fixes #533

--- tools/release/src/stage-release/stage-release.ts
+++ tools/release/src/stage-release/stage-release.ts
@@ -16,6 +16,7 @@
 
 import {
   PackageJson,
+  PackageLockJson,
   tryJsonParse,
 } from '@dynatrace/barista-components/tools/shared';
 import * as OctokitApi from '@octokit/rest';
@@ -67,6 +68,10 @@ export async function stageRelease(
   const currentVersion = await parsePackageVersion(workspaceRoot);
   const packageJsonPath = join(workspaceRoot, 'package.json');
   const packageJson = await tryJsonParse<PackageJson>(packageJsonPath);
+  const packageLockJsonPath = join(workspaceRoot, 'package-lock.json');
+  const packageLockJson = await tryJsonParse<PackageLockJson>(
+    packageLockJsonPath,
+  );
 
   const newVersion = await promptForNewVersion(currentVersion);
   const newVersionName = newVersion.raw!;
@@ -90,6 +95,12 @@ export async function stageRelease(
     await updatePackageJsonVersion(
       packageJson,
       packageJsonPath,
+      newVersionName,
+    );
+
+    await updatePackageJsonVersion(
+      packageLockJson,
+      packageLockJsonPath,
       newVersionName,
     );
 
@@ -197,7 +208,7 @@ function switchToPublishBranch(git: GitClient, newVersion: SemVer): string {
  * writes the changes to disk.
  */
 async function updatePackageJsonVersion(
-  packageJson: PackageJson,
+  packageJson: PackageJson | PackageLockJson,
   packageJsonPath: string,
   newVersionName: string,
 ): Promise<void> {
