andular/barista-1

barista: Refactor barista to use the angular router.
Refactor barista to leverage the angular router instead of manipulating the location.
This enables us to use the built in lazy loading of the routes and therefore we can reduce the main bundle size by half.
Furthermore we are now lazy loading all the examples in separate chunks to keep the transferred code size as small as possible.
This means when the button page is visited only the button examples are loaded and not all examples.

Fixes: #626

apps/barista-design-system/src/app/app.ts
@@ -1,0 +1,165 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// tslint:disable: indent object-literal-key-quotes quotemark trailing-comma max-line-length no-duplicate-imports max-file-line-count
+
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pluck,
+  share,
+  takeUntil,
+} from 'rxjs/operators';
+import {
+  BaPageService,
+  getPageKeyFromUrl,
+  getUrlPathName,
+} from '../shared/services/page.service';
+import {
+  BaContentTypes,
+  BaPageLayoutType,
+} from '@dynatrace/shared/barista-definitions';
+import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
+
+const PAGE_THEME_MAP = new Map<string, string>([
+  ['brand', 'purple'],
+  ['resources', 'blue'],
+  ['components', 'royalblue'],
+  ['patterns', 'turquoise'],
+]);
+
+export const DEFAULT_PAGE_THEME = 'turquoise';
+
+export interface BaBreadCrumb {
+  label: string;
+  url: string;
+}
+
+@Component({
+  selector: 'ba-app',
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+})
+export class BaApp implements OnInit, OnDestroy {
+  _url$ = this._router.events.pipe(
+    filter((event: Event) => event instanceof NavigationEnd),
+    pluck('url'),
+    distinctUntilChanged(),
+    share(),
+  );
+
+  /** The stream that holds the current breadcrumbs */
+  _breadcrumbs$ = this._url$.pipe(
+    map((url: string) => this._createBreadcrumbs(url)),
+  );
+
+  private _destroy$ = new Subject<void>();
+
+  constructor(
+    private _pageService: BaPageService,
+    private _router: Router,
+    private _meta: Meta,
+    private _title: Title,
+    @Inject(DOCUMENT) private _document: any,
+  ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this._url$
+      .pipe(
+        map((url: string) => {
+          const key = getPageKeyFromUrl(this._document, url);
+          return this._pageService._cache.get(key);
+        }),
+        takeUntil(this._destroy$),
+      )
+      .subscribe(page => {
+        this._setMeta(page);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  /** @internal Gets the page theme based on the current location. */
+  _getPageTheme(): string {
+    const path = this._router.url.substr(1);
+    let pageTheme = DEFAULT_PAGE_THEME;
+    if (path.length) {
+      const firstPart = path.split('/')[0];
+      pageTheme = PAGE_THEME_MAP.get(firstPart) || pageTheme;
+    }
+    return pageTheme;
+  }
+
+  /** @internal Generate Breadcrumbs based on the url */
+  _createBreadcrumbs(url: string): BaBreadCrumb[] {
+    const urlParts = getUrlPathName(this._document, url).split('/');
+
+    return urlParts.reduce((prev: BaBreadCrumb[], cur: string) => {
+      const currentUrl = `${prev.map(part => part.url).join('/')}/${cur}`;
+      const page = this._pageService._cache.get(
+        getPageKeyFromUrl(this._document, currentUrl),
+      );
+      const label = page ? page.title : cur.replace(/\-/gm, '');
+
+      prev.push({
+        label,
+        url: currentUrl,
+      });
+      return prev;
+    }, [] as BaBreadCrumb[]);
+  }
+
+  private _setMeta(page: BaContentTypes): void {
+    const pageTitle =
+      page.layout === BaPageLayoutType.Index
+        ? page.title
+        : `Barista - ${page.title}`;
+
+    const keywords = [
+      'Dynatrace',
+      'Barista',
+      'Design System',
+      'Open Source',
+      ...(page.hasOwnProperty('tags') ? page['tags'] : []),
+    ];
+
+    const metaTags: MetaDefinition[] = [
+      { property: 'og:url', content: this._router.url },
+      { property: 'og:title', content: pageTitle },
+      { property: 'keywords', content: keywords.join(', ') },
+    ];
+
+    if (page.description) {
+      metaTags.push({ name: 'description', content: page.description });
+    }
+
+    metaTags.forEach(tag => {
+      this._meta.updateTag(tag);
+    });
+
+    this._title.setTitle(pageTitle);
+  }
+}

apps/barista-design-system/src/components/live-example/live-example.ts
@@ -14,35 +14,31 @@
  * limitations under the License.
  */
 
-import {
-  Component,
-  Input,
-  ComponentFactoryResolver,
-  Injector,
-  ViewChild,
-  ElementRef,
-  ViewContainerRef,
-  ComponentRef,
-  OnDestroy,
-} from '@angular/core';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Platform } from '@angular/cdk/platform';
-import { EXAMPLES_MAP } from '@dynatrace/examples';
-import { timer, Subscription } from 'rxjs';
+import {
+  Compiler,
+  Component,
+  Input,
+  NgModule,
+  OnDestroy,
+  OnInit,
+  Type,
+} from '@angular/core';
 import {
   Highlighter,
-  registerLanguages,
   htmlRender,
   init,
   process,
+  registerLanguages,
+  SCSS,
   TypeScript,
   XML,
-  SCSS,
 } from 'highlight-ts';
-
-import { createComponent } from '../../utils/create-component';
+import { from, Observable, Subscription, timer } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { BaCopyToClipboardService } from '../../shared/services/copy-to-clipboard.service';
 import { wrapCodeLines } from '../../utils/wrap-code-lines';
-import { BaCopyToClipboardService } from '../../shared/copy-to-clipboard.service';
 
 type BaSourceType = 'html' | 'ts' | 'scss';
 
@@ -56,17 +52,17 @@ type BaSourceType = 'html' | 'ts' | 'scss';
     '[class.ba-live-example-full-width]': 'fullwidth',
   },
 })
-export class BaLiveExample implements OnDestroy {
+export class BaLiveExample implements OnInit, OnDestroy {
   /** The name of the example (class name) that will be instantiated. */
-  @Input()
-  get name(): string {
-    return this._name;
-  }
-  set name(value: string) {
-    this._name = value;
-    this._initExample();
-  }
-  private _name: string;
+  @Input() name: string;
+
+  /**
+   * The directory inside the libs/examples/src/* where
+   * the examples are located.
+   * Is needed to know from which directory the example should be loaded
+   * via the es6 import statement in the `_initExample` method
+   */
+  @Input() directory: string;
 
   /** Whether the example should run in the dark theme. */
   @Input()
@@ -142,14 +138,9 @@ export class BaLiveExample implements OnDestroy {
 
   /**
    * @internal
-   * The placeholder element that will be replaced with
-   * the example element once it is instantiated.
+   * The stream that holds the current component type.
    */
-  @ViewChild('demoPlaceholder', { read: ElementRef, static: true })
-  _placeholder: ElementRef;
-
-  /** @internal The component-ref of the instantiated class. */
-  _componentRef: ComponentRef<unknown>;
+  example$: Observable<any>;
 
   /**
    * @internal
@@ -181,9 +172,7 @@ export class BaLiveExample implements OnDestroy {
   private _highlighter: Highlighter<string>;
 
   constructor(
-    private _componentFactoryResolver: ComponentFactoryResolver,
-    private _injector: Injector,
-    private _viewContainerRef: ViewContainerRef,
+    private _compiler: Compiler,
     private _platform: Platform,
     private _ctcService: BaCopyToClipboardService,
   ) {
@@ -191,10 +180,11 @@ export class BaLiveExample implements OnDestroy {
     this._highlighter = init(htmlRender);
   }
 
+  ngOnInit(): void {
+    this._initExample();
+  }
+
   ngOnDestroy(): void {
-    if (this._componentRef) {
-      this._componentRef.destroy();
-    }
     this._timerSubscription.unsubscribe();
   }
 
@@ -237,19 +227,20 @@ export class BaLiveExample implements OnDestroy {
   }
 
   private _initExample(): void {
-    const exampleType = EXAMPLES_MAP.get(this._name);
-    if (exampleType) {
-      const factory = this._componentFactoryResolver.resolveComponentFactory(
-        exampleType,
-      );
-      this._componentRef = createComponent(
-        factory,
-        this._viewContainerRef,
-        this._injector,
-        this._placeholder.nativeElement,
-        true,
-      );
-    }
+    this.example$ = from(
+      import(`../../../../../libs/examples/src/${this.directory}/index.ts`),
+    ).pipe(
+      map(es6Module => getNgModuleFromEs6Module(es6Module)),
+      filter(Boolean),
+      switchMap((moduleType: Type<NgModule>) =>
+        this._compiler.compileModuleAndAllComponentsAsync(moduleType),
+      ),
+      map(({ componentFactories }) => {
+        return componentFactories.find(
+          factory => factory.componentType.name === this.name,
+        )?.componentType;
+      }),
+    );
   }
 
   /**
@@ -282,3 +273,20 @@ export class BaLiveExample implements OnDestroy {
     return wrapCodeLines(transformedCode, 'ba-live-example-code-line');
   }
 }
+
+/** Retrieves the NgModule of an es6 module */
+function getNgModuleFromEs6Module(es6Module: any): Type<NgModule> | null {
+  for (const key of Object.keys(es6Module)) {
+    if (isNgModule(es6Module[key])) {
+      return es6Module[key];
+    }
+  }
+  return null;
+}
+
+/** Checks if a provided type is an Angular Module */
+const isNgModule = (moduleType: any): boolean =>
+  !!(
+    Array.isArray(moduleType?.decorators) &&
+    moduleType.decorators[0]?.type?.prototype?.ngMetadataName === 'NgModule'
+  );

dynatrace-oss/barista

apps/barista-design-system/src/app/app.ts
@@ -1,0 +1,164 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import {
+  BaContentTypes,
+  BaPageLayoutType,
+} from '@dynatrace/shared/barista-definitions';
+import { Subject } from 'rxjs';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pluck,
+  share,
+  takeUntil,
+} from 'rxjs/operators';
+import {
+  BaPageService,
+  getPageKeyFromUrl,
+  getUrlPathName,
+} from '../shared/services/page.service';
+
+const PAGE_THEME_MAP = new Map<string, string>([
+  ['brand', 'purple'],
+  ['resources', 'blue'],
+  ['components', 'royalblue'],
+  ['patterns', 'turquoise'],
+]);
+
+export const DEFAULT_PAGE_THEME = 'turquoise';
+
+export interface BaBreadcrumb {
+  label: string;
+  url: string;
+}
+
+@Component({
+  selector: 'ba-app',
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+})
+export class BaApp implements OnInit, OnDestroy {
+  /** @internal The stream that holds the current url after a navigation was triggered */
+  _url$ = this._router.events.pipe(
+    filter((event: Event) => event instanceof NavigationEnd),
+    pluck('url'),
+    distinctUntilChanged(),
+    share(),
+  );
+
+  /** @internal The stream that holds the current breadcrumbs */
+  _breadcrumbs$ = this._url$.pipe(
+    map((url: string) => this._createBreadcrumbs(url)),
+  );
+
+  /** Subject used for bulk unsubscribing */
+  private _destroy$ = new Subject<void>();
+
+  constructor(
+    private _pageService: BaPageService,
+    private _router: Router,
+    private _meta: Meta,
+    private _title: Title,
+    @Inject(DOCUMENT) private _document: any,
+  ) {}
+
+  ngOnInit(): void {
+    this._url$
+      .pipe(
+        map((url: string) => {
+          const key = getPageKeyFromUrl(this._document, url);
+          return this._pageService._cache.get(key);
+        }),
+        takeUntil(this._destroy$),
+      )
+      .subscribe(page => {
+        this._setMeta(page);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  /** @internal Gets the page theme based on the current location. */
+  _getPageTheme(): string {
+    const path = this._router.url.substr(1);
+    let pageTheme = DEFAULT_PAGE_THEME;
+    if (path.length) {
+      const firstPart = path.split('/')[0];
+      pageTheme = PAGE_THEME_MAP.get(firstPart) || pageTheme;
+    }
+    return pageTheme;
+  }
+
+  /** @internal Generate Breadcrumbs based on the url */
+  _createBreadcrumbs(url: string): BaBreadcrumb[] {
+    const urlParts = getUrlPathName(this._document, url).split('/');
+
+    return urlParts.reduce((prev: BaBreadcrumb[], cur: string) => {
+      const currentUrl = `${prev.map(part => part.url).join('/')}/${cur}`;
+      const page = this._pageService._cache.get(
+        getPageKeyFromUrl(this._document, currentUrl),
+      );
+      const label = page ? page.title : cur.replace(/\-/gm, '');
+
+      prev.push({
+        label,
+        url: currentUrl,
+      });
+      return prev;
+    }, [] as BaBreadcrumb[]);
+  }
+
+  /** Sets all the necessary meta tags to improve our SEO score. */
+  private _setMeta(page: BaContentTypes): void {
+    const pageTitle =
+      page.layout === BaPageLayoutType.Index
+        ? page.title
+        : `Barista - ${page.title}`;
+
+    const keywords = [
+      'Dynatrace',
+      'Barista',
+      'Design System',
+      'Open Source',
+      ...(page.hasOwnProperty('tags') ? page['tags'] : []),
+    ];
+
+    const metaTags: MetaDefinition[] = [
+      { property: 'og:url', content: this._router.url },
+      { property: 'og:title', content: pageTitle },
+      { property: 'keywords', content: keywords.join(', ') },
+    ];
+
+    if (page.description) {
+      metaTags.push({ name: 'description', content: page.description });
+    }
+
+    metaTags.forEach(tag => {
+      this._meta.updateTag(tag);
+    });
+
+    this._title.setTitle(pageTitle);
+  }
+}

apps/barista-design-system/src/components/live-example/live-example.ts
@@ -14,35 +14,31 @@
  * limitations under the License.
  */
 
-import {
-  Component,
-  Input,
-  ComponentFactoryResolver,
-  Injector,
-  ViewChild,
-  ElementRef,
-  ViewContainerRef,
-  ComponentRef,
-  OnDestroy,
-} from '@angular/core';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Platform } from '@angular/cdk/platform';
-import { EXAMPLES_MAP } from '@dynatrace/examples';
-import { timer, Subscription } from 'rxjs';
+import {
+  Compiler,
+  Component,
+  Input,
+  NgModule,
+  OnDestroy,
+  OnInit,
+  Type,
+} from '@angular/core';
 import {
   Highlighter,
-  registerLanguages,
   htmlRender,
   init,
   process,
+  registerLanguages,
+  SCSS,
   TypeScript,
   XML,
-  SCSS,
 } from 'highlight-ts';
-
-import { createComponent } from '../../utils/create-component';
+import { from, Observable, Subscription, timer } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { BaCopyToClipboardService } from '../../shared/services/copy-to-clipboard.service';
 import { wrapCodeLines } from '../../utils/wrap-code-lines';
-import { BaCopyToClipboardService } from '../../shared/copy-to-clipboard.service';
 
 type BaSourceType = 'html' | 'ts' | 'scss';
 
@@ -56,17 +52,17 @@ type BaSourceType = 'html' | 'ts' | 'scss';
     '[class.ba-live-example-full-width]': 'fullwidth',
   },
 })
-export class BaLiveExample implements OnDestroy {
+export class BaLiveExample implements OnInit, OnDestroy {
   /** The name of the example (class name) that will be instantiated. */
-  @Input()
-  get name(): string {
-    return this._name;
-  }
-  set name(value: string) {
-    this._name = value;
-    this._initExample();
-  }
-  private _name: string;
+  @Input() name: string;
+
+  /**
+   * The directory inside the libs/examples/src/* where
+   * the examples are located.
+   * Is needed to know from which directory the example should be loaded
+   * via the es6 import statement in the `_initExample` method
+   */
+  @Input() directory: string;
 
   /** Whether the example should run in the dark theme. */
   @Input()
@@ -142,14 +138,9 @@ export class BaLiveExample implements OnDestroy {
 
   /**
    * @internal
-   * The placeholder element that will be replaced with
-   * the example element once it is instantiated.
+   * The stream that holds the current component type.
    */
-  @ViewChild('demoPlaceholder', { read: ElementRef, static: true })
-  _placeholder: ElementRef;
-
-  /** @internal The component-ref of the instantiated class. */
-  _componentRef: ComponentRef<unknown>;
+  _example$: Observable<any>;
 
   /**
    * @internal
@@ -181,9 +172,7 @@ export class BaLiveExample implements OnDestroy {
   private _highlighter: Highlighter<string>;
 
   constructor(
-    private _componentFactoryResolver: ComponentFactoryResolver,
-    private _injector: Injector,
-    private _viewContainerRef: ViewContainerRef,
+    private _compiler: Compiler,
     private _platform: Platform,
     private _ctcService: BaCopyToClipboardService,
   ) {
@@ -191,10 +180,11 @@ export class BaLiveExample implements OnDestroy {
     this._highlighter = init(htmlRender);
   }
 
+  ngOnInit(): void {
+    this._initExample();
+  }
+
   ngOnDestroy(): void {
-    if (this._componentRef) {
-      this._componentRef.destroy();
-    }
     this._timerSubscription.unsubscribe();
   }
 
@@ -237,19 +227,20 @@ export class BaLiveExample implements OnDestroy {
   }
 
   private _initExample(): void {
-    const exampleType = EXAMPLES_MAP.get(this._name);
-    if (exampleType) {
-      const factory = this._componentFactoryResolver.resolveComponentFactory(
-        exampleType,
-      );
-      this._componentRef = createComponent(
-        factory,
-        this._viewContainerRef,
-        this._injector,
-        this._placeholder.nativeElement,
-        true,
-      );
-    }
+    this._example$ = from(
+      import(`../../../../../libs/examples/src/${this.directory}/index.ts`),
+    ).pipe(
+      map(es6Module => getNgModuleFromEs6Module(es6Module)),
+      filter(Boolean),
+      switchMap((moduleType: Type<NgModule>) =>
+        this._compiler.compileModuleAndAllComponentsAsync(moduleType),
+      ),
+      map(({ componentFactories }) => {
+        return componentFactories.find(
+          factory => factory.componentType.name === this.name,
+        )?.componentType;
+      }),
+    );
   }
 
   /**
@@ -282,3 +273,20 @@ export class BaLiveExample implements OnDestroy {
     return wrapCodeLines(transformedCode, 'ba-live-example-code-line');
   }
 }
+
+/** Retrieves the NgModule of an es6 module */
+function getNgModuleFromEs6Module(es6Module: any): Type<NgModule> | null {
+  for (const key of Object.keys(es6Module)) {
+    if (isNgModule(es6Module[key])) {
+      return es6Module[key];
+    }
+  }
+  return null;
+}
+
+/** Checks if a provided type is an Angular Module */
+const isNgModule = (moduleType: any): boolean =>
+  !!(
+    Array.isArray(moduleType?.decorators) &&
+    moduleType.decorators[0]?.type?.prototype?.ngMetadataName === 'NgModule'
+  );