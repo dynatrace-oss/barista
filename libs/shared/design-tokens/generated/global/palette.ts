bded8f3089b52f7b92cb67a540c39502e8935f8e
Thomas Heller <thomas.heller@dynatrace.com>
2020-03-19 08:43:19 +0100

    chore: Added design tokens to the library

---
+++ libs/shared/design-tokens/generated/global/palette.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * THIS FILE IS GENERATED BASED ON THE DESIGN TOKENS DEFINED IN THE src/**.yml
+ * FILES, DO NOT CHANGE MANUALLY.
+ * TO GENERATE THESE FILES RUN `ng build shared-design-tokens`
+ */
+
+export const FLUID_COLOR_KEY_LIGHTER = '#8390dd';
+export const FLUID_COLOR_KEY_LIGHT = '#6c7ae4';
+export const FLUID_COLOR_KEY_BASE = '#5b63f1';
+export const FLUID_COLOR_KEY_DARK = '#504df3';
+export const FLUID_COLOR_KEY_DARKER = '#4247b5';
+export const FLUID_COLOR_NEUTRAL_LIGHTER = '#8d95a7';
+export const FLUID_COLOR_NEUTRAL_LIGHT = '#7c8391';
+export const FLUID_COLOR_NEUTRAL_BASE = '#6f7581';
+export const FLUID_COLOR_NEUTRAL_DARK = '#626771';
+export const FLUID_COLOR_NEUTRAL_DARKER = '#51555c';
+export const FLUID_COLOR_NEGATIVE_LIGHTER = '#e5736a';
+export const FLUID_COLOR_NEGATIVE_LIGHT = '#e0534c';
+export const FLUID_COLOR_NEGATIVE_BASE = '#d23f3b';
+export const FLUID_COLOR_NEGATIVE_DARK = '#b63d38';
+export const FLUID_COLOR_NEGATIVE_DARKER = '#903832';
+export const FLUID_COLOR_WARNING_LIGHTER = '#ab942e';
+export const FLUID_COLOR_WARNING_LIGHT = '#95822f';
+export const FLUID_COLOR_WARNING_BASE = '#83742f';
+export const FLUID_COLOR_WARNING_DARK = '#74662d';
+export const FLUID_COLOR_WARNING_DARKER = '#5f5529';
+export const FLUID_COLOR_POSITIVE_LIGHTER = '#48a678';
+export const FLUID_COLOR_POSITIVE_LIGHT = '#46916b';
+export const FLUID_COLOR_POSITIVE_BASE = '#438060';
+export const FLUID_COLOR_POSITIVE_DARK = '#3f7156';
+export const FLUID_COLOR_POSITIVE_DARKER = '#385c48';
