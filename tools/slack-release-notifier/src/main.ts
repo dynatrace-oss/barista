5d8015e97038556c317e7f6c0a4706fabd06b02c
Rene Buchmayer <rene.buchmayer@dynatrace.com>
2020-03-24 09:40:16 +0100

    ci(slack-release-notifier): Send automatic Slack notifications on release.

---
+++ tools/slack-release-notifier/src/main.ts
@@ -0,0 +1,117 @@
+/**
+ * @license
+ * Copyright 2019 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { getInput } from '@actions/core';
+import { context, GitHub } from '@actions/github';
+import { IncomingWebhook } from '@slack/webhook';
+import { ReposGetReleaseByTagResponse } from '@octokit/rest';
+
+import { getReleaseType, getTextForReleaseType } from './utils/release-type';
+import { convertToMrkdwn } from './utils/convert-to-mrkdwn';
+import { fancifyText } from './utils/fancify-text';
+
+function getReleaseTitle(release: ReposGetReleaseByTagResponse) {
+  if (!release.name) {
+    return release.tag_name;
+  }
+
+  return release.name.includes(release.tag_name)
+    ? release.name
+    : `${release.name} (${release.tag_name})`;
+}
+
+async function run(): Promise<void> {
+  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+  const githubToken = getInput('repo-token', { required: true });
+
+  if (!slackWebhookUrl) {
+    throw new Error(
+      'An environment variable "SLACK_WEBHOOK_URL" should contain the webhook URL.',
+    );
+  }
+
+  if (!context.ref.includes('/')) {
+    throw new Error('Malformed tag ref');
+  }
+
+  // The ref in the context is formatted like 'ref/tags/1.0.0'.
+  // The API expects only the tag string e.g. 1.0.0
+  const splitTag = context.ref.split('/');
+  const tag = splitTag[splitTag.length - 1];
+
+  const client = new GitHub(githubToken);
+
+  const release = (
+    await client.repos.getReleaseByTag({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag,
+    })
+  ).data;
+
+  const webhook = new IncomingWebhook(slackWebhookUrl);
+  await webhook.send({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `:fire: New ${context.repo.owner}/${context.repo.repo} release! :rocket:`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            getTextForReleaseType(getReleaseType(release)) +
+            `\n:package: ${getReleaseTitle(release)}`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'View on GitHub :github:',
+            emoji: true,
+          },
+          url: release.html_url,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          // Print text if body is empty since the Slack API doesn't like empty strings here
+          text:
+            convertToMrkdwn(fancifyText(release.body)) ||
+            'Check out GitHub for more info.',
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'Thanks to all contributors for helping us out!',
+        },
+      },
+    ],
+  });
+}
+
+run().catch(error => {
+  console.log(error);
+  process.exit(1);
+});
