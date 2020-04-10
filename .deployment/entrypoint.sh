9921b7519c71368056ef3b59d87ef952c5b536a8
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-04-10 08:51:30 +0200

    ci: Use string compare instead of the built in sha1sum compare.
    The sha1sum built in compare function was behaving odd on the github action so we replaced it with a good old string compare.

--- .deployment/entrypoint.sh
+++ .deployment/entrypoint.sh
@@ -3,18 +3,24 @@
 echo "
 ---------------------------------------------------------
 Welcome to the Barista Workspace! 🔥
-
 You are currently working in:
 $(pwd)
 ---------------------------------------------------------
 
 "
 
-if [ ! "$(sha1sum -c package-lock.sha1)" ]; then
+oldSha=$(cat ./package-lock.sha1)
+currentSha="$(sha1sum ./package-lock.json)"
+
+if [ "$oldSha" != "$currentSha" ]; then
   echo "⚠️ Need to install packages due to updated package-lock.json"
   # When the checksums are not matching perform an npm install
   npm ci --ignore-scripts
+  echo "Successfully updated all packages!"
+else
+  echo "All packages are up to date! 🐙"
 fi
+
 
 # Run the command from the docker image
 exec "$@"
