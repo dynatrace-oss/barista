b0f64065c4f8d50272ffe6f97a517689b2c68591
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-05-18 11:15:43 +0200

    barista(builder): Fixes an issue with the index file generation.
    Fixes #905
    
    Fixes a problem where the index file was not generated propperly and therefore the server crashed on the healthcheck.
    
    Added further tests to test the file renaming and rendering.

--- .deployment/barista/barista-internal.Dockerfile
+++ .deployment/barista/barista-internal.Dockerfile
@@ -1,10 +1,12 @@
-
 FROM nginx:alpine
 
 COPY .deployment/barista/nginx/nginx.conf /etc/nginx/nginx.conf
 COPY .deployment/barista/nginx/sites-available /etc/nginx/sites-available
 
-RUN ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled
+RUN ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled && \
+    # remove the default nginx config (hello I'm nginx) because it is
+    # running on localhost:80 as well and it would conflict with barista
+    rm -rf /etc/nginx/conf.d/default.conf
 
 COPY dist/apps/barista-design-system/browser /var/www/barista
 
--- libs/workspace/src/builders/barista-build/renderer.ts
+++ libs/workspace/src/builders/barista-build/renderer.ts
@@ -15,18 +15,35 @@
  */
 
 import axios from 'axios';
-import { mkdirSync, promises as fs } from 'fs';
+import { mkdirSync, promises as fs, readFile } from 'fs';
 import { dirname, join } from 'path';
 
 /** generates the html filename out of a route */
-function generateFileName(route: string): string {
+export function generateFileName(route: string): string {
+  // If the route is the index route we don't need to wrap it in a folder
+  // https://regex101.com/r/lp5RRn/1
+  if (route.match(/^\/?(index)?$/gm)) {
+    return 'index.html';
+  }
+
   return join(route, 'index.html');
 }
 
-/** Renders each route that is provided via the process args */
-(async () => {
-  const [outputPath, baseUrl, ...routes] = process.argv.slice(2);
-
+/**
+ * Function that renders the provided routes and send a signal to the process that is running
+ * when it succeeded or failed.
+ *
+ * This function will be executed via the self executed function in the bottom of this file
+ * and this file will be spawned via a fork!
+ * @param outputPath The path where the rendered files should be written to
+ * @param baseUrl The base url where the routes should be appended
+ * @param routes The list of routes that should be rendered
+ */
+export async function render(
+  outputPath: string,
+  baseUrl: string,
+  routes: string[],
+): Promise<void> {
   for (const route of routes) {
     const fileName = generateFileName(route);
     const filePath = join(outputPath, fileName);
@@ -56,4 +73,10 @@ function generateFileName(route: string): string {
       }
     }
   }
+}
+
+/** Renders each route that is provided via the process args */
+(async () => {
+  const [outputPath, baseUrl, ...routes] = process.argv.slice(2);
+  render(outputPath, baseUrl, routes);
 })().catch();
