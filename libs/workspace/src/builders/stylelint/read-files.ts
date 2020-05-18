42a26a3bd05db2e2f8219895fe03f0f6411c759f
Mihaly Bardos <bardosmisi@gmail.com>
2020-05-18 16:03:06 +0200

    build(workspace): Fix affected linting on windows.
    Co-authored-by: Thomas Heller <thomas.heller@dynatrace.com>

--- libs/workspace/src/builders/stylelint/read-files.ts
+++ libs/workspace/src/builders/stylelint/read-files.ts
@@ -24,8 +24,15 @@ export function readFiles(
 ): string[] {
   const { files, exclude: ignore } = options;
 
-  return files
-    .map((file) => sync(file, { cwd: root, ignore, nodir: true }))
-    .reduce((prev, curr) => prev.concat(curr), [])
-    .map((file) => join(root, file));
+  return (
+    files
+      .map((file) => sync(file, { cwd: root, ignore, nodir: true }))
+      .reduce((prev, curr) => prev.concat(curr), [])
+      .map((file) => join(root, file))
+      // Stylelint passes the pattern internally into globby
+      // https://github.com/stylelint/stylelint/blob/81211b108153e650bfffdd04d8fd02aedb5d5501/lib/standalone.js#L197
+      // globby uses forward slashes to construct their patterns, independently to path separators of operating systems
+      // https://github.com/sindresorhus/globby/blob/master/readme.md#api
+      .map((file) => file.replace(/\\/g, '/'))
+  );
 }
