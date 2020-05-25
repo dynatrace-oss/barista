15a8c02667cc4061d3cc113940a93596002fcf6b
Lukas Furtner <lukas.furtner@dynatrace.com>
2020-05-25 10:53:12 +0200

    chore(workspace): Add CI step to build a target with Bazel

--- tools/build-cache/setup_ci_build_cache.sh
+++ tools/build-cache/setup_ci_build_cache.sh
@@ -3,6 +3,6 @@
 set +e
 
 openssl aes-256-cbc -pbkdf2 -salt -a -d\
-  -out cache-write-key.json \
-  -in cache-write-key.json.enc \
+  -out $(pwd)/cache-write-key.json \
+  -in $(pwd)/cache-write-key.json.enc \
   -pass pass:${BAZEL_REMOTE_CACHE_PASSWORD}
