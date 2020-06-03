e97879be2586a861931424fc2fc152a781b95fd9
Thomas Heller <thomas.heller@dynatrace.com>
2020-06-03 08:52:37 +0000

    build(e2e): Create a custom builder that only passes affected sources into the e2e test environment.
    Using the nx affected projects methods to figure out, which componenten modules are affected
    and looking for these component modules within the imports of the e2e-testapp-modules
    to determine which e2e tests need to be run.

--- libs/workspace/webpack.config.js
+++ libs/workspace/webpack.config.js
@@ -15,6 +15,7 @@ const entries = [
   'src/builders/design-tokens/dependency-tree/index.ts',
   'src/builders/design-tokens/tailwindcss/index.ts',
   'src/builders/elements/index.ts',
+  'src/builders/affected-e2e/index.ts',
   'src/schematics/dt-component-e2e/index.ts',
   'src/schematics/dt-component-dev/index.ts',
   'src/index.ts',
