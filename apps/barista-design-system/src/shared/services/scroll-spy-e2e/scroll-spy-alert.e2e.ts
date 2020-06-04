b7e2c1e5332e1ebdd336b0e23e60caa3f754cb13
rowa-audil <rowa.audil@dynatrace.com>
2020-06-04 16:08:03 +0200

    feat(scroll-spy): E2e

---
+++ apps/barista-design-system/src/shared/services/scroll-spy-e2e/scroll-spy-alert.e2e.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Selector } from 'testcafe';
+
+fixture('Scroll Spy - ALERT').page('http://localhost:4200/components/alert');
+
+test('should highlight correct headline after scrolling', async (testController: TestController) => {
+  await testController
+    .wait(200)
+    .click('a[id="do-s-and-don-ts"')
+    .wait(200)
+    .expect(
+      Selector('a.ba-toc-item.ba-toc-item-active').getStyleProperty(
+        'background-color',
+      ),
+    )
+    .eql('rgb(82, 108, 255)');
+});
