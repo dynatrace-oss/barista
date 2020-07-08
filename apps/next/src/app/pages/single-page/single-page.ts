26a5ae27f77cc82d2dfa32a28bd516831b44666f
rowa-audil <rowa.audil@dynatrace.com>
2020-07-08 07:13:21 +0000

    ds(next): Adds the overview page from the ux mock for demonstration purposes.

--- apps/next/src/app/pages/single-page/single-page.ts
+++ apps/next/src/app/pages/single-page/single-page.ts
@@ -17,6 +17,7 @@
 import { Component } from '@angular/core';
 import { BaSinglePageContent } from '@dynatrace/shared/design-system/interfaces';
 import { DsPageService } from '@dynatrace/shared/data-access-strapi';
+import '@dynatrace/fluid-elements/button';
 
 @Component({
   selector: 'ds-single-page',
