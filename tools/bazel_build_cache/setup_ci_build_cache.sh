44dbbfed8d5ea4557a21e2b2bdacf5a425f30182
Lukas Furtner <lukas.furtner@dynatrace.com>
2020-07-09 12:52:10 +0200

    chore(ci): Parameterize Bazel version

--- tools/bazel_build_cache/setup_ci_build_cache.sh
+++ tools/bazel_build_cache/setup_ci_build_cache.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Forked pull requests have CIRCLE_BRANCH set to pull/XXX
-if [[ "${CIRCLE_BRANCH}" =~ pull\/[0-9]+ ]]; then
+if [ "${CIRCLE_BRANCH}" =~ pull\/[0-9]+ ]; then
   echo "Forked pull request no setup for upload to remote chache!"
   echo "build --remote_upload_local_results=false" >> ../../.circleci/bazel.rc
   exit 0
 fi
 
-if [[ -z "${BAZEL_REMOTE_CACHE_PASSWORD}" ]]; then
+if [ -z "${BAZEL_REMOTE_CACHE_PASSWORD}" ]; then
   echo "Please specify the \"BAZEL_REMOTE_CACHE_PASSWORD\" environment variable when setting up remote " \
       "cache"
   exit 1
