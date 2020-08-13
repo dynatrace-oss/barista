c3d8c99db07c0603e73eef1a54effcd0b7e3bea1
rowa-audil <rowa.audil@dynatrace.com>
2020-08-13 09:49:34 +0200

    chore: Adds arrow-down and -up support for the getNextGroupItemIndex function

--- libs/fluid-elements/core/src/lib/keyboard/get-next-group-item-index.ts
+++ libs/fluid-elements/core/src/lib/keyboard/get-next-group-item-index.ts
@@ -14,7 +14,12 @@
  * limitations under the License.
  */
 
-import { ARROW_LEFT, ARROW_RIGHT } from '@dynatrace/shared/keycodes';
+import {
+  ARROW_LEFT,
+  ARROW_RIGHT,
+  ARROW_DOWN,
+  ARROW_UP,
+} from '@dynatrace/shared/keycodes';
 
 /** Calculate based on a length and the direction (arrow key) what the next index is supporting rotationing */
 export function getNextGroupItemIndex(
@@ -22,11 +27,17 @@ export function getNextGroupItemIndex(
   length: number,
   key: string,
 ): number {
-  if (key === ARROW_RIGHT) {
-    index += 1;
-  }
-  if (key === ARROW_LEFT) {
-    index -= 1;
+  switch (key) {
+    case ARROW_RIGHT:
+    case ARROW_DOWN:
+      index += 1;
+      break;
+    case ARROW_LEFT:
+    case ARROW_UP:
+      index -= 1;
+      break;
+    default:
+      break;
   }
   if (index > length - 1) {
     index = 0;
