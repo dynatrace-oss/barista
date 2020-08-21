324391914e23e9cc0f571ce3691e1b4098f56e99
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-08-21 12:32:53 +0000

    build(fluid-button): Add jest testing for the fluid button
    Add jest setup for web components. For that we are transpiling lit-html
    and lit-element to the umd module format.

--- libs/fluid-elements/button/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'fluid-button',
-  preset: '../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/fluid-elements/button',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
-};
