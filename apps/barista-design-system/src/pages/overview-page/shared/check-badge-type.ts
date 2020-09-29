c9fda252a1e73cf683e37965a775bdc0fe8d0c0a
rowa-audil <rowa.audil@dynatrace.com>
2020-09-29 09:36:25 +0200

    chore: Pr Fixes. Extracts badgetype check function

---
+++ apps/barista-design-system/src/pages/overview-page/shared/check-badge-type.ts
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export function checkBadgeType(
+  badge: string[],
+): { icon: string; style: string } | undefined {
+  if (badge && badge.includes('favorite')) {
+    return {
+      icon: '/assets/favorite-white.svg',
+      style: 'ba-tile-badge-favorite',
+    };
+  }
+  if (badge && badge.includes('deprecated')) {
+    return {
+      icon: '/assets/incident-white.svg',
+      style: 'ba-tile-badge-warning',
+    };
+  }
+  if (badge && badge.includes('experimental')) {
+    return {
+      icon: '/assets/laboratory-white.svg',
+      style: 'ba-tile-badge-warning',
+    };
+  }
+  if (badge && badge.includes('work in progress')) {
+    return {
+      icon: '/assets/maintenance-royalblue.svg',
+      style: 'ba-tile-badge-workinprogress',
+    };
+  }
+}
