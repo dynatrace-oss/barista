ea061ad5641fa7bbe450f6bb716cfd2813c94458
Andreas Doppelhofer <andreas.doppelhofer@dynatrace.com>
2020-10-16 08:33:00 +0200

    ci: Removed leftover

--- .buildkite/build_cache/setup_ci_build_cache.sh
+++ .buildkite/build_cache/setup_ci_build_cache.sh
@@ -8,15 +8,6 @@ DIR=$(dirname "$SCRIPT")
 
 bazel_rc="${DIR}/../bazel.rc"
 
-PULL_BRANCH_REGEX="^pull\/[0-9]+$"
-
-# Forked pull requests have CIRCLE_BRANCH set to pull/XXX
-if [[ "${BUILDKITE_BRANCH}" =~ $PULL_BRANCH_REGEX ]]; then
-  echo "Forked pull request no setup for upload to remote chache!"
-  echo "build --remote_upload_local_results=false" >> ${bazel_rc}
-  exit 0
-fi
-
 if [ -z "${BUILDKITE}" ]; then
   # no CI call
   exit 0
