2be0996da0a4d334fe8a8cd791fb5444aa052dd3
Thomas Heller <thomas.heller@dynatrace.com>
2020-12-01 16:47:37 +0100

    chore: Remove testing custom-element lib.

--- libs/testing/custom-element/README.md
+++
@@ -1,8 +0,0 @@
-# testing-custom-element
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test testing-custom-element` to execute the unit tests via
-[Jest](https://jestjs.io).
--- libs/testing/custom-element/src/__mocks__/styleMock.js
+++
@@ -1,5 +0,0 @@
-// Mock css module that is injected by the libs/testing/custom-element/jest.config.js
-// as a default dummy style, so the jest ts transformer can compile correctly.
-// Styles are probably not needed in the unit-tests, which makes this solution
-// applicable for now.
-module.export = {};
