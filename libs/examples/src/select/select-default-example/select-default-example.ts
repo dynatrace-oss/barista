d3f17240044f33794e6bfa5ce95264e3cec778cd
ffriedl89 <ffriedl89@gmail.com>
2021-04-14 09:35:24 +0200

    fix(select): Remove scrollIntoView on hover, which caused endless scrolling in edge cases with zoom enabled.

--- libs/examples/src/select/select-default-example/select-default-example.ts
+++ libs/examples/src/select/select-default-example/select-default-example.ts
@@ -20,4 +20,8 @@ import { Component } from '@angular/core';
   selector: 'dt-example-select-default',
   templateUrl: './select-default-example.html',
 })
-export class DtExampleSelectDefault {}
+export class DtExampleSelectDefault {
+  entries = Array.from(new Array(2300))
+    .map(() => Math.random().toString(36).substring(7))
+    .sort();
+}
