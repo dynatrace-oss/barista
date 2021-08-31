aa66961facfd026cbb794877d2fda8cdc9d83be2
Tom Heller <thomas.heller@dynatrace.com>
2021-08-31 13:41:00 +0200

    chore(workspace): Run nx migration script.

--- apps/components-e2e/jest.config.js
+++ apps/components-e2e/jest.config.js
@@ -2,20 +2,18 @@ module.exports = {
   name: 'components-e2e',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/apps/components-e2e',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
     },
   },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
 };
