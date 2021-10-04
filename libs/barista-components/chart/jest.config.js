aa7f70ee88c612286d467f6f4b0cb785ecaf1d09
Tom Heller <thomas.heller@dynatrace.com>
2021-10-04 12:49:30 +0200

    chore(workspace): Move lint, test and build steps back to NX based setup.

---
+++ libs/barista-components/chart/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  displayName: 'chart',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/barista-components/chart',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  moduleNameMapper: {
+    // map lodash-es to lodash bundle since jest needs commonjs
+    '^lodash-es$': 'node_modules/lodash/index.js',
+  },
+};
---
+++ libs/barista-components/toggle-button-group/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  displayName: 'toggle-button-group',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory:
+    '../../../coverage/libs/barista-components/toggle-button-group',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
