d95b6c488b23e390f812697d41584bd068363f2d
Tom Heller <thomas.heller@dynatrace.com>
2021-10-07 14:33:53 +0200

    feat(components): Update d3-shape and d3-scale dependencies.
    Breaking-change: d3-colore and d3-shape are peerDependencies, which
    makes their version upgrade a breaking change. Barista-components now
    requires d3-scale: ^4.0.0 and d3-shape: ^3.0.1

--- libs/barista-components/radial-chart/jest.config.js
+++ libs/barista-components/radial-chart/jest.config.js
@@ -10,6 +10,18 @@ module.exports = {
     },
   },
   coverageDirectory: '../../../coverage/libs/barista-components/radial-chart',
+  // map lodash-es to lodash bundle since jest needs commonjs
+  moduleNameMapper: {
+    '^d3-scale$': 'node_modules/d3-scale/dist/d3-scale.js',
+    '^d3-shape$': 'node_modules/d3-shape/dist/d3-shape.js',
+    '^d3-path$': 'node_modules/d3-path/dist/d3-path.js',
+    '^d3-array$': 'node_modules/d3-array/dist/d3-array.js',
+    '^d3-interpolate$': 'node_modules/d3-interpolate/dist/d3-interpolate.js',
+    '^d3-color$': 'node_modules/d3-color/dist/d3-color.js',
+    '^d3-format$': 'node_modules/d3-format/dist/d3-format.js',
+    '^d3-time$': 'node_modules/d3-time/dist/d3-time.js',
+    '^d3-time-format$': 'node_modules/d3-time-format/dist/d3-time-format.js',
+  },
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
