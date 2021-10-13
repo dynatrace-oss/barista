c781ae03d781d9fd6e5fd0f4763028b3fc8be79b
Tom Heller <thomas.heller@dynatrace.com>
2021-10-13 08:13:10 +0200

    chore(workspace): Update license header date

--- libs/examples/src/combobox/combobox-form-control-example/combobox-form-control-example.ts
+++ libs/examples/src/combobox/combobox-form-control-example/combobox-form-control-example.ts
@@ -1,7 +1,7 @@
 import { FormControl, Validators } from '@angular/forms';
 /**
  * @license
- * Copyright 2020 Dynatrace LLC
+ * Copyright 2021 Dynatrace LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
