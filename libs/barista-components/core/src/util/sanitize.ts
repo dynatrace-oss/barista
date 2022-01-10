990c0b5b98801e234f33ba467ba11993e086d203
ffriedl89 <ffriedl89@gmail.com>
2022-01-10 13:40:44 +0000

    chore(chart, microchart): Applies sanitization to options for highcharts to avoid XSS attacks with malicious options.

---
+++ libs/barista-components/core/src/util/sanitize.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2021 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { sanitize as DOMPurifySanitize } from 'dompurify';
+
+/** Sanitizes a nested object or string from malicious html code  */
+export const sanitize = <T extends {} | string>(option: T): T => {
+  if (typeof option === 'string') {
+    return DOMPurifySanitize(option);
+  }
+
+  Object.keys(option).forEach((key) => {
+    if (typeof option[key] === 'string') {
+      option[key] = DOMPurifySanitize(option[key]);
+    } else if (Array.isArray(option[key])) {
+      option[key].forEach((item, i) => {
+        option[key][i] = sanitize(item);
+      });
+    } else if (typeof option[key] === 'object') {
+      option[key] = sanitize(option[key]);
+    }
+  });
+  return option;
+};
